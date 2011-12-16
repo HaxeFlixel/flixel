bf213393aab8474e6766412d2caf596d2fc8e564
unknown <Zaphod@.(none)>
2011-12-16 13:13:10 +0300

    oops. wrong FlxAssets file

--- Source/org/flixel/FlxAssets.hx
+++ Source/org/flixel/FlxAssets.hx
@@ -65,7 +65,7 @@ class ImgLogo extends Bitmap
 
 class ImgDefault extends Bitmap 
 {
-	public function new() { super(Assets.getBitmapData("assets/data/defaultGraphics.png")); }
+	public function new() { super(Assets.getBitmapData("assets/data/default.png")); }
 }
 //
 class ImgAuto extends Bitmap 
