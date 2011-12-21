87c5431292630e9fd3ab3fbefedc9e6808403627
unknown <Zaphod@.(none)>
2011-12-21 22:15:23 +0300

    FlxCaveGenerator added

--- Source/Mode.hx
+++ Source/Mode.hx
@@ -1,5 +1,6 @@
 package;
 
+import addonsTests.CaveGeneratorTest;
 import org.flixel.FlxGame;
 	
 class Mode extends FlxGame
@@ -7,6 +8,7 @@ class Mode extends FlxGame
 	public function new()
 	{
 		super(320, 240, MenuState, 2, 50, 50);
+		//super(320, 240, CaveGeneratorTest, 2, 50, 50);
 		forceDebugger = true;
 	}
 }
