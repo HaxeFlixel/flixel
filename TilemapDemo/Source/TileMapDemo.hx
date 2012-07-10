cd00c4c1738f97d85f54b04a0612bbdacf4b1b05
Alexander Hohlov <zaphy@yandex.ru>
2012-07-10 20:49:53 +0400

    Tilemap demo

---
+++ TilemapDemo/Source/TileMapDemo.hx
@@ -0,0 +1,17 @@
+package ;
+import org.flixel.FlxGame;
+
+/**
+ * ...
+ * @author Zaphod
+ */
+
+class TileMapDemo extends FlxGame
+{
+
+	public function new() 
+	{
+		super(400, 300, MenuState, 1, 20, 20);
+	}
+	
+}
