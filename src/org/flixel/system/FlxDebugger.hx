9a080245db697565fc9123d9701e4af2478f2882
impaler <user123@server.fake>
2013-02-21 15:27:03 +1030

    Fix mouse hide show behaviour with debugger

--- src/org/flixel/system/FlxDebugger.hx
+++ src/org/flixel/system/FlxDebugger.hx
@@ -10,6 +10,7 @@ import nme.geom.Point;
 import nme.geom.Rectangle;
 import nme.text.TextField;
 import nme.text.TextFormat;
+import nme.ui.Mouse;
 import org.flixel.FlxAssets;
 import org.flixel.FlxG;
 import org.flixel.system.debug.Log;
@@ -168,6 +169,9 @@ class FlxDebugger extends Sprite
 	private function onMouseOver(E:MouseEvent = null):Void
 	{
 		hasMouse = true;
+		#if !FLX_NO_MOUSE
+		Mouse.show();
+		#end
 	}
 	
 	/**
@@ -177,6 +181,10 @@ class FlxDebugger extends Sprite
 	private function onMouseOut(E:MouseEvent = null):Void
 	{
 		hasMouse = false;
+		#if !FLX_NO_MOUSE
+		if (!FlxG.mouse.useSystemCursor && !FlxG._game._debugger.vcr.paused)
+			Mouse.hide();
+		#end
 	}
 	
 	/**
