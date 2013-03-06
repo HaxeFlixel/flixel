73a0549fd9a0b2add6fd52810069bac34b6bafa1
Zaphod <zaphy@yandex.ru>
2013-03-06 23:57:45 +0400

    some code formatting

--- src/org/flixel/plugin/texturepacker/FlxSpriteTex.hx
+++ src/org/flixel/plugin/texturepacker/FlxSpriteTex.hx
@@ -14,92 +14,83 @@ import nme.display.BitmapData;
 
 class FlxSpriteTex extends FlxSprite
 {
-  var _tex : TexturePackerSprites;
-  var _spriteId : Int;
-
-  public function new (x, y, spriteName : String, tex)
-  {
-    _tex = tex;
-    _spriteId = tex.frames.get (spriteName);
+	var _tex:TexturePackerSprites;
+	var _spriteId:Int;
+	
+	public function new (x:Float = 0, y:Float = 0, spriteName:String, tex:TexturePackerSprites)
+	{
+		_tex = tex;
+		_spriteId = tex.frames.get(spriteName);
 #if !flash
-    super (x, y, tex.assetName);
+		super(x, y, tex.assetName);
 #else
-    super (x, y);
-
-    var spriteInfo = _tex.sprites[_spriteId];
-    var bm : BitmapData = new BitmapData (
-        Std.int (spriteInfo.source.width),
-        Std.int (spriteInfo.source.height),
-        true, 0x00ffffff);
-
-    if (spriteInfo.rotated)
-    {
-      _matrix.identity ();
-      _matrix.translate (-spriteInfo.source.width * 0.5,
-          -spriteInfo.source.height * 0.5);
-      _matrix.rotate (-90.0 * FlxG.RAD);
-      _matrix.translate (spriteInfo.source.width * 0.5,
-          spriteInfo.source.height * 0.5);
-      _matrix.translate (spriteInfo.offset.x, spriteInfo.offset.y);
-
-      var r = new Rectangle (spriteInfo.frame.x,
-          spriteInfo.frame.y,
-          spriteInfo.frame.width + spriteInfo.offset.x,
-          spriteInfo.frame.height + spriteInfo.offset.y);
-      bm.draw (_tex.asset, _matrix, null, null, r, false);
-    }
-    else
-    {
-      var dst : Point = new Point (spriteInfo.offset.x, spriteInfo.offset.y);
-      bm.copyPixels (_tex.asset, spriteInfo.frame, dst);
-    }
-
-    loadGraphic (bm, false, false, 0, 0, false, _tex.assetName + spriteName);
+		super(x, y);
+		
+		var spriteInfo:TexturePackerSprite = _tex.sprites[_spriteId];
+		var bm:BitmapData = new BitmapData(Std.int(spriteInfo.source.width), Std.int(spriteInfo.source.height), true, 0x00ffffff);
+		
+		if (spriteInfo.rotated)
+		{
+			_matrix.identity();
+			_matrix.translate(-spriteInfo.source.width * 0.5, -spriteInfo.source.height * 0.5);
+			_matrix.rotate(-90.0 * FlxG.RAD);
+			_matrix.translate(spriteInfo.source.width * 0.5, spriteInfo.source.height * 0.5);
+			_matrix.translate(spriteInfo.offset.x, spriteInfo.offset.y);
+			
+			var r:Rectangle = new Rectangle(spriteInfo.frame.x, spriteInfo.frame.y, spriteInfo.frame.width + spriteInfo.offset.x, spriteInfo.frame.height + spriteInfo.offset.y);
+			bm.draw(_tex.asset, _matrix, null, null, r, false);
+		}
+		else
+		{
+			var dst:Point = new Point(spriteInfo.offset.x, spriteInfo.offset.y);
+			bm.copyPixels(_tex.asset, spriteInfo.frame, dst);
+		}
+		
+		loadGraphic(bm, false, false, 0, 0, false, _tex.assetName + spriteName);
 #end
-  }
-
-  override private function resetHelpers () : Void
-  {
+	}
+	
+	override private function resetHelpers():Void
+	{
 #if !flash
-    var spriteInfo = _tex.sprites[_spriteId];
-    width = frameWidth = Std.int (spriteInfo.source.width);
-    height = frameHeight = Std.int (spriteInfo.source.height);
-    offset = spriteInfo.offset;
-    if (spriteInfo.rotated)
-    {
-      _additionalAngle = -90.0;
-    }
+		var spriteInfo:TexturePackerSprite = _tex.sprites[_spriteId];
+		width = frameWidth = Std.int(spriteInfo.source.width);
+		height = frameHeight = Std.int(spriteInfo.source.height);
+		offset = spriteInfo.offset;
+		if (spriteInfo.rotated)
+		{
+			_additionalAngle = -90.0;
+		}
 #end
-    super.resetHelpers ();
-  }
+		super.resetHelpers();
+	}
 
 #if !flash
-  override public function getAtlas () : Atlas
-  {
-    var bm : BitmapData = FlxG._cache.get (_bitmapDataKey);
-    if (bm != null)
-    {
-      return TexturePackerAtlas.getAtlas (_bitmapDataKey, bm, _tex);
-    }
-    else
-    {
-#if !FLX_NO_DEBUG
-      throw "There isn't bitmapdata in cache with key: " + _bitmapDataKey;
+	override public function getAtlas():Atlas
+	{
+		var bm:BitmapData = FlxG._cache.get(_bitmapDataKey);
+		if (bm != null)
+		{
+			return TexturePackerAtlas.getAtlas(_bitmapDataKey, bm, _tex);
+		}
+		else
+		{
+	#if !FLX_NO_DEBUG
+			throw "There isn't bitmapdata in cache with key: " + _bitmapDataKey;
+	#end
+		}
+		
+		return null;
+	}
 #end
-    }
 
-    return null;
-  }
-#end
-
-  override public function updateFrameData () : Void
-  {
+	override public function updateFrameData():Void
+	{
 #if !flash
-    _framesData = _node.addSpriteFramesData (Std.int (width), Std.int (height));
-    _frameID = _framesData.frameIDs[_spriteId];
+		_framesData = _node.addSpriteFramesData(Std.int(width), Std.int(height));
+		_frameID = _framesData.frameIDs[_spriteId];
 #else
-    super.updateFrameData ();
+		super.updateFrameData ();
 #end
-  }
+	}
 }
-
--- src/org/flixel/plugin/texturepacker/TexturePackerSprites.hx
+++ src/org/flixel/plugin/texturepacker/TexturePackerSprites.hx
@@ -8,67 +8,63 @@ import haxe.Json;
 
 class TexturePackerSprites
 {
-  public var frames : Hash <Int>;
-  public var sprites : Array <TexturePackerSprite>;
+	public var frames:Hash<Int>;
+	public var sprites:Array<TexturePackerSprite>;
 
-  public var data : Dynamic;
-  public var assetName : String;
-  public var asset : BitmapData;
+	public var data:Dynamic;
+	public var assetName:String;
+	public var asset:BitmapData;
 
-  public function new (description : String, assetName : String)
-  {
-    this.frames = new Hash ();
-    this.sprites = new Array ();
-
-    this.assetName = assetName;
-    this.asset = Assets.getBitmapData (this.assetName);
-    this.data = Json.parse (Assets.getText (description));
-
-    for (frame in Lambda.array (data.frames))
-    {
-      this.sprites.push (new TexturePackerSprite (frame));
-      this.frames.set (frame.filename, this.sprites.length - 1);
-    }
-  }
+	public function new (description:String, assetName:String)
+	{
+		this.frames = new Hash<Int>();
+		this.sprites = new Array<TexturePackerSprite>();
+		
+		this.assetName = assetName;
+		this.asset = Assets.getBitmapData(this.assetName);
+		this.data = Json.parse(Assets.getText(description));
+		
+		for (frame in Lambda.array(data.frames))
+		{
+			this.sprites.push(new TexturePackerSprite(frame));
+			this.frames.set(frame.filename, this.sprites.length - 1);
+		}
+	}
 }
 
 class TexturePackerSprite
 {
-  public var frame : Rectangle;
-  public var source : Rectangle;
-  public var trimmed : Bool;
-  public var rotated : Bool;
-  public var offset : FlxPoint;
+	public var frame:Rectangle;
+	public var source:Rectangle;
+	public var trimmed:Bool;
+	public var rotated:Bool;
+	public var offset:FlxPoint;
+	public var filename:String;
 
-  public function new (s : Dynamic)
-  {
-    trimmed = s.trimmed;
-    rotated = s.rotated;
-    source = new Rectangle (0, 0, s.sourceSize.w, s.sourceSize.h);
-    offset = new FlxPoint (0, 0);
+	public function new (s:Dynamic)
+	{
+		trimmed = s.trimmed;
+		rotated = s.rotated;
+		filename = s.filename;
+		source = new Rectangle(0, 0, s.sourceSize.w, s.sourceSize.h);
+		offset = new FlxPoint(0, 0);
 #if !flash
-    // we use negative offset because code in FlxSprite.draw
-    // use it in such way
-    offset.make (
-        -(s.sourceSize.w - s.frame.w) * 0.5,
-        -(s.sourceSize.h - s.frame.h) * 0.5);
-    if (rotated)
-    {
-      frame = new Rectangle (s.frame.x, s.frame.y, s.frame.h, s.frame.w);
-    }
-    else
-    {
-      frame = new Rectangle (s.frame.x, s.frame.y, s.frame.w, s.frame.h);
-    }
+		// we use negative offset because code in FlxSprite.draw
+		// use it in such way
+		offset.make (-(s.sourceSize.w - s.frame.w) * 0.5, -(s.sourceSize.h - s.frame.h) * 0.5);
+		if (rotated)
+		{
+			frame = new Rectangle (s.frame.x, s.frame.y, s.frame.h, s.frame.w);
+		}
+		else
+		{
+			frame = new Rectangle (s.frame.x, s.frame.y, s.frame.w, s.frame.h);
+		}
 #else
-    frame = new Rectangle (s.frame.x, s.frame.y, s.frame.w, s.frame.h);
-    // we use positive offset because in FlxSpriteTex.new
-    // we need to translate a sprite
-    offset.make (
-        (s.sourceSize.w - s.frame.w) * 0.5,
-        (s.sourceSize.h - s.frame.h) * 0.5);
+		frame = new Rectangle (s.frame.x, s.frame.y, s.frame.w, s.frame.h);
+		// we use positive offset because in FlxSpriteTex.new
+		// we need to translate a sprite
+		offset.make ((s.sourceSize.w - s.frame.w) * 0.5, (s.sourceSize.h - s.frame.h) * 0.5);
 #end
-  }
+	}
 }
-
-
