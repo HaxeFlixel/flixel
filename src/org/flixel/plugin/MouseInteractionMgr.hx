0e47b6df2248e1e4b5a624d150dba49a33c86e93
Zaphod <zaphy@yandex.ru>
2013-03-08 23:24:28 +0400

    formatting and fix for non-flash targets for MouseInteractionMgr

--- src/org/flixel/plugin/MouseInteractionMgr.hx
+++ src/org/flixel/plugin/MouseInteractionMgr.hx
@@ -14,88 +14,82 @@ typedef SpriteReg = {
 	var sprite:FlxSprite;
 	var mouseChildren:Bool;
 	var mouseEnabled:Bool;
-	var onMouseDown:FlxSprite-> Void;
-	var onMouseUp:FlxSprite-> Void;
-	var onMouseOver:FlxSprite-> Void;
-	var onMouseOut:FlxSprite-> Void;
+	var onMouseDown:FlxSprite->Void;
+	var onMouseUp:FlxSprite->Void;
+	var onMouseOver:FlxSprite->Void;
+	var onMouseOut:FlxSprite->Void;
 }
 /**
- * Provides mouse event detection for normal FlxSprites.
- * @author TiagoLr (~~~ ProG4mr ~~~)
- */
+* Provides mouse event detection for normal FlxSprites.
+* @author TiagoLr (~~~ ProG4mr ~~~)
+*/
 class MouseInteractionMgr extends FlxBasic
 {
 	private static var registeredSprites:Array<SpriteReg>;
 	private static var mouseOverSprites:Array<SpriteReg>;
-	
+
 	private static var _point:FlxPoint;
-	
+
 	/**
-	 * Call this using FlxG.addPlugin(new MouseInteractionMgr()).
-	 */
+	* Call this using FlxG.addPlugin(new MouseInteractionMgr()).
+	*/
 	public function new()
 	{
 		super();
-		
+
 		_point = new FlxPoint();
-		// 
-		if (registeredSprites != null) 
+		//
+		if (registeredSprites != null)
 		{
 			clearRegistry();
 		}
 		//
-		
+
 		registeredSprites = new Array<SpriteReg>();
 		mouseOverSprites = new Array<SpriteReg>();
 	}
 	/**
-	 * As alternative you can call MouseInteractionMgr.initTimeManager()).
-	 */
+	* As alternative you can call MouseInteractionMgr.initTimeManager()).
+	*/
 	public static function initTimerManager()
 	{
 		if (FlxG.getPlugin(MouseInteractionMgr) == null)
 			FlxG.addPlugin(new MouseInteractionMgr());
 	}
 	/**
-	 * Adds a sprite to MouseInteractionMgr registry.
-	 * Even without any initialization, this is all that is needed to add mouse behaviour 
-	 * to FlxSprites.
-	 * 
-	 * @param	sprite			The FlxSprite
-	 * @param	onMouseDown		Callback when mouse is pressed down over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite). 
-	 * @param	onMouseUp		Callback when mouse is released over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
-	 * @param	onMouseOver		Callback when mouse is this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
-	 * @param	onMouseOut		Callback when mouse moves out of this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
-	 * @param	mouseChildren	If mouseChildren is enabled, other sprites overlaped by this will still receive mouse events.
-	 * @param	mouseEnabled	If mouseEnabled this sprite will receive mouse events.
-	 */
-	public static function addSprite(sprite:FlxSprite,
-									onMouseDown:FlxSprite-> Void = null,
-									onMouseUp:FlxSprite->Void = null,
-									onMouseOver:FlxSprite-> Void = null, 
-									onMouseOut:FlxSprite-> Void = null, 
-									mouseChildren = false, mouseEnabled = true) 
+	* Adds a sprite to MouseInteractionMgr registry.
+	* Even without any initialization, this is all that is needed to add mouse behaviour
+	* to FlxSprites.
+	*
+	* @param sprite The FlxSprite
+	* @param onMouseDown Callback when mouse is pressed down over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	* @param onMouseUp Callback when mouse is released over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	* @param onMouseOver Callback when mouse is this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	* @param onMouseOut Callback when mouse moves out of this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	* @param mouseChildren If mouseChildren is enabled, other sprites overlaped by this will still receive mouse events.
+	* @param mouseEnabled If mouseEnabled this sprite will receive mouse events.
+	*/
+	public static function addSprite(sprite:FlxSprite, onMouseDown:FlxSprite->Void = null, onMouseUp:FlxSprite->Void = null, onMouseOver:FlxSprite->Void = null, onMouseOut:FlxSprite->Void = null, mouseChildren = false, mouseEnabled = true)
 	{
 		if (FlxG.getPlugin(MouseInteractionMgr) == null)
 			FlxG.addPlugin(new MouseInteractionMgr());
-		
+
 		var newReg:SpriteReg = {
-			sprite : sprite,
-			mouseChildren 	: mouseChildren,
-			mouseEnabled 	: mouseEnabled,
-			onMouseDown 	: onMouseDown,
-			onMouseUp 		: onMouseUp,
-			onMouseOver		: onMouseOver,
-			onMouseOut	 	: onMouseOut,
+			sprite: sprite,
+			mouseChildren: mouseChildren,
+			mouseEnabled: mouseEnabled,
+			onMouseDown: onMouseDown,
+			onMouseUp: onMouseUp,
+			onMouseOver: onMouseOver,
+			onMouseOut: onMouseOut,
 		}
-		
+
 		registeredSprites.unshift(newReg);
-		
 	}
 	/**
-	 * Removes a sprite from the registry.
-	 * @param	sprite
-	 */
+	* Removes a sprite from the registry.
+	* @param sprite
+	*/
 	public static function removeSprite(sprite:FlxSprite)
 	{
 		for (reg in registeredSprites)
@@ -112,37 +106,36 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * 
-	 */
-	override public function destroy():Void 
+	*
+	*/
+	override public function destroy():Void
 	{
 		clearRegistry();
 		_point = null;
 		super.destroy();
 	}
 	/**
-	 * 
-	 */
-	override public function update():Void 
+	*
+	*/
+	override public function update():Void
 	{
 		super.update();
-		
+
 		var currentOverSprites:Array<SpriteReg> = new Array<SpriteReg>();
 		for (reg in registeredSprites)
 		{
-			
 			// Sprite destroyed check.
 			if (reg.sprite.scale == null)
 			{
 				removeSprite(reg.sprite);
 				continue;
 			}
-			
+
 			if (reg.sprite.alive == false || reg.mouseEnabled == false )
 			{
 				continue;
 			}
-			
+
 			if (checkOverlap(reg.sprite))
 			{
 				currentOverSprites.push(reg);
@@ -152,15 +145,15 @@ class MouseInteractionMgr extends FlxBasic
 				}
 			}
 		}
-		
+
 		// MouseOver - Look for new sprites with mouse over.
-		for (current in currentOverSprites) 
+		for (current in currentOverSprites)
 		{
 			if (getRegister(current.sprite, mouseOverSprites) == null)
 			{
 				if (current.onMouseOver != null)
 					current.onMouseOver(current.sprite);
-			} 
+			}
 		}
 		// MouseOut - Look for sprites that lost mouse over.
 		for (over in mouseOverSprites)
@@ -189,9 +182,8 @@ class MouseInteractionMgr extends FlxBasic
 					current.onMouseUp(current.sprite);
 			}
 		}
-		
+
 		mouseOverSprites = currentOverSprites;
-		
 	}
 
 	private function checkOverlap(sprite:FlxSprite):Bool
@@ -202,28 +194,32 @@ class MouseInteractionMgr extends FlxBasic
 		while (i < l)
 		{
 			camera = FlxG.cameras[i++];
-			
+
 			#if !FLX_NO_MOUSE
 			FlxG.mouse.getWorldPosition(camera, _point);
-			if (sprite.angle != 0) 
+			if (sprite.angle != 0)
 			{
 				FlxU.rotatePoint(_point.x, _point.y, sprite.x + sprite.origin.x, sprite.y + sprite.origin.y, -180 + sprite.angle, _point);	
 			}
 			if (sprite.overlapsPoint(_point, true, camera))
 			{
+				#if flash
 				if (sprite.pixelsOverlapPoint(_point, 0x0, camera))
+				#else
+				if (sprite.pixelsOverlapPoint(_point, 0x01, camera))
+				#end
 				{
 					return true;
 				}
 			}
 			#end
-			
+
 			#if !FLX_NO_TOUCH
 			for (j in 0...FlxG.touchManager.touches.length)
 			{
 				var touch:FlxTouch = FlxG.touchManager.touches[j];
 				touch.getWorldPosition(camera, _point);
-				if (sprite.angle != 0) 
+				if (sprite.angle != 0)
 				{
 					FlxU.rotatePoint(_point.x, _point.y, sprite.x + sprite.origin.x, sprite.y + sprite.origin.y, -180 + sprite.angle, _point);	
 				}
@@ -237,57 +233,57 @@ class MouseInteractionMgr extends FlxBasic
 			}
 			#end
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
-	 * Reorders the registered sprites, using the current sprite drawing order.
-	 * This should be called if you alter the draw/update order of a registered sprite,
-	 * That is, if you alter the position of a registered sprite inside its FlxGroup.
-	 * It may also be called if the sprites are not registered by the same order they are
-	 * added to FlxGroup.
-	 */
+	* Reorders the registered sprites, using the current sprite drawing order.
+	* This should be called if you alter the draw/update order of a registered sprite,
+	* That is, if you alter the position of a registered sprite inside its FlxGroup.
+	* It may also be called if the sprites are not registered by the same order they are
+	* added to FlxGroup.
+	*/
 	public static function reorderSprites()
 	{
 		var orderedSprites:Array<SpriteReg> = new Array<SpriteReg>();
 		var group:Array<FlxBasic> = FlxG.state.members;
-		
+
 		traverseFlxGroup(FlxG.state, orderedSprites);
-		
+
 		orderedSprites.reverse();
 		registeredSprites = orderedSprites;
 	}
 
 	private static function traverseFlxGroup(group:FlxGroup, orderedSprites:Array<SpriteReg>)
 	{
-		for (basic in group.members) 
+		for (basic in group.members)
 		{
-			if (Std.is(basic , FlxGroup))
+			if (Std.is(basic, FlxGroup))
 			{
 				traverseFlxGroup(cast(basic, FlxGroup), orderedSprites);
 			}
-			
+
 			if (Std.is(basic, FlxSprite))
 			{
 				var reg = getRegister(cast(basic, FlxSprite));
-				if (reg != null) 
+				if (reg != null)
 				{
 					orderedSprites.push(reg);
 				}
 			}
 		}
 	}
-	
+
 	// ---------------------------------------------------------------------------
 	// ---------------------------------------------------------------------------
 
 	/**
-	 * Sets the mouseDown callback associated with a sprite.
-	 * 
-	 * @param	sprite			The sprite to set the callback.
-	 * @param	onMouseDown		Callback when mouse is pressed down over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite). 
-	 */
+	* Sets the mouseDown callback associated with a sprite.
+	*
+	* @param sprite The sprite to set the callback.
+	* @param onMouseDown Callback when mouse is pressed down over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	*/
 	public static function setMouseDownCallback(sprite:FlxSprite, onMouseDown:FlxSprite-> Void)
 	{
 		var reg:SpriteReg = getRegister(sprite);
@@ -297,11 +293,11 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Sets the mouseUp callback associated with a sprite.
-	 * 
-	 * @param	sprite			The sprite to set the callback.
-	 * @param	onMouseUp		Callback when mouse is released over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite). 
-	 */
+	* Sets the mouseUp callback associated with a sprite.
+	*
+	* @param sprite The sprite to set the callback.
+	* @param onMouseUp Callback when mouse is released over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	*/
 	public static function setMouseUpCallback(sprite:FlxSprite, onMouseUp:FlxSprite-> Void)
 	{
 		var reg:SpriteReg = getRegister(sprite);
@@ -311,11 +307,11 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Sets the mouseOver callback associated with a sprite.
-	 * 
-	 * @param	sprite			The sprite to set the callback.
-	 * @param	onMouseOver		Callback when mouse is over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite). 
-	 */
+	* Sets the mouseOver callback associated with a sprite.
+	*
+	* @param sprite The sprite to set the callback.
+	* @param onMouseOver Callback when mouse is over this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	*/
 	public static function setMouseOverCallback(sprite:FlxSprite, onMouseOver:FlxSprite-> Void)
 	{
 		var reg:SpriteReg = getRegister(sprite);
@@ -325,11 +321,11 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Sets the mouseOut callback associated with a sprite.
-	 * 
-	 * @param	sprite			The FlxSprite to set the callback.
-	 * @param	onMouseOver		Callback when mouse is moves out of this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite). 
-	 */
+	* Sets the mouseOut callback associated with a sprite.
+	*
+	* @param sprite The FlxSprite to set the callback.
+	* @param onMouseOver Callback when mouse is moves out of this sprite. Must have Sprite as argument - e.g. onMouseDown(sprite:FlxSprite).
+	*/
 	public static function setMouseOutCallback(sprite:FlxSprite, onMouseOut:FlxSprite-> Void)
 	{
 		var reg:SpriteReg = getRegister(sprite);
@@ -339,11 +335,11 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Enables/disables mouse behavior for an FlxSprite.
-	 * 
-	 * @param	sprite			The FlxSprite.
-	 * @param	mouseEnabled	Whether this sprite will be tested for mouse events.
-	 */
+	* Enables/disables mouse behavior for an FlxSprite.
+	*
+	* @param sprite The FlxSprite.
+	* @param mouseEnabled Whether this sprite will be tested for mouse events.
+	*/
 	public static function setSpriteMouseEnabled(sprite:FlxSprite, mouseEnabled:Bool)
 	{
 		var reg:SpriteReg = getRegister(sprite);
@@ -353,30 +349,30 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Checks if an FlxSprite is mouseEnabled.
-	 * 
-	 * @param	sprite		The FlxSprite.
-	 * @return	
-	 */
+	* Checks if an FlxSprite is mouseEnabled.
+	*
+	* @param sprite The FlxSprite.
+	* @return
+	*/
 	public static function isSpriteMouseEnabled(sprite:FlxSprite):Bool
 	{
 		var reg:SpriteReg = getRegister(sprite);
 		if (reg != null)
 		{
 			return reg.mouseEnabled;
-		} 
-		else 
+		}
+		else
 		{
 			return false;
 		}
 	}
 	/**
-	 * Enables/disables mouseChildren for an FlxSprite.
-	 * 
-	 * @param	sprite			The FlxSprite.
-	 * @param	mouseChildren	Whether this sprite will allow other overlapped sprites to receive mouseEvents.
-	 */
-	public static function setSpriteMouseChildren(sprite:FlxSprite, mouseChildren:Bool)
+	* Enables/disables mouseChildren for an FlxSprite.
+	*
+	* @param sprite The FlxSprite.
+	* @param mouseChildren Whether this sprite will allow other overlapped sprites to receive mouseEvents.
+	*/
+	public static function setSpriteMouseChildren(sprite:FlxSprite, mouseChildren:Bool):Void
 	{
 		var reg:SpriteReg = getRegister(sprite);
 		if (reg != null)
@@ -385,30 +381,30 @@ class MouseInteractionMgr extends FlxBasic
 		}
 	}
 	/**
-	 * Checks if an FlxSprite allows mouseChildren.
-	 * @param	sprite		The FlxSprite.
-	 * @return
-	 */
+	* Checks if an FlxSprite allows mouseChildren.
+	* @param sprite The FlxSprite.
+	* @return
+	*/
 	public static function isSpriteMouseChildren(sprite:FlxSprite):Bool
 	{
 		var reg:SpriteReg = getRegister(sprite);
 		if (reg != null)
 		{
 			return reg.mouseChildren;
-		} 
-		else 
+		}
+		else
 		{
 			throw new Error("MouseInteractionManager , isSpriteMouseChildren() : sprite not found");
 		}
 	}
-	
+
 	// ---------------------------------------------------------------------------
 
-	private static function getRegister(sprite:FlxSprite, register:Array<SpriteReg>=null):SpriteReg
+	private static function getRegister(sprite:FlxSprite, register:Array<SpriteReg> = null):SpriteReg
 	{
-		if (register == null) 
-			register = registeredSprites; 
-		
+		if (register == null)
+			register = registeredSprites;
+
 		for (reg in register)
 		{
 			if (reg.sprite == sprite)
@@ -418,8 +414,8 @@ class MouseInteractionMgr extends FlxBasic
 		}
 		return null;
 	}
-		
-	private function clearRegistry()
+
+	private function clearRegistry():Void
 	{
 		mouseOverSprites = null;
 		for (reg in registeredSprites)
