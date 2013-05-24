e391e166096c3875638b1319bbb45982c13995c2
Zaphod <zaphy@yandex.ru>
2013-05-24 21:23:40 +0400

    FlxSpriteGroup documentation and changes for properties

--- src/org/flixel/addons/FlxSpriteGroup.hx
+++ src/org/flixel/addons/FlxSpriteGroup.hx
@@ -4,70 +4,79 @@ import nme.display.Sprite;
 import org.flixel.FlxSprite;
 import org.flixel.FlxTypedGroup;
 
+/**
+ * <code>FlxSpriteGroup</code> is a special <code>FlxGroup</code>
+ * that can be treated like a <code>FlxSprite</code> due to having
+ * x, y and alpha values. It can only contain <code>FlxSprites</code>.
+ */
 class FlxSpriteGroup extends FlxTypedGroup<FlxSprite>
 {
-    private var _x:Float = 0;
-    private var _y:Float = 0;
-    private var _alpha:Float = 1;
-	
-	public var x(get_x, set_x):Float;
-
-    private function get_x():Float 
-	{
-		return _x;
-	}
+	/**
+	 * The x position of this group.
+	 */
+	public var x(default, set_x):Float;
 	
     private function set_x(nx:Float):Float
     {
-        var offset:Float = nx - _x;
+        var offset:Float = nx - x;
         transformChildren(xTransform, offset);
-        _x = nx;
+        x = nx;
 		return nx;
     }
 	
-	public var y(get_y, set_y):Float;
-	
-    private function get_y():Float 
-	{ 
-		return _y;
-	}
+	/**
+	 * The y position of this group.
+	 */
+	public var y(default, set_y):Float;
 	
     private function set_y(ny:Float):Float
     {
-        var offset:Float = ny - _y;
+        var offset:Float = ny - y;
         transformChildren(yTransform, offset);
-        _y = ny;
+        y = ny;
 		return ny;
     }
 	
-	public var alpha(get_alpha, set_alpha):Float;
-	
-    private function get_alpha():Float
-	{ 
-		return _alpha; 
-	}
+	/**
+	 * The alpha value of this group.
+	 */
+	public var alpha(default, set_alpha):Float;
 	
     private function set_alpha(n:Float):Float 
     {
-        _alpha = n;
-        if (_alpha > 1)  _alpha = 1;
-        else if (_alpha < 0)  _alpha = 0;
-        transformChildren(alphaTransform, _alpha);
+        alpha = n;
+        if (alpha > 1)  alpha = 1;
+        else if (alpha < 0)  alpha = 0;
+        transformChildren(alphaTransform, alpha);
 		return n;
     }
 	
+	public function new(MaxSize:Int = 0)
+	{
+		super(MaxSize);
+
+		x = 0;
+		y = 0;
+		alpha = 1;
+	}
+	
 	public function move(newX:Float, newY:Float):Void
 	{
-		var xOffset:Float = newX - _x;
-		var yOffset:Float = newY - _y;
+		var xOffset:Float = newX - x;
+		var yOffset:Float = newY - y;
 		var valueArr:Array<Dynamic> = [xOffset, yOffset];
 		var lambdaArr:Array<FlxSprite->Dynamic->Void> = [xTransform, yTransform];
 		multiTransformChildren(lambdaArr, valueArr);
-		_x = newX;
-		_y = newY;
+		x = newX;
+		y = newY;
 	}
-
-    private function transformChildren(lambda:FlxSprite->Dynamic->Void, value:Dynamic = 0):Void
+	
+	/**
+	 * Handy function that allows you to quickly transform one property of sprites in this group at a time.
+	 * @param lambda Function to transform the sprites. Example: function(s:FlxSprite, v:Dynamic) { s.acceleration.x = v; s.makeGraphic(10,10,0xFF000000); }
+	 * @param value  Value which will passed to lambda function
+	 */
+    public function transformChildren(lambda:FlxSprite->Dynamic->Void, value:Dynamic = 0):Void
     {
         var sprite:FlxSprite;
         var i:Int = 0;
@@ -81,7 +90,12 @@ class FlxSpriteGroup extends FlxTypedGroup<FlxSprite>
         }
     }
 	
-	private function multiTransformChildren(lambdaArr:Array<FlxSprite->Dynamic->Void>, valueArr:Array<Dynamic>):Void
+	/**
+	 * Handy function that allows you to quickly transform multiple properties of sprites in this group at a time.
+	 * @param	lambdaArr	Array of functions to transform sprites in this group.
+	 * @param	valueArr	Array of values which will be passed to lambda functions
+	 */
+	public function multiTransformChildren(lambdaArr:Array<FlxSprite->Dynamic->Void>, valueArr:Array<Dynamic>):Void
     {
         var numProps:Int = lambdaArr.length;
 		if (numProps > valueArr.length)
@@ -107,16 +121,31 @@ class FlxSpriteGroup extends FlxTypedGroup<FlxSprite>
         }
     }
 	
+	/**
+	 * Helper function for transformation of sprite's x coordinate
+	 * @param	s	sprite to manipulate
+	 * @param	dx	value to add to sprite's x coordinate
+	 */
 	private function xTransform(s:FlxSprite, dx:Float):Void
 	{
 		s.x += dx;
 	}
 	
+	/**
+	 * Helper function for transformation of sprite's y coordinate
+	 * @param	s	sprite to manipulate
+	 * @param	dx	value to add to sprite's y coordinate
+	 */
 	private function yTransform(s:FlxSprite, dy:Float):Void
 	{
 		s.y += dy;
 	}
 	
+	/**
+	 * Helper function for transformation of sprite's alpha coordinate
+	 * @param	s	sprite to manipulate
+	 * @param	dx	alpha value to set
+	 */
 	private function alphaTransform(s:FlxSprite, a:Float):Void
 	{
 		s.alpha = a;
