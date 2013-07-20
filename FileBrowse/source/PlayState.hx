e5ab2d36c9f96b678efe058342b31397b40ee624
Gama11 <jensfischer95@gmail.com>
2013-07-20 10:20:45 +0200

    FileBrowse: Center image, add mouse wheel scaling, keyboard shortcuts, better ui

--- FileBrowse/source/PlayState.hx
+++ FileBrowse/source/PlayState.hx
@@ -1,11 +1,14 @@
 package; 
+
 #if flash
-	import flash.display.Loader;
-	import flash.display.LoaderInfo;
-	import flash.net.FileReference;
-	import flash.net.FileFilter;
+import flash.display.Loader;
+import flash.display.LoaderInfo;
+import flash.net.FileReference;
+import flash.net.FileFilter;
+import flixel.system.FlxAssets;
+import flixel.util.FlxMath;
 #elseif (cpp || neko)
-	import systools.Dialogs;
+import systools.Dialogs;
 #end
 
 import flash.display.Bitmap;
@@ -19,6 +22,7 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
+import flixel.system.FlxAssets;
 import flixel.text.FlxText;
 import flixel.tile.FlxTilemap;
 import flixel.ui.FlxButton;
@@ -28,38 +32,122 @@ import flixel.util.FlxColor;
  * ...
  * @author Lars Doucet
  */
-
 class PlayState extends FlxState
 {
+	inline static private var MIN_SCALE:Float = 0.1;
+	inline static private var MAX_SCALE:Float = 5;
+	inline static private var ZOOM_FACTOR:Int = 40;
+	
 	private var _text:FlxText;
 	private var _button:FlxButton;
 	private var _img:FlxSprite;
+	private var _displayWidth:Float;
+	private var _displayHeight:Float;
+	private var _scaleText:FlxText;
 	
 	override public function create():Void 
 	{
-		FlxG.cameras.bgColor = 0xff888888;
+		FlxG.cameras.bgColor = FlxColor.BLACK;
+		FlxG.mouse.show();
 		
 		_img = new FlxSprite(0, 0);
-		_img.makeGraphic(FlxG.width, FlxG.height, 0xffaaaaaa);
+		_img.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		_img.antialiasing = true;
 		add(_img);
 		
-		var _button:FlxButton = new FlxButton(0, 0, "Open Image", _onClick);
+		var gutter:Int = 4;
+		
+		var uiBG:FlxSprite = new FlxSprite(0, 0);
+		uiBG.makeGraphic(FlxG.width, 30, FlxColor.BLACK);
+		uiBG.alpha = 0.6;
+		add(uiBG);
+		
+		var _button:FlxButton = new FlxButton(gutter, gutter, "Open Image", _onClick);
 		add(_button);
 		
-		var bw:Float = _button.width + 5;
-		_text = new FlxText(bw, 0, FlxG.width-(Std.int(bw)*2), "Click the button to load a PNG or JPG!");
-		_text.setFormat(null, 16, FlxColor.WHITE, "center", FlxColor.BLACK, true);
-		
+		var bw:Float = _button.width + 15;
+		_text = new FlxText(bw, gutter, Std.int(FlxG.width - bw - 5), "Click the button to load a PNG or JPG!");
+		_text.setFormat(null, 16, FlxColor.WHITE, "left", FlxColor.GRAY, true);
 		add(_text);
 		
-		FlxG.mouse.show();
+		_scaleText = new FlxText(FlxG.width - 100, FlxG.height - 20, 100, 100);
+		_scaleText.setFormat(null, 16, FlxColor.WHITE, "right", FlxColor.GRAY, true);
+		add(_scaleText);
+		
+		_showImage(FlxG.bitmap.add(FlxAssets.IMG_LOGO));
 	}
 	
-	private function _onClick():Void {
+	override public function update():Void
+	{
+		if (FlxG.mouse.wheel != 0)
+		{
+			_updateScale(_img.scale.x + FlxG.mouse.wheel / ZOOM_FACTOR);
+		}
+		
+		// Little fading effect for the scale text
+		if (_scaleText.alpha > 0)
+		{
+			_scaleText.alpha -= 0.03;
+		}
+		
+		// Reset to a scale of x1
+		if (FlxG.keys.justReleased("R") || FlxG.keys.justReleased("SPACE") || FlxG.keys.justReleased("ONE"))
+		{
+			_updateScale(1);
+		}
+		
+		// Scale shortcuts
+		if (FlxG.keys.justReleased("TWO"))
+		{
+			_updateScale(2);
+		}
+		else if (FlxG.keys.justReleased("THREE"))
+		{
+			_updateScale(3);
+		}
+		else if (FlxG.keys.justReleased("FOUR"))
+		{
+			_updateScale(4);
+		}
+		else if (FlxG.keys.justReleased("FIVE"))
+		{
+			_updateScale(5);
+		}
+		
+		super.update();
+	}
+	
+	private function _updateScale(NewScale:Float):Void
+	{
+		if (NewScale > MAX_SCALE)
+		{
+			NewScale = MAX_SCALE;
+		}
+		else if (NewScale < MIN_SCALE) 
+		{
+			NewScale = MIN_SCALE;
+		}
+		
+		_img.scale.set(NewScale, NewScale);
+		_scaleText.text = "x" + FlxMath.roundDecimal(_img.scale.x, 2);
+		_scaleText.alpha = 1;
+		_centerImage();
+	}
+	
+	private function _centerImage():Void
+	{
+		_img.offset.x = _displayWidth * _img.scale.x / 2;
+		_img.offset.y = _displayHeight * _img.scale.y / 2;
+		_img.centerOffsets();
+	}
+	
+	private function _onClick():Void 
+	{
 		_showFileDialog();
 	}
 	
-	private function _showFileDialog():Void {
+	private function _showFileDialog():Void 
+	{
 		#if flash
 			var fr:FileReference = new FileReference();
 			fr.addEventListener(Event.SELECT, _onSelect, false, 0, true);
@@ -79,65 +167,93 @@ class PlayState extends FlxState
 				, "Please select one or more files, so we can see if this method works"
 				, filters
 				);	
+				
 			_onSelect(result);
 		#end
-   }
-   
-   #if flash
-		private function _onSelect(e:Event):Void {
-			var fr:FileReference = cast(e.target, FileReference);
-			_text.text = fr.name;			
-			fr.addEventListener(Event.COMPLETE, _onLoad, false, 0, true);			
-			fr.load();
-		}   		
-		private function _onLoad(e:Event):Void {
-			var fr:FileReference = e.target;
-			fr.removeEventListener(Event.COMPLETE, _onLoad);
-						
-			var loader:Loader = new Loader();
-			loader.contentLoaderInfo.addEventListener(Event.COMPLETE, _onImgLoad);
-			loader.loadBytes(fr.data);
-		}
+	}
+
+	#if flash
+	private function _onSelect(E:Event):Void 
+	{
+		var fr:FileReference = cast(E.target, FileReference);
+		_text.text = fr.name;			
+		fr.addEventListener(Event.COMPLETE, _onLoad, false, 0, true);			
+		fr.load();
+	}   		
+	
+	private function _onLoad(E:Event):Void 
+	{
+		var fr:FileReference = E.target;
+		fr.removeEventListener(Event.COMPLETE, _onLoad);
 		
-		private function _onImgLoad(e:Event):Void {
-			var loaderInfo:LoaderInfo = e.target;
-			loaderInfo.removeEventListener(Event.COMPLETE, _onImgLoad);
-			var bmp:Bitmap = cast(loaderInfo.content, Bitmap);
-			_showImage(bmp.bitmapData);
-		}
-		
+		var loader:Loader = new Loader();
+		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, _onImgLoad);
+		loader.loadBytes(fr.data);
+	}
+	
+	private function _onImgLoad(E:Event):Void 
+	{
+		var loaderInfo:LoaderInfo = E.target;
+		loaderInfo.removeEventListener(Event.COMPLETE, _onImgLoad);
+		var bmp:Bitmap = cast(loaderInfo.content, Bitmap);
+		_showImage(bmp.bitmapData);
+	}
+	
 	#elseif (cpp || neko)
-		private function _onSelect(arr:Array<String>):Void {	   
-			if(arr != null && arr.length > 0){
-				_text.text = arr[0];
-				var img:BitmapData = BitmapData.load(arr[0]);
-				if (img != null) {					
-					trace("img = " + img.width + "," + img.height);
-					_showImage(img);
-				}
-			}else {
-				_onCancel(null);
+	private function _onSelect(arr:Array<String>):Void 
+	{	   
+		if (arr != null && arr.length > 0)
+		{
+			_text.text = arr[0];
+			var img:BitmapData = BitmapData.load(arr[0]);
+			
+			if (img != null) 
+			{					
+				trace("img = " + img.width + "," + img.height);
+				_showImage(img);
 			}
 		}
+		else 
+		{
+			_onCancel(null);
+		}
+	}
 	#end
-			
-	private function _onCancel(e:Event):Void {	   
+	
+	private function _onCancel(E:Event):Void 
+	{	   
 		_text.text = "Cancelled!";
 	}		
 	
-	private function _showImage(data:BitmapData):Void {
-		var data2:BitmapData = _img.pixels.clone();
-		var dwidth:Float = _img.width / data.width;
-		var dheight:Float = _img.height / data.height;
-				
-		var scale:Float = dwidth <= dheight ? dwidth : dheight;
-		if (scale > 1) { scale = 1;}
+	private function _showImage(Data:BitmapData):Void 
+	{
+		_img.scale.set(1, 1);
 		
+		var imgWidth:Float = FlxG.width / Data.width;
+		var imgHeight:Float = FlxG.height / Data.height;
+		
+		var scale:Float = imgWidth <= imgHeight ? imgWidth : imgHeight;
+		
+		// Cap the scale at x1
+		if (scale > 1)
+		{ 
+			scale = 1;
+		}
+		
+		_displayWidth = Data.width * scale;
+		_displayHeight = Data.height * scale;
+		_img.makeGraphic(Std.int(_displayWidth), Std.int(_displayHeight), FlxColor.BLACK);
+		
+		var data2:BitmapData = _img.pixels.clone();
 		var matrix:Matrix = new Matrix();
 		matrix.identity();
 		matrix.scale(scale, scale);
-		data2.fillRect(data2.rect, 0xffaaaaaa);
-		data2.draw(data, matrix, null, null, null, true);
+		data2.fillRect(data2.rect, FlxColor.BLACK);
+		data2.draw(Data, matrix, null, null, null, true);
 		_img.pixels = data2;
+		
+		// Center the image
+		_img.x = (FlxG.width - _displayWidth) / 2;
+		_img.y = (FlxG.height - _displayHeight) / 2;
 	}
 }
