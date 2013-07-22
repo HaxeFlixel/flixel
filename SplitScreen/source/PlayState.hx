8b4f352b92892748fa63fb2441ef8e4c5380d281
Gama11 <jensfischer95@gmail.com>
2013-07-22 11:52:19 +0200

    SpliScreen: Update and improvement

---
+++ SplitScreen/source/PlayState.hx
@@ -0,0 +1,127 @@
+package;
+
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.system.input.FlxJoystickManager;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import openfl.Assets;
+
+class PlayState extends FlxState
+{
+	private var _level:FlxTilemap;
+	private var _player1:FlxSprite;
+	private var _player2:FlxSprite;
+	
+	private var _halfWidth:Int;
+	private var _textY:Int;
+	
+	override public function create():Void
+	{
+		// Set the background color to white
+		FlxG.cameras.bgColor = FlxColor.WHITE;
+		
+		// Create the tilemap from the levelData we just created
+		_level = new FlxTilemap();
+		_level.loadMap(Assets.getText("assets/level.csv"), FlxTilemap.imgAuto, 0, 0, FlxTilemap.AUTO);
+		add(_level);
+		
+		_halfWidth = Std.int(FlxG.width / 2);
+		_textY = FlxG.height - 24;
+		
+		// Player1 is red and starts at 265, 200
+		_player1 = createPlayer(265, 200, FlxColor.RED);
+		add(_player1);
+		
+		// Player 2 is blue and starts at 65, 200
+		_player2 = createPlayer(65, 200, FlxColor.BLUE);
+		add(_player2);
+		
+		// Then we setup two cameras to follow each of the two players
+		createCamera(_halfWidth, 0xFFCCCC, _player1);
+		createCamera(0, 0xCCCCFF, _player2);
+		
+		// Some instructions
+		var textBG:FlxSprite = new FlxSprite(0, _textY);
+		textBG.makeGraphic(FlxG.width, 24, FlxColor.BLACK);
+		add(textBG);
+		
+		var blueText:FlxText = new FlxText(0, _textY, _halfWidth, "WASD");
+		blueText.setFormat(null, 16, FlxColor.AZURE, "center");
+		add(blueText);
+		
+		var redText:FlxText = new FlxText(_halfWidth, _textY, _halfWidth, "Arrow keys", 16);
+		redText.setFormat(null, 16, FlxColor.RED, "center");
+		add(redText);
+	}
+	
+	private function createPlayer(X:Int, Y:Int, Color:Int):FlxSprite
+	{
+		var player:FlxSprite = new FlxSprite(X, Y);
+		player.makeGraphic(10, 12, Color);
+		player.maxVelocity.set(100, 200);
+		player.acceleration.y = 200;
+		player.forceComplexRender = true;
+		player.drag.x = player.maxVelocity.x * 4;
+		
+		return player;
+	}
+	
+	private function createCamera(X:Int, Color:Int, Follow:FlxSprite):Void
+	{
+		var camera:FlxCamera = new FlxCamera(X, 0, _halfWidth, _textY);
+		camera.setBounds(0, 0, _level.width - 8, _textY);
+		camera.color = Color;
+		camera.follow(Follow);
+		FlxG.cameras.add(camera);
+	}
+	
+	override public function update():Void
+	{
+		// Collide everything
+		FlxG.collide();
+		
+		// Player 1 controls
+		_player1.acceleration.x = 0;
+		
+		if (FlxG.keys.LEFT)
+		{
+			_player1.acceleration.x = -_player1.maxVelocity.x * 4;
+		}
+		
+		if (FlxG.keys.RIGHT)
+		{
+			_player1.acceleration.x = _player1.maxVelocity.x * 4;
+		}
+		
+		if (FlxG.keys.justPressed("UP") && _player1.isTouching(FlxObject.FLOOR))
+		{
+			_player1.velocity.y -= _player1.maxVelocity.y / 1.5;
+		}
+		
+		// Player 2 controls
+		_player2.acceleration.x = 0;
+		
+		if (FlxG.keys.A)
+		{
+			_player2.acceleration.x = -_player2.maxVelocity.x*4;
+		}
+		
+		if (FlxG.keys.D)
+		{
+			_player2.acceleration.x = _player2.maxVelocity.x*4;
+		}
+		
+		if (FlxG.keys.justPressed("W") && _player2.isTouching(FlxObject.FLOOR))
+		{
+			_player2.velocity.y -= _player2.maxVelocity.y/1.5;
+		}
+		
+		super.update();	
+	}
+}
