3b1246c1cdaeb93917861e36497993e1631705c9
Gama11 <jensfischer95@gmail.com>
2013-07-23 13:18:38 +0200

    FlxBlur: Update

---
+++ flash-FlxBlur_backup/source/Main.hx
@@ -0,0 +1,24 @@
+﻿package;
+
+import flash.display.Sprite;
+import flash.Lib;
+
+/**
+ * ...
+ * @author Zaphod
+ */
+
+class Main extends Sprite
+{
+	
+	public static function main() 
+	{
+		new Main();
+	}
+	
+	public function new() 
+	{
+		super();
+		Lib.current.addChild(new FlxBlur());
+	}
+}
--- flash-FlxBlur_backup/source/PlayState.hx
+++ flash-FlxBlur_backup/source/PlayState.hx
@@ -1,42 +1,37 @@
 package;
-
-import flixel.effects.particles.FlxEmitter;
-import flixel.effects.particles.FlxParticle;
-import flixel.FlxG;
-import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
-import flixel.util.FlxRandom;
-
+import org.flixel.FlxEmitter;
+import org.flixel.FlxG;
+import org.flixel.FlxParticle;
+import org.flixel.FlxState;
+import org.flixel.FlxText;
+import org.flixel.util.FlxColor;
+import org.flixel.util.FlxRandom;
 class PlayState extends FlxState
 {
-	private var _enabled:Bool;
+	public var toggle:Bool;
 	
 	override public function create():Void
 	{
-		// Title text, nothing crazy here!
+		//Title text, nothing crazy here!
 		var text:FlxText;
 		text = new FlxText(FlxG.width / 4, FlxG.height / 2 - 20, Math.floor(FlxG.width / 2), "FlxBlur", true);
 		text.setFormat(null, 32, FlxColor.WHITE, "center");
 		add(text);
-		
 		text = new FlxText(FlxG.width / 4, FlxG.height / 2 + 20, Math.floor(FlxG.width / 2), "press space to toggle", true);
 		text.setFormat(null, 16, FlxColor.BLUE, "center");
 		add(text);
 		
-		// This is the particle emitter that spews things off the bottom of the screen.
-		// I'm not going to go over it in too much detail here, but basically we
+		//This is the particle emitter that spews things off the bottom of the screen.
+		//I'm not going to go over it in too much detail here, but basically we
 		// create the emitter, then we create 50 32x32 sprites and add them to it.
 		var emitter:FlxEmitter = new FlxEmitter(0, FlxG.height + 20, 50);
 		emitter.width = FlxG.width;
 		emitter.gravity = -40;
-		emitter.setXSpeed( -20, 20);
-		emitter.setYSpeed( -75, -25);
-		
+		emitter.setXSpeed(-20,20);
+		emitter.setYSpeed(-75,-25);
 		var particle:FlxParticle;
 		var particles:Int = 50;
 		var colors:Array<Int> = [FlxColor.BLUE, (FlxColor.BLUE | FlxColor.GREEN), FlxColor.GREEN, (FlxColor.GREEN | FlxColor.RED), FlxColor.RED];
-		
 		for (i in 0...particles)
 		{
 			particle = new FlxParticle();
@@ -44,31 +39,29 @@ class PlayState extends FlxState
 			particle.exists = false;
 			emitter.add(particle);
 		}
-		
 		emitter.start(false, 0, 0.1);
 		add(emitter);
 		
-		// Let the player toggle the effect with the space bar.  Effect starts on.
-		_enabled = true;
+		//Let the player toggle the effect with the space bar.  Effect starts on.
+		toggle = true;
 	}
 	
 	override public function update():Void
 	{
-		if (FlxG.keys.justPressed("SPACE"))
+		if(FlxG.keys.justPressed("SPACE"))
 		{
-			_enabled = !_enabled;
+			toggle = !toggle;
 		}
-		
-		if (_enabled)
+		if(toggle)
 		{
-			// By setting the background color to a value with a low transparency,
-			// we can generate a cool "blur" effect.
-			FlxG.cameras.bgColor = 0x11000000;
+			//By setting the background color to a value with a low transparency,
+			//we can generate a cool "blur" effect.
+			FlxG.bgColor = 0x11000000;
 		}
 		else
 		{
-			// Setting it to an opaque color will turn the effect back off.
-			FlxG.cameras.bgColor = FlxColor.BLACK;
+			//Setting it to an opaque color will turn the effect back off.
+			FlxG.bgColor = FlxColor.BLACK;
 		}
 		
 		super.update();
