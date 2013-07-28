6e8fbb52527463ac598d51ac6b1f3cf990c5ee6a
crazysam <sambatista1@gmail.com>
2013-07-28 03:24:49 -0400

    Adding deepCopy function to FlxMisc.

--- flixel/util/FlxMisc.hx
+++ flixel/util/FlxMisc.hx
@@ -33,26 +33,45 @@ class FlxMisc
 		return Type.getClassName(Object1) == Type.getClassName(Object2);
 	}
 	
-	// TODO: fix issue with compiling this function
-	// "flash.utils.ByteArray has no field writeObject"
-	// "flash.utils.ByteArray has no field readObject"
-		
 	/**
-	 * Performs a complete object deep-copy and returns a duplicate (not a reference)
+	 * Performs a complete object deep-copy and returns a duplicate (not a reference).
+	 * This works best on anonymous objects, arrays, and classes with no function calls on their constructors.
 	 * 
 	 * @param	Value	The object you want copied
 	 * @return	A copy of this object
 	 */
-	//static public function copyObject(Value:Dynamic):Dynamic
-	//{
-		
-		//var buffer:ByteArray = new ByteArray();
-		//buffer.writeObject(Value);
-		//buffer.position = 0;
-		//var result:Dynamic = buffer.readObject();
-
-		//return result;
-	//}
+	public static function deepCopy<T>( v:T ) : T 
+	{ 
+		if (!Reflect.isObject(v)) // simple type 
+		{ 
+			return v; 
+		} 
+		else if( Std.is( v, Array ) ) // array 
+		{ 
+			var r = Type.createInstance(Type.getClass(v), []); 
+			untyped 
+			{ 
+				for( ii in 0...v.length ) 
+				r.push(deepCopy(v[ii])); 
+			} 
+			return r; 
+		} 
+		else if( Type.getClass(v) == null ) // anonymous object 
+		{ 
+			var obj : Dynamic = {}; 
+			for( ff in Reflect.fields(v) ) 
+				Reflect.setField(obj, ff, deepCopy(Reflect.field(v, ff))); 
+			return obj; 
+		} 
+		else // class 
+		{ 
+			var obj = Type.createEmptyInstance(Type.getClass(v));
+			for( ff in Reflect.fields(v) ) 
+				Reflect.setField(obj, ff, deepCopy(Reflect.field(v, ff))); 
+			return obj; 
+		} 
+		return null; 
+	} 
 	
 	/**
 	 * The Display List index of the mouse pointer
