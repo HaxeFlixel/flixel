61c1a628299bd3a119074df74829a908e14b3882
Zaphod <zaphy@yandex.ru>
2013-08-03 13:16:42 +0400

    FlxSoundUtil improvement

--- flixel/util/FlxSoundUtil.hx
+++ flixel/util/FlxSoundUtil.hx
@@ -6,30 +6,22 @@ import haxe.ds.ObjectMap;
 
 class FlxSoundUtil
 {
-	static private var timerCallbackMap:ObjectMap<FlxTimer, Void->Void> = new ObjectMap<FlxTimer, Void->Void>();
-	
 	static public function playWithCallback(EmbeddedSound:String, ?OnComplete:Void->Void, Duration:Float = 0, Volume:Float = 1):FlxSound
 	{
 		var sound = FlxG.sound.play(EmbeddedSound, Volume, false, true, OnComplete);
 		#if !(flash || desktop)
-		timerCallbackMap.set(FlxTimer.start(Duration, timerCallback), OnComplete);
+		var timer:FlxTimer = FlxTimer.start(Duration, timerCallback);
+		timer.userData = OnComplete;
 		#end
 		return sound;
 	}
 	
-	static public function clear():Void
-	{
-		timerCallbackMap = new ObjectMap<FlxTimer, Void->Void>();
-	}
-	
 	static private function timerCallback(timer:FlxTimer):Void
 	{
-		if (timerCallbackMap.exists(timer))
+		var callback:Void->Void = timer.userData();
+		if (callback != null)
 		{
-			var callback:Void->Void = timerCallbackMap.get(timer);
 			callback();
-			timerCallbackMap.remove(timer);
-			timer.destroy();
 		}
 	}
 }
