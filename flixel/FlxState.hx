5d0e2921b879eedc3a53b6c198e1d7ab296f36df
rszrama <ryan.szrama@gmail.com>
2013-08-03 15:24:58 -0400

    Correct the misspellings of persistant to persistent in FlxState.hx.

--- flixel/FlxState.hx
+++ flixel/FlxState.hx
@@ -18,8 +18,8 @@ class FlxState extends FlxGroup
 	*
 	* @default false
 	*/
-	public var persistantUpdate:Bool;
-	
+	public var persistentUpdate:Bool;
+
 	/**
 	* Determines whether or not this state is updated even when it is not the active state. For
 	* example, if you have your game state first, and then you push a menu state on top of it,
@@ -30,38 +30,38 @@ class FlxState extends FlxGroup
 	*
 	* @default true
 	*/
-	public var persistantDraw:Bool;
-	
+	public var persistentDraw:Bool;
+
 	private var _subState:FlxSubState;
 	/**
 	 * Current substate.
 	 * Substates also can have substates
 	 */
 	public var subState(get_subState, null):FlxSubState;
-	
-	private function get_subState():FlxSubState 
+
+	private function get_subState():FlxSubState
 	{
 		return _subState;
 	}
-	
+
 	/**
 	 * Background color of this state
 	 */
 	private var _bgColor:Int;
 	public var bgColor(get_bgColor, set_bgColor):Int;
-	
-	private function get_bgColor():Int 
+
+	private function get_bgColor():Int
 	{
 		return FlxG.cameras.bgColor;
 	}
-	
-	private function set_bgColor(value:Int):Int 
+
+	private function set_bgColor(value:Int):Int
 	{
 		return FlxG.cameras.bgColor = value;
 	}
-	
+
 	private var _useMouse:Bool = false;
-	
+
 	/**
 	 * Whether to show mouse pointer or not
 	 */
@@ -84,69 +84,69 @@ class FlxState extends FlxGroup
 		#end
 	#end
 	}
-	
+
 	/**
 	 * State constructor
 	 */
 	public function new()
 	{
 		super();
-		
-		persistantUpdate = false;
-		persistantDraw = true;
+
+		persistentUpdate = false;
+		persistentDraw = true;
 		#if !FLX_NO_MOUSE
 		this.useMouse = FlxG.mouse.visible;
 		#end
 	}
-	
+
 	/**
 	 * This function is called after the game engine successfully switches states.
 	 * Override this function, NOT the constructor, to initialize or set up your game state.
 	 * We do NOT recommend overriding the constructor, unless you want some crazy unpredictable things to happen!
 	 */
 	public function create():Void { }
-	
+
 	override public function draw():Void
 	{
-		if (persistantDraw || _subState == null)
+		if (persistentDraw || _subState == null)
 		{
 			super.draw();
 		}
-		
+
 		if (_subState != null)
 		{
 			_subState.draw();
 		}
 	}
-	
+
 #if !FLX_NO_DEBUG
 	override public function drawDebug():Void
 	{
-		if (persistantDraw || _subState == null)
+		if (persistentDraw || _subState == null)
 		{
 			super.drawDebug();
 		}
-		
+
 		if (_subState != null)
 		{
 			_subState.drawDebug();
 		}
 	}
 #end
-	
+
 	public function tryUpdate():Void
 	{
-		if (persistantUpdate || _subState == null)
+		if (persistentUpdate || _subState == null)
 		{
 			update();
 		}
-		
+
 		if (_subState != null)
 		{
 			_subState.tryUpdate();
 		}
 	}
-	
+
 	/**
 	 * Manually close the sub-state
 	*/
@@ -154,7 +154,7 @@ class FlxState extends FlxGroup
 	{
 		this.setSubState(null, null, destroy);
 	}
-	
+
 	/**
 	 * Set substate for this state
 	 * @param	requestedState		substate to add
@@ -179,15 +179,15 @@ class FlxState extends FlxGroup
 			//WARNING: What if the state has already been created?
 			// I'm just copying the code from "FlxGame::switchState" which doesn't check for already craeted states. :/
 			_subState._parentState = this;
-			
+
 			_subState.closeCallback = closeCallback;
 
 			//Reset the input so things like "justPressed" won't interfere
-			if (!persistantUpdate) 
-			{ 
+			if (!persistentUpdate)
+			{
 				FlxG.inputs.reset();
 			}
-			
+
 			if (!_subState.initialized)
 			{
 				_subState.initialize();
@@ -195,7 +195,7 @@ class FlxState extends FlxGroup
 			}
 		}
 	}
-	
+
 	/**
 	 * Helper method for closing substate
 	 * @param	destroy		whether to destroy current substate (by default) or leave it as is, so closed substate can be reused many times
@@ -206,36 +206,36 @@ class FlxState extends FlxGroup
 		{
 			_subState.closeCallback();
 		}
-		
+
 		if (destroy)
 		{
 			_subState.destroy();
 		}
 		_subState = null;
-		
+
 		this.updateMouseVisibility();
 	}
 
 	override public function destroy():Void
 	{
-		if (_subState != null)	
+		if (_subState != null)
 			this.closeSubState();
-			
+
 		super.destroy();
 	}
-	
+
 	/**
 	 * This method is called after application losts its focus.
 	 * Can be useful if you using third part libraries, such as tweening engines.
 	 */
 	public function onFocusLost():Void { }
-	
+
 	/**
 	 * This method is called after application gets focus.
 	 * Can be useful if you using third part libraries, such as tweening engines.
 	 */
 	public function onFocus():Void { }
-	
+
 	/**
 	 * This method is called after application gets resized.
 	 * Can be useful to update the game based on the new dimensions.
