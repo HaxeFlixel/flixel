88e44f2d0bfdf3cb50b1f7f3361a464a70f3e755
sergey-miryanov <sergey.miryanov@gmail.com>
2013-08-18 19:15:39 +0600

    Fix log call in FlxGame

--- flixel/FlxGame.hx
+++ flixel/FlxGame.hx
@@ -272,7 +272,7 @@ class FlxGame extends Sprite
 		
 		if (FlxG.framerate < FlxG.flashFramerate)
 		{
-			log.warn("FlxG.flashFramerate: The game's framerate shouldn't be smaller than the flash framerate, since it can stop your game from updating.");
+			FlxG.log.warn("FlxG.flashFramerate: The game's framerate shouldn't be smaller than the flash framerate, since it can stop your game from updating.");
 		}
 		
 		// Finally, set up an event for the actual game loop stuff.
