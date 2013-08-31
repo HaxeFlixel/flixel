e61b2a594022e465a93fa0008bee261c60f39b0c
Lars Doucet <lars.doucet@gmail.com>
2013-08-31 15:50:33 -0500

    RPG Interface: Skinnable pop-ups

--- User Interface/RPG Interface/source/State_DefaultTest.hx
+++ User Interface/RPG Interface/source/State_DefaultTest.hx
@@ -1,3 +1,4 @@
+import flixel.addons.ui.FlxUIPopup;
 import haxe.xml.Fast;
 import flash.Lib;
 import flixel.FlxG;
@@ -23,8 +24,33 @@ class State_DefaultTest extends FlxUIState
 	
 	public override function eventResponse(id:String,target:Dynamic,data:Array<Dynamic>):Void {
 		if (data != null) {
-			switch(cast(data[0], String)) {
-				case "back": FlxG.switchState(new State_Title());
+			switch(id) {
+				case "click_button":
+					switch(cast(data[0], String)) {
+						case "back": FlxG.switchState(new State_Title());
+						case "popup":var popup:FlxUIPopup = new FlxUIPopup(); //create the popup
+									 popup.quickSetup						  //set it up
+									 (						  
+									   Main.tongue.get("$POPUP_DEMO_2_TITLE","ui"), 	//title text
+									   Main.tongue.get("$POPUP_DEMO_2_BODY","ui"), 		//body text
+									   ["<yes>", "<no>", "<cancel>"]	//FlxUI will translate labels to "$POPUP_YES", etc, 
+									 ); 	  							  //and localize them if possible, 
+																		  //otherwise it will display "YES", "NO", etc
+																		  
+									 setSubState(popup);				  //show the popup
+									 
+									 //you can call quickSetup() before or after setting the subState, it will behave properly either way
+									 
+									 //since the above example is a little messy, here's what it looks like without the firetongue calls:
+									 //  popup.quickSetup("title","body",["Yes","No","Cancel"]);
+									
+					}
+				case "click_popup":
+					switch(cast(data[0], Int)) {
+						case 0:	trace("Yes was clicked");
+						case 1: trace("No was clicked");
+						case 2: trace("Cancel was clicked");
+					}
 			}
 		}
 	}
