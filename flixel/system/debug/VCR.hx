61e879974b75da8bc6f15cb95c79c52115aac9da
impaler <user123@server.fake>
2013-09-10 12:37:52 +1000

    init new Debugger FlxSystemButtons

--- flixel/system/debug/VCR.hx
+++ flixel/system/debug/VCR.hx
@@ -1,7 +1,7 @@
 package flixel.system.debug;
 
-import flash.display.Bitmap;
-import flash.display.Sprite;
+import flixel.system.ui.FlxSystemButton;
+import flash.display.BitmapData;
 import flash.events.Event;
 import flash.events.IOErrorEvent;
 import flash.events.MouseEvent;
@@ -25,7 +25,7 @@ import flash.net.FileReference;
 /**
  * This class contains the record, stop, play, and step 1 frame buttons seen on the top edge of the debugger overlay.
  */
-class VCR extends Sprite
+class VCR
 {
 	#if flash
 	static private var FILE_TYPES:Array<FileFilter> = [new FileFilter("Flixel Game Recording", "*.fgr")];
@@ -41,28 +41,6 @@ class VCR extends Sprite
 	 */
 	public var stepRequested:Bool;
 	
-	private var _open:Bitmap;
-	private var _recordOff:Bitmap;
-	private var _recordOn:Bitmap;
-	private var _stop:Bitmap;
-	private var _flixel:Bitmap;
-	private var _restart:Bitmap;
-	private var _pause:Bitmap;
-	private var _play:Bitmap;
-	private var _step:Bitmap;
-	
-	private var _overOpen:Bool;
-	private var _overRecord:Bool;
-	private var _overRestart:Bool;
-	private var _overPause:Bool;
-	private var _overStep:Bool;
-	
-	private var _pressingOpen:Bool;
-	private var _pressingRecord:Bool;
-	private var _pressingRestart:Bool;
-	private var _pressingPause:Bool;
-	private var _pressingStep:Bool;
-	
 	#if flash
 	private var _file:FileReference;
 	#end
@@ -70,28 +48,26 @@ class VCR extends Sprite
 	private var _runtimeDisplay:TextField;
 	
 	private var _runtime:Int;
-	
-	
+
+	public var playbackToggleBtn:FlxSystemButton;
+	public var stepBtn:FlxSystemButton;
+	public var restartBtn:FlxSystemButton;
+	public var recordBtn:FlxSystemButton;
+
 	/**
 	 * Creates the "VCR" control panel for debugger pausing, stepping, and recording.
 	 */
 	public function new()
 	{
-		super();
-		
-		var spacing:Int = 7;
-		
-		_open = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_OPEN));
-		addChild(_open);
-		
-		_recordOff = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_RECORD_OFF));
-		_recordOff.x = _open.x + _open.width + spacing;
-		addChild(_recordOff);
-		
-		_recordOn = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_RECORD_ON));
-		_recordOn.x = _recordOff.x;
-		_recordOn.visible = false;
-		addChild(_recordOn);
+		//super();
+
+		playbackToggleBtn = new FlxSystemButton(FlxAssets.getBitmapData(FlxAssets.IMG_PAUSE), onPause);
+
+		stepBtn = new FlxSystemButton(FlxAssets.getBitmapData(FlxAssets.IMG_STEP), onStep);
+
+		restartBtn = new FlxSystemButton(FlxAssets.getBitmapData(FlxAssets.IMG_RESTART), FlxG.resetState);
+
+		recordBtn = new FlxSystemButton(FlxAssets.getBitmapData(FlxAssets.IMG_RESTART), onRecord);
 		
 		#if !FLX_RECORD
 		_open.visible = false;
@@ -99,35 +75,9 @@ class VCR extends Sprite
 		_recordOn.visible = false;
 		#end
 		
-		_stop = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_STOP));
-		_stop.x = _recordOff.x;
-		_stop.visible = false;
-		addChild(_stop);
-		
-		_flixel = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_FLIXEL));
-		_flixel.x = _recordOff.x + _recordOff.width + spacing;
-		addChild(_flixel);
-		
-		_restart = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_RESTART));
-		_restart.x = _flixel.x + _flixel.width + spacing;
-		addChild(_restart);
-		
-		_pause = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_PAUSE));
-		_pause.x = _restart.x + _restart.width + spacing;
-		addChild(_pause);
-		
-		_play = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_PAUSE));
-		_play.x = _pause.x;
-		_play.visible = false;
-		addChild(_play);
-		
-		_step = new Bitmap(FlxAssets.getBitmapData(FlxAssets.IMG_STEP));
-		_step.x = _pause.x + _pause.width + spacing;
-		addChild(_step);
-		
 		_runtimeDisplay = new TextField();
-		_runtimeDisplay.width = width;
-		_runtimeDisplay.x = width;
+		//_runtimeDisplay.width = width;
+		//_runtimeDisplay.x = width;
 		_runtimeDisplay.y = -2;
 		_runtimeDisplay.multiline = false;
 		_runtimeDisplay.wordWrap = false;
@@ -138,19 +88,14 @@ class VCR extends Sprite
 		//_runtimeDisplay.defaultTextFormat = new TextFormat("Courier", 12, 0xffffff, null, null, null, null, null, "center");
 		_runtimeDisplay.defaultTextFormat = dtf;
 		_runtimeDisplay.visible = false;
-		addChild(_runtimeDisplay);
+		//addChild(_runtimeDisplay);
 		_runtime = 0;
 		
 		stepRequested = false;
+
 		#if flash
 		_file = null;
 		#end
-		
-		unpress();
-		checkOver();
-		updateGUI();
-		
-		addEventListener(Event.ENTER_FRAME, init);
 	}
 	
 	/**
@@ -161,59 +106,6 @@ class VCR extends Sprite
 		#if flash
 		_file = null;
 		#end
-		
-		if (_open != null)
-		{
-			removeChild(_open);
-		}
-		_open = null;
-		if (_recordOff != null)
-		{
-			removeChild(_recordOff);
-		}
-		_recordOff = null;
-		if (_recordOn != null)
-		{
-			removeChild(_recordOn);
-		}
-		_recordOn = null;
-		if (_stop != null)
-		{
-			removeChild(_stop);
-		}
-		_stop = null;
-		if (_flixel != null)
-		{
-			removeChild(_flixel);
-		}
-		_flixel = null;
-		if (_restart != null)
-		{
-			removeChild(_restart);
-		}
-		_restart = null;
-		if (_pause != null)
-		{
-			removeChild(_pause);
-		}
-		_pause = null;
-		if (_play != null)
-		{
-			removeChild(_play);
-		}
-		_play = null;
-		if (_step != null)
-		{
-			removeChild(_step);
-		}
-		_step = null;
-		
-		if (parent != null)
-		{
-			parent.removeEventListener(MouseEvent.MOUSE_MOVE,onMouseMove);
-			parent.removeEventListener(MouseEvent.MOUSE_DOWN,onMouseDown);
-			parent.removeEventListener(MouseEvent.MOUSE_UP,onMouseUp);
-		}
 	}
 	
 	/**
@@ -222,9 +114,9 @@ class VCR extends Sprite
 	 */
 	public function recording():Void
 	{
-		_stop.visible = false;
-		_recordOff.visible = false;
-		_recordOn.visible = true;
+		//_stop.visible = false;
+		//_recordOff.visible = false;
+		//_recordOn.visible = true;
 	}
 	
 	/**
@@ -233,9 +125,9 @@ class VCR extends Sprite
 	 */
 	public function stopped():Void
 	{
-		_stop.visible = false;
-		_recordOn.visible = false;
-		_recordOff.visible = true;
+		//_stop.visible = false;
+		//_recordOn.visible = false;
+		//_recordOff.visible = true;
 	}
 	
 	/**
@@ -244,9 +136,9 @@ class VCR extends Sprite
 	 */
 	public function playing():Void
 	{
-		_recordOff.visible = false;
-		_recordOn.visible = false;
-		_stop.visible = true;
+		//_recordOff.visible = false;
+		//_recordOn.visible = false;
+		//_stop.visible = true;
 	}
 	
 	/**
@@ -261,9 +153,54 @@ class VCR extends Sprite
 			_runtimeDisplay.visible = true;
 		}
 	}
+
+	/**
+	 * Called when the user presses the Pause button.
+	 * This is different from user-defined pause behavior, or focus lost behavior.
+	 * Does NOT pause music playback!!
+	 */
+	public function onPause():Void
+	{
+		#if !FLX_NO_MOUSE
+		if (!FlxG.mouse.useSystemCursor)
+		Mouse.show();
+		#end
+
+		paused = true;
+		playbackToggleBtn.changeIcon(FlxAssets.getBitmapData(FlxAssets.IMG_PLAY));
+		playbackToggleBtn.changeHandler(onPlay);
+	}
+
+	/**
+	 * Called when the user presses the Play button.
+	 * This is different from user-defined unpause behavior, or focus gained behavior.
+	 */
+	public function onPlay():Void
+	{
+		#if !FLX_NO_MOUSE
+		if (!FlxG.mouse.useSystemCursor)
+		Mouse.hide();
+		#end
+
+		paused = false;
+		playbackToggleBtn.changeIcon(FlxAssets.getBitmapData(FlxAssets.IMG_PAUSE));
+		playbackToggleBtn.changeHandler(onPause);
+	}
+
+	/**
+	 * Called when the user presses the fast-forward-looking button.
+	 * Requests a 1-frame step forward in the game loop.
+	 */
+	public function onStep():Void
+	{
+		if (!paused)
+		{
+			onPause();
+		}
+		stepRequested = true;
+	}
 	
 	#if FLX_RECORD
-	//*** ACTUAL BUTTON BEHAVIORS ***//
 	
 	/**
 	 * Called when the "open file" button is pressed.
@@ -354,16 +291,14 @@ class VCR extends Sprite
 	
 	/**
 	 * Called when the user presses the white record button.
-	 * If Alt is pressed, the current state is reset, and a new recording is requested.
-	 * If Alt is NOT pressed, the game is reset, and a new recording is requested.
 	 * @param	StandardMode	Whether to reset the whole game, or just this <code>FlxState</code>.  StandardMode == false is useful for recording demos or attract modes.
 	 */
 	public function onRecord(StandardMode:Bool = false):Void
 	{
-		if (_play.visible)
-		{
-			onPlay();
-		}
+		//if (_play.visible)
+		//{
+		//	onPlay();
+		//}
 		FlxG.vcr.startRecording(StandardMode);
 	}
 	
@@ -466,170 +401,124 @@ class VCR extends Sprite
 		#end
 	}
 	
-	/**
-	 * Called when the user presses the Pause button.
-	 * This is different from user-defined pause behavior, or focus lost behavior.
-	 * Does NOT pause music playback!!
-	 */
-	public function onPause():Void
-	{
-		#if !FLX_NO_MOUSE
-		if (!FlxG.mouse.useSystemCursor)
-			Mouse.show();
-		#end
-		
-		paused = true;
-		_pause.visible = false;
-		_play.visible = true;
-	}
-	
-	/**
-	 * Called when the user presses the Play button.
-	 * This is different from user-defined unpause behavior, or focus gained behavior.
-	 */
-	public function onPlay():Void
-	{
-		#if !FLX_NO_MOUSE
-		if (!FlxG.mouse.useSystemCursor)
-			Mouse.hide();
-		#end
-	
-		paused = false;
-		_play.visible = false;
-		_pause.visible = true;
-	}
-	
-	/**
-	 * Called when the user presses the fast-forward-looking button.
-	 * Requests a 1-frame step forward in the game loop.
-	 */
-	public function onStep():Void
-	{
-		if (!paused)
-		{
-			onPause();
-		}
-		stepRequested = true;
-	}
-	
 	//***EVENT HANDLERS***//
 	
 	/**
 	 * Just sets up basic mouse listeners, a la FlxWindow.
 	 * @param	E	Flash event.
 	 */
-	private function init(E:Event = null):Void
-	{
-		#if flash
-		if (root == null)
-		#else
-		if (stage == null)
-		#end
-		{
-			return;
-		}
-		removeEventListener(Event.ENTER_FRAME, init);
-
-		parent.addEventListener(MouseEvent.MOUSE_MOVE,onMouseMove);
-		parent.addEventListener(MouseEvent.MOUSE_DOWN,onMouseDown);
-		parent.addEventListener(MouseEvent.MOUSE_UP,onMouseUp);
-	}
+	//private function init(E:Event = null):Void
+	//{
+	//	#if flash
+	//	if (root == null)
+	//	#else
+	//	if (stage == null)
+	//	#end
+	//	{
+	//		return;
+	//	}
+	//	removeEventListener(Event.ENTER_FRAME, init);
+	//
+	//	parent.addEventListener(MouseEvent.MOUSE_MOVE,onMouseMove);
+	//	parent.addEventListener(MouseEvent.MOUSE_DOWN,onMouseDown);
+	//	parent.addEventListener(MouseEvent.MOUSE_UP,onMouseUp);
+	//}
 	
 	/**
 	 * If the mouse moves, check to see if any buttons should be highlighted.
 	 * @param	E	Flash mouse event.
 	 */
-	private function onMouseMove(E:MouseEvent = null):Void
-	{
-		if (!checkOver())
-		{
-			unpress();
-		}
-		updateGUI();
-	}
+	//private function onMouseMove(E:MouseEvent = null):Void
+	//{
+	//	if (!checkOver())
+	//	{
+	//		unpress();
+	//	}
+	//	updateGUI();
+	//}
 	
 	/**
 	 * If the mouse is pressed down, check to see if the user started pressing down a specific button.
 	 * @param	E	Flash mouse event.
 	 */
-	private function onMouseDown(E:MouseEvent = null):Void
-	{
-		unpress();
-		if (_overOpen)
-		{
-			_pressingOpen = true;
-		}
-		if (_overRecord)
-		{
-			_pressingRecord = true;
-		}
-		if (_overRestart)
-		{
-			_pressingRestart = true;
-		}
-		if (_overPause)
-		{
-			_pressingPause = true;
-		}
-		if (_overStep)
-		{
-			_pressingStep = true;
-		}
-	}
+	//private function onMouseDown(E:MouseEvent = null):Void
+	//{
+	//	unpress();
+	//	if (_overOpen)
+	//	{
+	//		_pressingOpen = true;
+	//	}
+	//	if (_overRecord)
+	//	{
+	//		_pressingRecord = true;
+	//	}
+	//	if (_overRestart)
+	//	{
+	//		_pressingRestart = true;
+	//	}
+	//	if (_overPause)
+	//	{
+	//		_pressingPause = true;
+	//	}
+	//	if (_overStep)
+	//	{
+	//		_pressingStep = true;
+	//	}
+	//}
 	
 	/**
 	 * If the mouse is released, check to see if it was released over a button that was pressed.
 	 * If it was, take the appropriate action based on button state and visibility.
 	 * @param	E	Flash mouse event.
 	 */
-	private function onMouseUp(E:MouseEvent = null):Void
-	{
-		#if FLX_RECORD
-		if (_overOpen && _pressingOpen)
-		{
-			onOpen();
-		}
-		else if(_overRecord && _pressingRecord)
-		{
-			if (_stop.visible)
-			{
-				onStop();
-			}
-			else if (_recordOn.visible)
-			{
-				stopRecording();
-			}
-			else
-			{
-				onRecord(!E.altKey);
-			}
-		}
-		else 
-		#end
-		if (_overRestart && _pressingRestart)
-		{
-			onRestart(!E.altKey);
-		}
-		else if(_overPause && _pressingPause)
-		{
-			if (_play.visible)
-			{
-				onPlay();
-			}
-			else
-			{
-				onPause();
-			}
-		}
-		else if (_overStep && _pressingStep)
-		{
-			onStep();
-		}
+	//private function onMouseUp(E:MouseEvent = null):Void
+	//{
+	//	#if FLX_RECORD
+	//	if (_overOpen && _pressingOpen)
+	//	{
+	//		onOpen();
+	//	}
+	//	else if(_overRecord && _pressingRecord)
+	//	{
+	//		if (_stop.visible)
+	//		{
+	//			onStop();
+	//		}
+	//		else if (_recordOn.visible)
+	//		{
+	//			stopRecording();
+	//		}
+	//		else
+	//		{
+	//			onRecord(!E.altKey);
+	//		}
+	//	}
+	//	else
+	//	#end
+	//	if (_overRestart && _pressingRestart)
+	//	{
+	//		onRestart(!E.altKey);
+	//	}
+	//	else if(_overPause && _pressingPause)
+	//	{
+	//		if (_play.visible)
+	//		{
+	//			onPlay();
+	//		}
+	//		else
+	//		{
+	//			onPause();
+	//		}
+	//	}
+	//	else if (_overStep && _pressingStep)
+	//	{
+	//		onStep();
+	//	}
 		
-		unpress();
-		checkOver();
-		updateGUI();
-	}
+		//unpress();
+		//checkOver();
+		//updateGUI();
+	//}
 	
 	//***MISC GUI MGMT STUFF***//
 	
@@ -638,106 +527,106 @@ class VCR extends Sprite
 	 * Has some special behavior based on whether a recording is happening or not.
 	 * @return	Whether the mouse was over any buttons or not.
 	 */
-	private function checkOver():Bool
-	{
-		_overOpen = _overRecord = _overRestart = _overPause = _overStep = false;
-		if ((mouseX < 0) || (mouseX > width) || (mouseY < 0) || (mouseY > 15))
-		{
-			return false;
-		}
-		if ((mouseX >= _recordOff.x) && (mouseX <= _recordOff.x + _recordOff.width))
-		{
-			_overRecord = true;
-		}
-		if(!_recordOn.visible && !_overRecord)
-		{
-			if ((mouseX >= _open.x) && (mouseX <= _open.x + _open.width))
-			{
-				_overOpen = true;
-			}
-			else if ((mouseX >= _restart.x) && (mouseX <= _restart.x + _restart.width))
-			{
-				_overRestart = true;
-			}
-			else if ((mouseX >= _pause.x) && (mouseX <= _pause.x + _pause.width))
-			{
-				_overPause = true;
-			}
-			else if ((mouseX >= _step.x) && (mouseX <= _step.x + _step.width))
-			{
-				_overStep = true;
-			}
-		}
-		return true;
-	}
+	//private function checkOver():Bool
+	//{
+	//	_overOpen = _overRecord = _overRestart = _overPause = _overStep = false;
+	//	if ((mouseX < 0) || (mouseX > width) || (mouseY < 0) || (mouseY > 15))
+	//	{
+	//		return false;
+	//	}
+	//	if ((mouseX >= _recordOff.x) && (mouseX <= _recordOff.x + _recordOff.width))
+	//	{
+	//		_overRecord = true;
+	//	}
+	//	if(!_recordOn.visible && !_overRecord)
+	//	{
+	//		if ((mouseX >= _open.x) && (mouseX <= _open.x + _open.width))
+	//		{
+	//			_overOpen = true;
+	//		}
+	//		else if ((mouseX >= _restart.x) && (mouseX <= _restart.x + _restart.width))
+	//		{
+	//			_overRestart = true;
+	//		}
+	//		else if ((mouseX >= _pause.x) && (mouseX <= _pause.x + _pause.width))
+	//		{
+	//			_overPause = true;
+	//		}
+	//		else if ((mouseX >= _step.x) && (mouseX <= _step.x + _step.width))
+	//		{
+	//			_overStep = true;
+	//		}
+	//	}
+	//	return true;
+	//}
 	
 	/**
 	 * Sets all the pressed state variables for the buttons to false.
 	 */
-	private function unpress():Void
-	{
-		_pressingOpen = false;
-		_pressingRecord = false;
-		_pressingRestart = false;
-		_pressingPause = false;
-		_pressingStep = false;
-	}
+	//private function unpress():Void
+	//{
+	//	_pressingOpen = false;
+	//	_pressingRecord = false;
+	//	_pressingRestart = false;
+	//	_pressingPause = false;
+	//	_pressingStep = false;
+	//}
 	
 	/**
 	 * Figures out what buttons to highlight based on the _overWhatever and _pressingWhatever variables.
 	 */
-	private function updateGUI():Void
-	{
-		if(_recordOn.visible)
-		{
-			_open.alpha = _restart.alpha = _pause.alpha = _step.alpha = 0.35;
-			_recordOn.alpha = 1.0;
-			return;
-		}
-		
-		if (_overOpen && (_open.alpha != 1.0))
-		{
-			_open.alpha = 1.0;
-		}
-		else if (!_overOpen && (_open.alpha != 0.8))
-		{
-			_open.alpha = 0.8;
-		}
-		
-		if (_overRecord && (_recordOff.alpha != 1.0))
-		{
-			_recordOff.alpha = _recordOn.alpha = _stop.alpha = 1.0;
-		}
-		else if (!_overRecord && (_recordOff.alpha != 0.8))
-		{
-			_recordOff.alpha = _recordOn.alpha = _stop.alpha = 0.8;
-		}
-		
-		if (_overRestart && (_restart.alpha != 1.0))
-		{
-			_restart.alpha = 1.0;
-		}
-		else if (!_overRestart && (_restart.alpha != 0.8))
-		{
-			_restart.alpha = 0.8;
-		}
-		
-		if (_overPause && (_pause.alpha != 1.0))
-		{
-			_pause.alpha = _play.alpha = 1.0;
-		}
-		else if (!_overPause && (_pause.alpha != 0.8))
-		{
-			_pause.alpha = _play.alpha = 0.8;
-		}
-		
-		if (_overStep && (_step.alpha != 1.0))
-		{
-			_step.alpha = 1.0;
-		}
-		else if (!_overStep && (_step.alpha != 0.8))
-		{
-			_step.alpha = 0.8;
-		}
-	}
+	//private function updateGUI():Void
+	//{
+	//	if(_recordOn.visible)
+	//	{
+	//		_open.alpha = _restart.alpha = _pause.alpha = _step.alpha = 0.35;
+	//		_recordOn.alpha = 1.0;
+	//		return;
+	//	}
+	//
+	//	if (_overOpen && (_open.alpha != 1.0))
+	//	{
+	//		_open.alpha = 1.0;
+	//	}
+	//	else if (!_overOpen && (_open.alpha != 0.8))
+	//	{
+	//		_open.alpha = 0.8;
+	//	}
+	//
+	//	if (_overRecord && (_recordOff.alpha != 1.0))
+	//	{
+	//		_recordOff.alpha = _recordOn.alpha = _stop.alpha = 1.0;
+	//	}
+	//	else if (!_overRecord && (_recordOff.alpha != 0.8))
+	//	{
+	//		_recordOff.alpha = _recordOn.alpha = _stop.alpha = 0.8;
+	//	}
+	//
+	//	if (_overRestart && (_restart.alpha != 1.0))
+	//	{
+	//		_restart.alpha = 1.0;
+	//	}
+	//	else if (!_overRestart && (_restart.alpha != 0.8))
+	//	{
+	//		_restart.alpha = 0.8;
+	//	}
+	//
+	//	if (_overPause && (_pause.alpha != 1.0))
+	//	{
+	//		_pause.alpha = _play.alpha = 1.0;
+	//	}
+	//	else if (!_overPause && (_pause.alpha != 0.8))
+	//	{
+	//		_pause.alpha = _play.alpha = 0.8;
+	//	}
+	//
+	//	if (_overStep && (_step.alpha != 1.0))
+	//	{
+	//		_step.alpha = 1.0;
+	//	}
+	//	else if (!_overStep && (_step.alpha != 0.8))
+	//	{
+	//		_step.alpha = 0.8;
+	//	}
+	//}
 }
---
+++ flixel/system/ui/FlxSystemButton.hx
@@ -0,0 +1,105 @@
+package flixel.system.ui;
+
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.Sprite;
+import flash.events.MouseEvent;
+
+/**
+* A basic button for the debugger, extends flash.display.Sprite
+* Cannot be used in a FlxState
+*
+*/
+class FlxSystemButton extends Sprite
+{
+
+	private var downHandler:Dynamic;
+	private var enabled:Bool = true;
+	private var icon:Bitmap;
+
+	public function new(?Icon:BitmapData, ?DownHandler:Dynamic)
+	{
+		super();
+
+		if(Icon != null)
+		{
+			this.icon = new Bitmap(Icon);
+			addChild(this.icon);
+		}
+
+		if(DownHandler != null)
+		{
+			this.downHandler = DownHandler;
+		}
+
+		addEventListener(MouseEvent.MOUSE_UP,onMouseUp);
+		addEventListener(MouseEvent.MOUSE_OUT,onMouseOut);
+		addEventListener(MouseEvent.MOUSE_OVER,onMouseOver);
+
+		alpha = 0.8;
+	}
+
+	/**
+	* Change the Icon of a button
+	*
+	* @NewIcon:Bitmap pass a new bitmap for the icon to be changed
+	*
+	**/
+	public function changeIcon (NewIcon:BitmapData):Void
+	{
+		if (this.icon != null)
+			removeChild(this.icon);
+
+		this.icon = null;
+
+		this.icon = new Bitmap(NewIcon);
+		addChild(this.icon);
+	}
+
+	/**
+	* Change the Button action Handler
+	*
+	* @NewHandler:Dynamic the new function to change for the button's action
+	*
+	**/
+	public function changeHandler (NewHandler:Dynamic):Void
+	{
+		this.downHandler = null;
+		this.downHandler = NewHandler;
+	}
+
+	public function enable():Void
+	{
+		enabled = true;
+	}
+
+	public function disable():Void
+	{
+		enabled = false;
+	}
+
+	private function onMouseUp(E:MouseEvent = null):Void
+	{
+		if (downHandler != null && enabled)
+			Reflect.callMethod (this, downHandler, []);
+	}
+
+	private function onMouseOver(E:MouseEvent = null):Void
+	{
+		alpha = 1;
+	}
+
+	private function onMouseOut(E:MouseEvent = null):Void
+	{
+		alpha = 0.8;
+	}
+
+	public function destroy():Void
+	{
+		removeEventListener(MouseEvent.MOUSE_OUT,onMouseOut);
+		removeEventListener(MouseEvent.MOUSE_OVER,onMouseOver);
+		removeEventListener(MouseEvent.MOUSE_UP,onMouseUp);
+		icon = null;
+		downHandler = null;
+	}
+}
