f519820a6d87ccf56f6acc5d18478ad166bc77dd
Samuel Batista <sambatista1@gmail.com>
2013-11-18 16:38:29 -0500

    Update FlxG.hx

--- flixel/FlxG.hx
+++ flixel/FlxG.hx
@@ -64,7 +64,7 @@ class FlxG
 	 * Assign a minor version to your library.
 	 * Appears after the decimal in the console.
 	 */
-	static public var LIBRARY_MINOR_VERSION:String = "0.0-dev";
+	static public var LIBRARY_MINOR_VERSION:String = "0.1-dev";
 	
 	/**
 	 * Internal tracker for game object.
