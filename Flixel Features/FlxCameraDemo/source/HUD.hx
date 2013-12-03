711cc9663de0feff4008fb4c04d7d656cf6c35aa
ProG4mr <prog4mr@gmail.com>
2013-12-03 10:20:48 +0000

    FlxCameraDemo added, unused assets removed from flxNapeDemo.

---
+++ Flixel Features/FlxCameraDemo/source/HUD.hx
@@ -0,0 +1,69 @@
+package ;
+import flixel.FlxG;
+import flixel.group.FlxGroup;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+//import pgr.gconsole.GameConsole;
+
+/**
+ * @author TiagoLr ( ~~~ProG4mr~~~ )
+ */
+
+class HUD extends FlxGroup
+{
+	private var txtStyle:FlxText;
+	private var txtLerp:FlxText;
+	private var txtLead:FlxText;
+	private var txtZoom:FlxText;
+	public var background:FlxSprite;
+
+	public function new() 
+	{
+		super();
+		
+		background = new FlxSprite(10000 - 50, -175, null);
+		background.makeGraphic(300, 360, 0xFF000000);
+		add(background);
+		
+		add(new FlxText(10010, 10, 200 - 10, "Use [W,A,S,D] to control the orb.\n\n[Y] or [H] to change camera style.\n\n\n\n[U] or [J] to change lerp.\n\n\n\n[I] or [K] to change lead.\n\n\n\n[O] or [L] to change zoom.")); 
+		
+		txtStyle = new FlxText(10010, 10 + 30 + 3, 190,  "STYLE_LOCKON");
+		txtStyle.setFormat(null, 11, 0x55FF55, "left");
+		add(txtStyle);
+		
+		txtLerp = new FlxText(10010, 50 + 30, 300,  "Camera lerp: 0");
+		txtLerp.setFormat(null, 11, 0x55FF55, "left");
+		add(txtLerp);
+		
+		txtLead = new FlxText(10010, 90 + 30, 300,  "Camera lead: 0");
+		txtLead.setFormat(null, 11, 0x55FF55, "left");
+		add(txtLead);
+
+		txtZoom = new FlxText(10010, 130 + 30, 300,  "Camera zoom: 1");
+		txtZoom.setFormat(null, 11, 0x55FF55, "left");
+		add(txtZoom);
+		
+	}
+	
+	public function updateStyle(string:String) 
+	{
+		txtStyle.text = string;
+	}
+	
+	public function updateCamLerp(lerp:Float) 
+	{
+		txtLerp.text = "Camera lerp: " + lerp;
+	}
+	
+	public function updateCamLead(lead:Float) 
+	{
+		txtLead.text = "Camera lead: " + lead;
+	}
+	
+	public function updateZoom(zoom:Float) 
+	{
+		//GameConsole.log("zoom  " + zoom);
+		txtZoom.text = "Camera Zoom: " + Math.floor(zoom * 10) / 10;
+	}
+	
+}
---
+++ Flixel Features/FlxCameraDemo/source/Orb.hx
@@ -0,0 +1,38 @@
+package ;
+import flixel.addons.nape.FlxNapeSprite;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import openfl.Assets;
+
+/**
+ * @author TiagoLr ( ~~~ProG4mr~~~ )
+ */
+
+class Orb extends FlxNapeSprite
+{
+
+	public var shadow:FlxSprite;
+	
+	public function new ()
+	{
+		super(FlxG.width / 2, FlxG.height / 2, Assets.getBitmapData("assets/Orb.png"));
+		createCircularBody(18);
+		body.allowRotation = false;
+		setDrag(0.98, 1);
+	}
+	
+	override public function update():Void 
+	{
+		super.update();
+		
+		if (FlxG.camera.target != null && FlxG.camera.followLead.x == 0) // target check is used for debug purposes.
+		{
+			x = Math.round(x); // Smooths camera and orb shadow following. Does not work well with camera lead.
+			y = Math.round(y); // Smooths camera and orb shadow following. Does not work well with camera lead.
+		}
+		
+		shadow.x = Math.round(x);
+		shadow.y = Math.round(y);
+	}
+	
+}
