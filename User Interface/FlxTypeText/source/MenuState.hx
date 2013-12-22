f8420f707c5c7a19f97b720adc7637a6c774c6f6
steverichey <stevenpatrickrichey@gmail.com>
2013-12-22 15:36:25 -0600

    some small visual tweaks

--- User Interface/FlxTypeText/source/MenuState.hx
+++ User Interface/FlxTypeText/source/MenuState.hx
@@ -2,6 +2,7 @@ package;
 
 import flash.display.BitmapData;
 import flash.geom.Matrix;
+import flash.geom.Rectangle;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -36,7 +37,7 @@ class MenuState extends FlxState
 		var square:FlxSprite = new FlxSprite( 10, 10 );
 		square.makeGraphic( FlxG.width - 20, FlxG.height - 76, 0xff333333 );
 		
-		_typeText = new FlxTypeText( 10, 10, FlxG.width - 20, "Hello, and welcome to the FlxTypeText demo. You can press the buttons below and see the different ways to control this class. Enjoy! :)", 16, true );
+		_typeText = new FlxTypeText( 15, 10, FlxG.width - 30, "Hello, and welcome to the FlxTypeText demo. You can press the buttons below and see the different ways to control this class. Enjoy! :)", 16, true );
 		
 		_typeText.delay = 0.1;
 		_typeText.eraseDelay = 0.2;
@@ -50,7 +51,7 @@ class MenuState extends FlxState
 		_typeText.color = 0x8811EE11;
 		_typeText.setSkipKeys( [ "SPACE" ] );
 		
-		_status = new FlxTypeText( 10, FlxG.height - 92, FlxG.width - 20, "None", 16 );
+		_status = new FlxTypeText( 15, FlxG.height - 102, FlxG.width - 20, "None", 16 );
 		_status.color = 0x8800AA00;
 		_status.prefix = "Status: ";
 		
@@ -64,6 +65,21 @@ class MenuState extends FlxState
 			{
 				bitmapdata.draw( scanline, new Matrix( 1, 0, 0, 1, 0, i ) );
 			}
+		}
+		
+		// round corners
+		
+		var cX:Array<Int> = [ 5, 3, 2, 2, 1 ];
+		var cY:Array<Int> = [ 1, 2, 2, 3, 5 ];
+		var w:Int = bitmapdata.width;
+		var h:Int = bitmapdata.height;
+		
+		for ( i in 0...5 )
+		{
+			bitmapdata.fillRect( new Rectangle( 0, 0, cX[i], cY[i] ), 0xff131c1b );
+			bitmapdata.fillRect( new Rectangle( w-cX[i], 0, cX[i], cY[i] ), 0xff131c1b );
+			bitmapdata.fillRect( new Rectangle( 0, h-cY[i], cX[i], cY[i] ), 0xff131c1b );
+			bitmapdata.fillRect( new Rectangle( w-cX[i], h-cY[i], cX[i], cY[i] ), 0xff131c1b );
 		}
 		
 		effect.loadGraphic( bitmapdata );
