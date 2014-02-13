75363c7089567808be49a1f9f860cd373c66c992
Gama11 <jensfischer95@gmail.com>
2014-02-13 13:20:41 +0100

    FlxState: small cleanup
    - removed _subState by making subState (default, null)
    - made tryUpdate() private
    - made requestedSubState and reqeustSubStateReset private

--- flixel/FlxState.hx
+++ flixel/FlxState.hx
@@ -11,37 +11,14 @@ class FlxState extends FlxGroup
 	/**
 	* Determines whether or not this state is updated even when it is not the active state. For example, if you have your game state first, and then you push a menu state on top of it,
 	* if this is set to true, the game state would continue to update in the background. By default this is false, so background states will be "paused" when they are not active.
-	* @default false
 	*/
 	public var persistentUpdate:Bool = false;
 
 	/**
 	* Determines whether or not this state is updated even when it is not the active state. For example, if you have your game state first, and then you push a menu state on top of it, if this is set to true, the game state would continue to be drawn behind the pause state.
 	* By default this is true, so background states will continue to be drawn behind the current state. If background states are not visible when you have a different state on top, you should set this to false for improved performance.
-	* @default true
 	*/
 	public var persistentDraw:Bool = true;
-
-	private var _subState:FlxSubState;
-	/**
-	 * Current substate.
-	 * Substates also can have substates
-	 */
-	public var subState(get, null):FlxSubState;
-
-	private inline function get_subState():FlxSubState
-	{
-		return _subState;
-	}
-	/**
-	 * If a state change was requested, the new state object is stored here until we switch to it.
-	 */
-	public var requestedSubState:FlxSubState = null;
-
-	/**
-	 * Whether to reset the substate (when it changes, or when it's closed).
-	 */
-	public var requestSubStateReset:Bool = false;
 
 	/**
 	 * If substates get destroyed when they are closed, setting this to false might reduce state creation time, at greater memory cost.
@@ -49,16 +26,21 @@ class FlxState extends FlxGroup
 	public var destroySubStates:Bool = true;
 	
 	public var bgColor(get, set):Int;
+	
+	/**
+	 * Current substate. Substates also can be nested.
+	 */
+	public var subState(default, null):FlxSubState;
+	
+	/**
+	 * If a state change was requested, the new state object is stored here until we switch to it.
+	 */
+	private var _requestedSubState:FlxSubState;
 
-	private function get_bgColor():Int
-	{
-		return FlxG.cameras.bgColor;
-	}
-
-	private function set_bgColor(Value:Int):Int
-	{
-		return FlxG.cameras.bgColor = Value;
-	}
+	/**
+	 * Whether to reset the substate (when it changes, or when it's closed).
+	 */
+	private var _requestSubStateReset:Bool = false;
 
 	/**
 	 * This function is called after the game engine successfully switches states. Override this function, NOT the constructor, to initialize or set up your game state.
@@ -68,58 +50,41 @@ class FlxState extends FlxGroup
 
 	override public function draw():Void
 	{
-		if (persistentDraw || _subState == null)
+		if (persistentDraw || subState == null)
 		{
 			super.draw();
 		}
 		
-		if (_subState != null)
+		if (subState != null)
 		{
-			_subState.draw();
+			subState.draw();
 		}
 	}
 
-#if !FLX_NO_DEBUG
+	#if !FLX_NO_DEBUG
 	override public function drawDebug():Void
 	{
-		if (persistentDraw || _subState == null)
+		if (persistentDraw || subState == null)
 		{
 			super.drawDebug();
 		}
 		
-		if (_subState != null)
+		if (subState != null)
 		{
-			_subState.drawDebug();
+			subState.drawDebug();
 		}
 	}
-#end
-
-	public function tryUpdate():Void
-	{
-		if (persistentUpdate || _subState == null)
-		{
-			update();
-		}
-		
-		if (requestSubStateReset)
-		{
-			resetSubState();
-			requestSubStateReset = false;
-		}
-		else if(_subState != null)
-		{
-			_subState.tryUpdate();
-		}
-	}
+	#end
 
 	/**
 	 * Manually close the sub-state
 	 */
 	public inline function setSubState(subState:FlxSubState):Void
 	{
-		requestedSubState = subState;
-		requestSubStateReset = true;
+		_requestedSubState = subState;
+		_requestSubStateReset = true;
 	}
+	
 	/**
 	 * Manually close the sub-state
 	 */
@@ -134,25 +99,25 @@ class FlxState extends FlxGroup
 	public function resetSubState():Void
 	{
 		// Close the old state (if there is an old state)
-		if(_subState != null)
+		if(subState != null)
 		{
-			if (_subState.closeCallback != null)
+			if (subState.closeCallback != null)
 			{
-				_subState.closeCallback();
+				subState.closeCallback();
 			}
 			if (destroySubStates)
 			{
-				_subState.destroy();
+				subState.destroy();
 			}
 		}
 		
 		// Assign the requested state (or set it to null)
-		_subState = requestedSubState;
+		subState = _requestedSubState;
 		
-		if (_subState != null)
+		if (subState != null)
 		{
 			// I'm just copying the code from "FlxGame::switchState" which doesn't check for already craeted states. :/
-			_subState._parentState = this;
+			subState._parentState = this;
 			
 			//Reset the input so things like "justPressed" won't interfere
 			if (!persistentUpdate)
@@ -160,20 +125,20 @@ class FlxState extends FlxGroup
 				FlxG.inputs.reset();
 			}
 			
-			if (!_subState.initialized)
+			if (!subState.initialized)
 			{
-				_subState.initialize();
- 				_subState.create();
+				subState.initialize();
+ 				subState.create();
 			}
 		}
 	}
 
 	override public function destroy():Void
 	{
-		if (_subState != null)
+		if (subState != null)
 		{
-			_subState.destroy();
-			_subState = null;
+			subState.destroy();
+			subState = null;
 		}
 		super.destroy();
 	}
@@ -182,18 +147,48 @@ class FlxState extends FlxGroup
 	 * This method is called after application losts its focus.
 	 * Can be useful if you using third part libraries, such as tweening engines.
 	 */
-	public function onFocusLost():Void { }
+	public function onFocusLost():Void {}
 
 	/**
 	 * This method is called after application gets focus.
 	 * Can be useful if you using third part libraries, such as tweening engines.
 	 */
-	public function onFocus():Void { }
+	public function onFocus():Void {}
 
 	/**
 	 * This function is called whenever the window size has been changed.
+	 * 
 	 * @param 	Width	The new window width
 	 * @param 	Height	The new window Height
 	 */  
-	public function onResize(Width:Int, Height:Int):Void { }
+	public function onResize(Width:Int, Height:Int):Void {}
+	
+	@:allow(flixel.FlxGame)
+	private function tryUpdate():Void
+	{
+		if (persistentUpdate || (subState == null))
+		{
+			update();
+		}
+		
+		if (_requestSubStateReset)
+		{
+			resetSubState();
+			_requestSubStateReset = false;
+		}
+		else if (subState != null)
+		{
+			subState.tryUpdate();
+		}
+	}
+	
+	private function get_bgColor():Int
+	{
+		return FlxG.cameras.bgColor;
+	}
+
+	private function set_bgColor(Value:Int):Int
+	{
+		return FlxG.cameras.bgColor = Value;
+	}
 }
