28181c8700deb3fb7bc42794c6e0018bfc5b28dd
Lars Doucet <lars.doucet@gmail.com>
2014-02-17 13:33:37 -0600

    BlendModes: simplified to show all blend modes at once

--- Effects/BlendModes/source/PlayState.hx
+++ Effects/BlendModes/source/PlayState.hx
@@ -22,65 +22,79 @@ class PlayState extends FlxUIState
 	private var _bottom:FlxSprite;
 	private var _top:FlxSprite;
 	
-	private var _dd_blends:FlxUIDropDownMenu;
 	private var _step_alpha:FlxUINumericStepper;
+	
+	private var _bottoms:Map<String,FlxSprite>;
+	private var _tops:Map<String,FlxSprite>;
+	
+	private var _list_blends:Array<String> = null;
 	
 	override public function create():Void
 	{
 		super.create();
+		
+		_list_blends = ["normal", "darken", "multiply", "lighten", "screen", "overlay", "hardlight", "difference", "add", "subtract", "invert"];
+		
 		var bkg:FlxSprite = new FlxSprite(0, 0);
 		bkg.makeGraphic(FlxG.width,FlxG.height,0xFF808080);
 		add(bkg);
 		
-		_bottom = new FlxSprite(0, 0, "assets/bottom.png");
-		_top = new FlxSprite(0, 0, "assets/top.png");
-		
-		_bottom.x = (FlxG.width - _bottom.width) - 10;
-		_bottom.y = (FlxG.height - _bottom.height) - 10;
-		
-		_top.x = _bottom.x;
-		_top.y = _bottom.y;
-		
-		var outline = new FlxSprite(0, 0);
-		outline.makeGraphic(cast _top.width + 2,cast _top.height + 2, FlxColor.BLACK);
-		
-		outline.x = _top.x - 1;
-		outline.y = _top.y - 1;
-		
-		add(outline);
-		add(_bottom);
-		add(_top);
-		
-		var list_blends:Array<StrIdLabel> = 
-		[
-			new StrIdLabel("normal", "Normal"),
-			new StrIdLabel("darken", "Darken"),
-			new StrIdLabel("multiply", "Multiply"),
-			new StrIdLabel("lighten", "Lighten"),
-			new StrIdLabel("screen", "Screen"),
-			new StrIdLabel("overlay", "Overlay"),
-			new StrIdLabel("hardlight", "Hard Light"),
-			new StrIdLabel("difference", "Difference"),
-			new StrIdLabel("add", "Add"),
-			new StrIdLabel("subtract", "Subtract"),
-			new StrIdLabel("invert", "Invert")
-		];
-		
-		var label_alpha:FlxText = new FlxText(5, 10, 50, "Alpha:");
+		var label_alpha:FlxText = new FlxText(2, 2, 50, "Alpha:");
 		label_alpha.color = 0xFFFFFF;
 		label_alpha.setBorderStyle(FlxText.BORDER_OUTLINE_FAST);
 		add(label_alpha);
 		
-		_step_alpha = new FlxUINumericStepper(40, 10, 0.01, 1.00, 0.00, 1.00, 2);
+		_step_alpha = new FlxUINumericStepper(40, 2, 0.01, 1.00, 0.00, 1.00, 2);
 		add(_step_alpha);
 		
-		var label_blends:FlxText = new FlxText(5, 45, 50, "Blend:");
-		label_blends.color = 0xFFFFFF;
-		label_blends.setBorderStyle(FlxText.BORDER_OUTLINE_FAST);
-		add(label_blends);
+		makeBlendImages();
+	}
+
+	private function makeBlendImages():Void {
+		var xx:Int = 100;
+		var yy:Int = 12;
+		var column:Int = 0;
+		var row:Int = 0;
 		
-		_dd_blends = new FlxUIDropDownMenu(40, 40, list_blends, onClickBlend);
-		add(_dd_blends);
+		_bottoms = new Map<String,FlxSprite>();
+		_tops = new Map<String,FlxSprite>();
+		
+		for (str in _list_blends) {
+			xx = 10 + column * 250;
+			yy = 30 + row * 250;
+			
+			var top:FlxSprite = new FlxSprite(0, 0, "assets/top_small.png");
+			var bottom:FlxSprite = new FlxSprite(0, 0, "assets/bottom_small.png");
+			
+			var outline = new FlxSprite();
+			outline.makeGraphic(cast top.width + 2, cast top.height + 2, FlxColor.BLACK);
+			
+			add(outline);
+			add(bottom);
+			add(top);
+			
+			outline.x = cast xx - 1;
+			outline.y = cast yy - 1;
+			top.x = bottom.x = xx;
+			top.y = bottom.y = yy;
+			
+			top.blend = getBlend(str);
+			
+			_tops.set(str, top);
+			_bottoms.set(str, top);
+			
+			var label:FlxText = new FlxText(top.x, top.y, cast top.width, str);
+			label.y -= label.height;
+			label.color = 0xffffff;
+			label.setBorderStyle(FlxText.BORDER_OUTLINE_FAST);
+			add(label);
+			
+			column++;
+			if (column >= 4) {
+				column = 0;
+				row++;
+			}
+		}
 	}
 	
 	private function onClickBlend(str:String):Void {
@@ -105,9 +119,12 @@ class PlayState extends FlxUIState
 	}
 	
 	public override function getEvent(event:String,sender:IFlxUIWidget,data:Dynamic,?params:Dynamic):Void {
-		if(event == FlxUINumericStepper.CLICK_EVENT){
+		if(event == FlxUINumericStepper.CHANGE_EVENT){
 			if (sender == _step_alpha) {
-				_top.alpha = cast data;
+				for (str in _list_blends) {
+					var top = _tops.get(str);
+					top.alpha = cast data;
+				}
 			}
 		}
 	}
