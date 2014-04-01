d7f04f136909505b8d00322a70ef963f7017fc41
Gama11 <jensfischer95@gmail.com>
2014-04-01 13:27:23 +0200

    FlxWaveSprite demo: cleanup

--- Effects/FlxWaveSprite/source/PlayState.hx
+++ Effects/FlxWaveSprite/source/PlayState.hx
@@ -1,171 +1,110 @@
 package;
 
+import flixel.addons.effects.FlxWaveSprite;
 import flixel.FlxG;
-import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.input.touch.FlxTouch;
 import flixel.text.FlxText;
-import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
-import flixel.util.FlxSpriteUtil;
-import flixel.addons.effects.FlxWaveSprite;
+import flixel.system.FlxAssets;
+using flixel.util.FlxSpriteUtil;
+using StringTools;
 
 class PlayState extends FlxState
 {
-
-	private var _wavLogo:FlxWaveSprite;
+	private static inline var INSTRUCTIONS = #if !mobile
+	                                         "Space to Cycle Modes\n" +
+	                                         "Left/Right to adjust strength\n" +
+	                                         "Up/Down to adjust center\n" +
+	                                         "W/S to adjust speed"; 
+	                                         #else
+	                                         "Touch to Cycle Modes"
+	                                         #end
+											 
+	private static inline var STATUS = "Mode: [mode]    Strength: [strength]    Center: [center]    Speed: [speed]";
 	
-	private var modes:Array<String>;
-	private var mode:Int = 0;
-	private var strength:Int = 20;
-	private var center:Int = 0;
-	private var speed:Float = 4;
-	
-	private var _txtMode:FlxText;
-	private var _txtStr:FlxText;
-	private var _txtCenter:FlxText;
-	private var _txtSpeed:FlxText;
+	private var _waveSprite:FlxWaveSprite;
+	private var _statusText:FlxText;
 	
 	override public function create():Void
 	{
-		modes =  ["ALL", "BOTTOM", "TOP"];
-		var _sprLogo:FlxSprite = new FlxSprite(0, 0, "assets/images/HaxeFlixel.png");
-		FlxSpriteUtil.screenCenter(_sprLogo);
+		var _sprite = new FlxSprite(0, 0, GraphicLogo);
+		_sprite.screenCenter();
 		
-		strength = 20;
-		mode = 0;
+		_waveSprite = new FlxWaveSprite(_sprite);
+		add(_waveSprite);
 		
-		_wavLogo = new FlxWaveSprite(_sprLogo, FlxWaveSprite.MODE_ALL, strength);
-		add(_wavLogo);
-		center = Std.int( _wavLogo.height / 2);
-		_wavLogo.center = center;
-		
-		var _txtInstruct:FlxText;
-		
-		#if !FLX_NO_KEYBOARD
-		_txtInstruct = new FlxText(0, 0, FlxG.width, "Space to Cycle Modes", 8);
+		var _txtInstruct:FlxText = new FlxText(0, 0, FlxG.width, INSTRUCTIONS);
 		_txtInstruct.alignment = "center";
 		add(_txtInstruct);
-		_txtInstruct = new FlxText(0, _txtInstruct.y+_txtInstruct.height-4, FlxG.width, "Left/Right to adjust strength", 8);
-		_txtInstruct.alignment = "center";
-		add(_txtInstruct);
-		_txtInstruct = new FlxText(0, _txtInstruct.y+_txtInstruct.height-4, FlxG.width, "Up/Down to adjust center", 8);
-		_txtInstruct.alignment = "center";
-		add(_txtInstruct);
-		_txtInstruct = new FlxText(0, _txtInstruct.y+_txtInstruct.height-4, FlxG.width, "W/S to adjust speed", 8);
-		_txtInstruct.alignment = "center";
-		add(_txtInstruct);
-		#else
-		_txtInstruct = new FlxText(0, 0, FlxG.width, "Touch to Cycle Modes", 8);
-		_txtInstruct.alignment = "center";
-		add(_txtInstruct);
-		#end
 		
-		var w:Int = Std.int((FlxG.width) / 4);
-		var a:Array<FlxObject> = [];
-		
-		_txtMode = new FlxText(0, 0, w, "Mode: " + modes[mode]);
-		_txtMode.alignment = "center";
-		add(_txtMode);
-		a.push(_txtMode);
-		
-		_txtStr = new FlxText(0, 0, w, "Strength: " + Std.string(strength));
-		_txtStr.alignment = "center";
-		add(_txtStr);
-		a.push(_txtStr);
-		
-		_txtCenter = new FlxText(0, 0, w, "Center: " + Std.string(center));
-		_txtCenter.alignment = "center";
-		add(_txtCenter);
-		a.push(_txtCenter);
-		
-		_txtSpeed = new FlxText(0, 0, w, "Speed: " + Std.string(speed));
-		_txtSpeed.alignment = "center";
-		add(_txtSpeed);
-		a.push(_txtSpeed);
-		
-		FlxSpriteUtil.space(a, 0, Std.int(FlxG.height - _txtSpeed.height),w);
+		_statusText = new FlxText(0, FlxG.height - 15, FlxG.width);
+		_statusText.alignment = "center";
+		add(_statusText);
 
 		super.create();
-		
 	}
 	
 	override public function update():Void
 	{
 		#if !FLX_NO_KEYBOARD
-		if (FlxG.keys.anyJustReleased(["SPACE"]))
-		{
-			mode++;
-			if (mode > 2)
-				mode = 0;
-			_txtMode.text = "Mode: " + modes[mode];
-			_wavLogo.mode = mode;
-		}
-		if (FlxG.keys.anyPressed(["RIGHT"]))
-		{
-			strength += 5;
-			if (strength > 500)
-				strength = 500;
-			_txtStr.text = "Strength: " + Std.string(strength);
-			_wavLogo.strength = strength;
-		}
-		if (FlxG.keys.anyPressed(["LEFT"]))
-		{
-			strength -= 5;
-			if (strength < 0)
-				strength = 0;
-			_txtStr.text = "Strength: " + Std.string(strength);
-			_wavLogo.strength = strength;
-		}
+		if (FlxG.keys.justReleased.SPACE)
+			incrementMode();
+	
+		// control center
+		if (FlxG.keys.pressed.UP)
+			_waveSprite.center++;
+		if (FlxG.keys.pressed.DOWN)
+			_waveSprite.center--;
+			
+		// control strength
+		if (FlxG.keys.pressed.LEFT)
+			_waveSprite.strength -= 5;
+		if (FlxG.keys.pressed.RIGHT)
+			_waveSprite.strength += 5;
 		
-		if (FlxG.keys.anyPressed(["UP"]))
-		{
-			center--;
-			if (center < 0)
-				center = 0;
-			_txtCenter.text = "Center: " + Std.string(center);
-			_wavLogo.center = center;
-		}
-		if (FlxG.keys.anyPressed(["DOWN"]))
-		{
-			center++;
-			if (center > _wavLogo.height)
-				center = Std.int(_wavLogo.height);
-			_txtCenter.text = "Center: " + Std.string(center);
-			_wavLogo.center = center;
-		}
-		if (FlxG.keys.anyPressed(["W"]))
-		{
-			speed++;
-			if (speed > 80)
-				speed = 80;
-			_txtSpeed.text = "Speed: " + Std.string(speed);
-			_wavLogo.speed = speed;
-		}
-		if (FlxG.keys.anyPressed(["S"]))
-		{
-			speed--;
-			if (speed < 0)
-				speed = 0;
-			_txtSpeed.text = "Speed: " + Std.string(speed);
-			_wavLogo.speed = speed;
-		}
+		// control speed
+		if (FlxG.keys.pressed.W)
+			_waveSprite.speed++;
+		if (FlxG.keys.pressed.S)
+			_waveSprite.speed--;
 		#end
 		
 		#if !FLX_NO_TOUCH
 		if (FlxG.touches.justStarted().length > 0)
-		{
-			mode++;
-			if (mode > 2)
-				mode = 0;
-			_txtMode.text = "Mode: " + modes[mode];
-			_wavLogo.mode = mode;
-		}
+			incrementMode();
 		#end
 		
+		boundValues();
+		updateStatusText();
 		super.update();
-		
-		
-	}	
+	}
+	
+	private function boundValues():Void
+	{
+		_waveSprite.center = Std.int(FlxMath.bound(_waveSprite.center, 0, _waveSprite.height));
+		_waveSprite.strength = Std.int(FlxMath.bound(_waveSprite.strength, 0, 500));
+		_waveSprite.speed = FlxMath.bound(_waveSprite.speed, 0, 80);
+	}
+	
+	private function incrementMode():Void
+	{
+		switch (_waveSprite.mode)
+		{
+			case FlxWaveSprite.MODE_ALL:
+				_waveSprite.mode = FlxWaveSprite.MODE_BOTTOM;
+			case FlxWaveSprite.MODE_BOTTOM:
+				_waveSprite.mode = FlxWaveSprite.MODE_TOP;
+			case FlxWaveSprite.MODE_TOP:
+				_waveSprite.mode = FlxWaveSprite.MODE_ALL;
+		}
+	}
+	
+	private function updateStatusText():Void 
+	{
+		_statusText.text = STATUS.replace("[mode]", Std.string(_waveSprite.mode))
+		                         .replace("[strength]", Std.string(_waveSprite.strength))
+		                         .replace("[center]",  Std.string(_waveSprite.center))
+		                         .replace("[speed]",  Std.string(_waveSprite.speed));
+	}
 }
