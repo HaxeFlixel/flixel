0493f87e80c024488364909814aa709950961c61
Gama11 <jensfischer95@gmail.com>
2014-04-08 10:14:53 +0200

    FlxSprite: move centerOrigin() next to centerOffsets()

--- flixel/FlxSprite.hx
+++ flixel/FlxSprite.hx
@@ -347,6 +347,7 @@ class FlxSprite extends FlxObject
 	/**
 	 * Create a pre-rotated sprite sheet from a simple sprite.
 	 * This can make a huge difference in graphical performance!
+	 * 
 	 * @param	Graphic			The image you want to rotate and stamp.
 	 * @param	Rotations		The number of rotation frames the final sprite should have.  For small sprites this can be quite a large number (360 even) without any problems.
 	 * @param	Frame			If the Graphic has a single row of square animation frames on it, you can specify which of the frames you want to use here.  Default is -1, or "use whole graphic."
@@ -497,11 +498,11 @@ class FlxSprite extends FlxObject
 	
 	/**
 	 * Loads TexturePacker atlas.
+	 * 
 	 * @param	Data		Atlas data holding links to json-data and atlas image
 	 * @param	Reverse		Whether you need this class to generate horizontally flipped versions of the animation frames. 
 	 * @param	Unique		Optional, whether the graphic should be a unique instance in the graphics cache.  Default is false.
 	 * @param	FrameName	Default frame to show. If null then will be used first available frame.
-	 * 
 	 * @return This FlxSprite instance (nice for chaining stuff together, if you're into that).
 	 */
 	public function loadGraphicFromTexture(Data:Dynamic, Reverse:Bool = false, Unique:Bool = false, ?FrameName:String):FlxSprite
@@ -549,12 +550,12 @@ class FlxSprite extends FlxObject
 	/**
 	 * Creates a pre-rotated sprite sheet from provided image in atlas.
 	 * This can make a huge difference in graphical performance on flash target!
+	 * 
 	 * @param	Data			Atlas data holding links to json-data and atlas image
 	 * @param	Image			The image from atlas you want to rotate and stamp.
 	 * @param	Rotations		The number of rotation frames the final sprite should have.  For small sprites this can be quite a large number (360 even) without any problems.
 	 * @param	AntiAliasing	Whether to use high quality rotations when creating the graphic.  Default is false.
 	 * @param	AutoBuffer		Whether to automatically increase the image size to accomodate rotated corners.
-	 * 
 	 * @return This FlxSprite instance (nice for chaining stuff together, if you're into that).
 	 */
 	public function loadRotatedGraphicFromTexture(Data:Dynamic, Image:String, Rotations:Int = 16, AntiAliasing:Bool = false, AutoBuffer:Bool = false):FlxSprite
@@ -630,15 +631,6 @@ class FlxSprite extends FlxObject
 	{
 		width = frameWidth;
 		height = frameHeight;
-	}
-	
-	/**
-	 * Sets the sprite's origin to its center - useful after adjusting 
-	 * scale to make sure rotations work as expected.
-	 */
-	public inline function centerOrigin():Void
-	{
-		origin.set(frameWidth * 0.5, frameHeight * 0.5);
 	}
 	
 	/**
@@ -982,6 +974,15 @@ class FlxSprite extends FlxObject
 			x += offset.x;
 			y += offset.y;
 		}
+	}
+
+	/**
+	 * Sets the sprite's origin to its center - useful after adjusting 
+	 * scale to make sure rotations work as expected.
+	 */
+	public inline function centerOrigin():Void
+	{
+		origin.set(frameWidth * 0.5, frameHeight * 0.5);
 	}
 	
 	/**
