7baeabcf856a8b4975451846a8b96e79831e6758
Eiyeron FULMICENDII <eiyeron@gmail.com>
2014-04-15 22:35:57 +0200

    Listened Gama about enums hints and tips. Camera enums now in the same file than FlxCamera and removed redundant prefixes.

--- flixel/system/frontEnds/CameraFrontEnd.hx
+++ flixel/system/frontEnds/CameraFrontEnd.hx
@@ -150,9 +150,9 @@ class CameraFrontEnd
 	 * @param	Duration	The length in seconds that the shaking effect should last.
 	 * @param	OnComplete	A function you want to run when the shake effect finishes.
 	 * @param	Force		Force the effect to reset (default = true, unlike flash() and fade()!).
-	 * @param	Direction	Whether to shake on both axes, just up and down, or just side to side (use FlxCameraShakeMode's class constants SHAKE_BOTH_AXES, SHAKE_VERTICAL_ONLY, or SHAKE_HORIZONTAL_ONLY).  Default value is SHAKE_BOTH_AXES (0).
+	 * @param	Direction	Whether to shake on both axes, just up and down, or just side to side (use FlxCameraShakeMode's class constants BOTH_AXES, VERTICAL_ONLY, or HORIZONTAL_ONLY).  Default value is BOTH_AXES (0).
 	 */
-	public function shake(Intensity:Float = 0.05, Duration:Float = 0.5, ?OnComplete:Void->Void, Force:Bool = true, Direction:FlxCameraShakeMode = FlxCameraShakeMode.SHAKE_BOTH_AXES):Void
+	public function shake(Intensity:Float = 0.05, Duration:Float = 0.5, ?OnComplete:Void->Void, Force:Bool = true, Direction:FlxCameraShakeMode = FlxCameraShakeMode.BOTH_AXES):Void
 	{
 		for (camera in list)
 		{
