fcb3fc09d981e23abfe6c98f4016941cdbcd4a01
Chris <impaler777@gmail.com>
2014-04-19 11:44:24 +1000

    Update Card.hx
    Add missing line break and quick comment.

--- Input/MouseEventManager/source/Card.hx
+++ Input/MouseEventManager/source/Card.hx
@@ -91,6 +91,8 @@ class Card extends FlxNapeSprite
 		// Finish the card animation
 		FlxTween.tween(scale, { x: 1 }, TURNING_TIME / 2);
 	}
+	
+	// Make sure that this object is removed from the MouseEventManger for gc
 	override public function destroy():Void {
 		MouseEventManager.remove(this);
 		super.destroy();
