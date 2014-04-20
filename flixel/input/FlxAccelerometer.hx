f30c3cc1000940e16091f7c75b0b966e66b5e197
KeyMaster <AccessKeyMaster@gmx.net>
2014-04-20 23:36:24 +0200

    Added documentation

--- flixel/input/FlxAccelerometer.hx
+++ flixel/input/FlxAccelerometer.hx
@@ -4,11 +4,29 @@ package flixel.system.input;
 import flash.events.AccelerometerEvent;
 import flash.sensors.Accelerometer;
 
+/**
+ * A class providing access to the accelerometer data of the mobile device.
+ */
 class FlxAccelerometer {
+	
+	/**
+	 * The x-axis value, in G (Multiples of 9.8m/s/s), can take any value
+	 */
 	public var x(default, null):Float = 0;
+	/**
+	 * The y-axis value, in G (Multiples of 9.8m/s/s), can take any value
+	 */
 	public var y(default, null):Float = 0;
+	/**
+	 * The z-axis value, in G (Multiples of 9.8m/s/s), can take any value
+	 */
 	public var z(default, null):Float = 0;
+	
+	/**
+	 * Wether the accelerometer is supported on this mobile device
+	 */
 	public var isSupported(get, never):Bool;
+	
 	private var sensor:Accelerometer;
 	
 	public function new() 
