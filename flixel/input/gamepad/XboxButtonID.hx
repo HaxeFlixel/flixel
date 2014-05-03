55b0c788f16b9080da75a6ed1f2a1f713939a665
Gama11 <jensfischer95@gmail.com>
2014-05-03 15:07:34 +0200

    XboxButtonID cleanup

--- flixel/input/gamepad/XboxButtonID.hx
+++ flixel/input/gamepad/XboxButtonID.hx
@@ -6,6 +6,9 @@ package flixel.input.gamepad;
 class XboxButtonID
 {
 #if flash
+	/**
+	 * Button IDs
+	 */
 	public static inline var A:Int = 4;
 	public static inline var B:Int = 5;
 	public static inline var X:Int = 6;
@@ -21,18 +24,21 @@ class XboxButtonID
 	public static inline var LEFT_ANALOGUE:Int = 14;
 	public static inline var RIGHT_ANALOGUE:Int = 15;
 	
-	public static inline var LEFT_ANALOGUE_X:Int = 0;
-	public static inline var LEFT_ANALOGUE_Y:Int = 1;
-	public static inline var RIGHT_ANALOGUE_X:Int = 2;
-	public static inline var RIGHT_ANALOGUE_Y:Int = 3;
-	
 	public static inline var DPAD_UP:Int = 16;
 	public static inline var DPAD_DOWN:Int = 17;
 	public static inline var DPAD_LEFT:Int = 18;
 	public static inline var DPAD_RIGHT:Int = 19;
+	
+	/**
+	 * Axis array indicies
+	 */
+	public static inline var LEFT_ANALOGUE_X:Int = 0;
+	public static inline var LEFT_ANALOGUE_Y:Int = 1;
+	public static inline var RIGHT_ANALOGUE_X:Int = 2;
+	public static inline var RIGHT_ANALOGUE_Y:Int = 3;
 #elseif mac
 	/**
-	 * Button IDs (DPAD values are obtained from FlxGamepad.hat)
+	 * Button IDs
 	 */
 	public static inline var A:Int = 0;
 	public static inline var B:Int = 1;
@@ -44,8 +50,14 @@ class XboxButtonID
 	public static inline var START:Int = 8;
 	public static inline var LEFT_ANALOGUE:Int = 6;
 	public static inline var RIGHT_ANALOGUE:Int = 7;
+	
 	public static inline var XBOX:Int = 10;
 
+	public static inline var DPAD_UP:Int = 11;
+	public static inline var DPAD_DOWN:Int = 12;
+	public static inline var DPAD_LEFT:Int = 13;
+	public static inline var DPAD_RIGHT:Int = 14;
+	
 	/**
 	 * Axis array indicies
 	 */
@@ -55,14 +67,9 @@ class XboxButtonID
 	public static inline var RIGHT_ANALOGUE_Y:Int = 4;
 	public static inline var LEFT_TRIGGER:Int = 2;
 	public static inline var RIGHT_TRIGGER:Int = 5;
-
-	public static inline var DPAD_UP:Int = 11;
-	public static inline var DPAD_DOWN:Int = 12;
-	public static inline var DPAD_LEFT:Int = 13;
-	public static inline var DPAD_RIGHT:Int = 14;
 #elseif linux
 	/**
-	 * Button IDs (DPAD values are obtained from FlxGamepad.hat)
+	 * Button IDs
 	 */
 	public static inline var A:Int = 0;
 	public static inline var B:Int = 1;
@@ -74,7 +81,13 @@ class XboxButtonID
 	public static inline var START:Int = 7;
 	public static inline var LEFT_ANALOGUE:Int = 9;
 	public static inline var RIGHT_ANALOGUE:Int = 10;
+	
 	public static inline var XBOX:Int = 8;
+	
+	public static inline var DPAD_UP:Int = 13;
+	public static inline var DPAD_DOWN:Int = 14;
+	public static inline var DPAD_LEFT:Int = 11;
+	public static inline var DPAD_RIGHT:Int = 12;
 	
 	/**
 	 * Axis array indicies
@@ -85,14 +98,9 @@ class XboxButtonID
 	public static inline var RIGHT_ANALOGUE_Y:Int = 4;
 	public static inline var LEFT_TRIGGER:Int = 2;
 	public static inline var RIGHT_TRIGGER:Int = 5;
-
-	public static inline var DPAD_UP:Int = 13;
-	public static inline var DPAD_DOWN:Int = 14;
-	public static inline var DPAD_LEFT:Int = 11;
-	public static inline var DPAD_RIGHT:Int = 12;
 #else // windows
 	/**
-	 * Button IDs (DPAD values are obtained from FlxGamepad.hat)
+	 * Button IDs
 	 */
 	public static inline var A:Int = 10;
 	public static inline var B:Int = 11;
@@ -107,6 +115,11 @@ class XboxButtonID
 	
 	public static inline var XBOX:Int = 14;
 	
+	public static inline var DPAD_UP:Int = 0;
+	public static inline var DPAD_DOWN:Int = 1;
+	public static inline var DPAD_LEFT:Int = 2;
+	public static inline var DPAD_RIGHT:Int = 3;
+	
 	/**
 	 * Axis array indicies
 	 */
@@ -116,10 +129,5 @@ class XboxButtonID
 	public static inline var RIGHT_ANALOGUE_Y:Int = 3;
 	public static inline var LEFT_TRIGGER:Int = 4;
 	public static inline var RIGHT_TRIGGER:Int = 5;
-	
-	public static inline var DPAD_UP:Int = 0;
-	public static inline var DPAD_DOWN:Int = 1;
-	public static inline var DPAD_LEFT:Int = 2;
-	public static inline var DPAD_RIGHT:Int = 3;
 #end
 }
