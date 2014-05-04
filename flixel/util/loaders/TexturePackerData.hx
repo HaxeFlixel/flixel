b8231b3e5ef5c1645975960cf646552cfbf5829b
Gama11 <jensfischer95@gmail.com>
2014-05-04 17:55:37 +0200

    Use FlxDestroyUtil in TexturePackerData.destroy(), #1048

--- flixel/util/loaders/TexturePackerData.hx
+++ flixel/util/loaders/TexturePackerData.hx
@@ -77,14 +77,9 @@ class TexturePackerData implements IFlxDestroyable
 	 */
 	public function destroy():Void
 	{
-		if (frames != null) {
-			for (frame in frames)
-			{	
-				frame = FlxDestroyUtil.destroy(frame);
-			}
-		}
-		frames = null;
+		frames = FlxDestroyUtil.destroyArray(frames);
 		assetName = null;
+		description = null;
 		asset = null;
 	}
 }
