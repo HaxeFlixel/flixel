161a5f215be5a37ca6795c9e4ebc5c90fcbb71e6
Gama11 <jensfischer95@gmail.com>
2014-05-08 16:17:33 +0200

    Slightly more accurate docs on FlxSpriteGroup

--- flixel/group/FlxSpriteGroup.hx
+++ flixel/group/FlxSpriteGroup.hx
@@ -3,8 +3,9 @@ package flixel.group;
 import flixel.FlxSprite;
 
 /**
- * FlxSpriteGroup is a special FlxGroup that can be treated like 
+ * FlxSpriteGroup is a special FlxSprite that can be treated like 
  * a single sprite even if it's made up of several member sprites.
+ * It shares the FlxTypedGroup API, but it doesn't inherit from it.
  */
 class FlxSpriteGroup extends FlxTypedSpriteGroup<FlxSprite>
 {
