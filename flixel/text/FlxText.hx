eb3e1ee8fbb0fae93a26a0f4b7321fde7c907ac9
Joe Williamson <huruey@gmail.com>
2014-05-11 14:17:34 +0100

    Comment change

--- flixel/text/FlxText.hx
+++ flixel/text/FlxText.hx
@@ -75,7 +75,7 @@ class FlxText extends FlxSprite
 	public var alignment(get, set):String;
 	
 	/**
-	 * Use a border style like FlxTextBorderStyle.SHADOW or FlxTextBorderStyle.OUTLINE
+	 * Use a border style
 	 */	
 	public var borderStyle(default, set):FlxTextBorderStyle = NONE;
 	
