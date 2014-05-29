HaxeFlixel/flixel

commit 3f0b0aaf4207dbe71d9d9ea53781063be9acc832

FlxBaseTilemap: fix for invalid DrawIndex with auto-tiling on cpp targets

flixel/tile/FlxBaseTilemap.hx
@@ -167,6 +167,13 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 		auto = (AutoTile == null) ? OFF : AutoTile;
 		_startingIndex = (StartingIndex <= 0) ? 0 : StartingIndex;
 
+		if (auto != OFF)
+		{
+			_startingIndex = 1;
+			DrawIndex = 1;
+			CollideIndex = 1;
+		}
+		
 		loadMapData(MapData);
 		applyAutoTile(DrawIndex, CollideIndex);
 		applyCustomRemap();
@@ -259,17 +266,9 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 	{
 		// Pre-process the map data if it's auto-tiled
 		var i:Int = 0;
-		
-		if (auto != OFF)
+		while (i < totalTiles)
 		{
-			_startingIndex = 1;
-			DrawIndex = 1;
-			CollideIndex = 1;
-			
-			while (i < totalTiles)
-			{
-				autoTile(i++);
-			}
+			autoTile(i++);
 		}
 	}
 	

commit e97a0ba036b2a89fc5c91b2957fe162a1645dd3b

Fix autotiling bug introduced in 3f0b0aaf4207dbe71d9d9ea53781063be9acc832

flixel/tile/FlxBaseTilemap.hx
@@ -265,10 +265,13 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 	private function applyAutoTile(DrawIndex:Int, CollideIndex:Int):Void	
 	{
 		// Pre-process the map data if it's auto-tiled
-		var i:Int = 0;
-		while (i < totalTiles)
+		if (auto != OFF)
 		{
-			autoTile(i++);
+			var i:Int = 0;
+			while (i < totalTiles)
+			{
+				autoTile(i++);
+			}
 		}
 	}
 	