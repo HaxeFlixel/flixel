81eaad679e822efe821642dc3cb7b49aa942ecb4
Gama11 <jensfischer95@gmail.com>
2014-05-31 11:30:00 +0200

    NeonVector compile fix

--- Performance/NeonVector/source/GameSound.hx
+++ Performance/NeonVector/source/GameSound.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.math.FlxRandom;
 
 /**
  * @author Masadow
@@ -14,7 +15,6 @@ class GameSound
 	public static var sfxShoot:Array<String> = ["sounds/shoot-01.mp3", "sounds/shoot-02.mp3", "sounds/shoot-03.mp3", "sounds/shoot-04.mp3"];
 			
 	public static var sfxSpawn:Array<String> = [
-			"sounds/spawn-01.mp3", "sounds/spawn-02.mp3", "sounds/spawn-03.mp3", "sounds/spawn-04.mp3", 
 			"sounds/spawn-05.mp3", "sounds/spawn-06.mp3", "sounds/spawn-07.mp3", "sounds/spawn-08.mp3"];
 			
 	public static function create():Void
