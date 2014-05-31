e7ab2b8bced3e68f34f32ad1995f4cf87427c74e
Gama11 <jensfischer95@gmail.com>
2014-05-31 16:42:37 +0200

    FlxTeroids: use space to start the game
    forcing the player to use the mouse in a game that is entirely keyboard-based is a horrible idea...

--- Arcade Classics/FlxTeroids/source/MenuState.hx
+++ Arcade Classics/FlxTeroids/source/MenuState.hx
@@ -20,14 +20,14 @@ class MenuState extends FlxState
 		t.setFormat(null, 32, FlxColor.WHITE, CENTER, OUTLINE);
 		add(t);
 		
-		t = new FlxText(0, FlxG.height - 30, FlxG.width, "click to play");
+		t = new FlxText(0, FlxG.height - 30, FlxG.width, "space to play");
 		t.setFormat(null, 16, FlxColor.WHITE, CENTER, OUTLINE);
 		add(t);
 	}
 	
 	override public function update():Void 
 	{
-		if (FlxG.mouse.justPressed)
+		if (FlxG.keys.justReleased.SPACE)
 		{
 			FlxG.switchState(new PlayState());
 		}
