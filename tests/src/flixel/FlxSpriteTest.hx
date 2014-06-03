HaxeFlixel/flixel

commit 4a8af9ecd55c1cbd812e47ee8ebaa007b2e4db3d

Fix issue with FlxButton status animations + loadGraphicFromSprite()

flixel/FlxSprite.hx
@@ -284,10 +284,12 @@ class FlxSprite extends FlxObject
 			centerOffsets();
 		}
 		
-		updateFrameData();
-		resetHelpers();
 		antialiasing = Sprite.antialiasing;
 		animation.copyFrom(Sprite.animation);
+		
+		updateFrameData();
+		resetHelpers();
+		
 		return this;
 	}
 	
@@ -653,7 +655,8 @@ class FlxSprite extends FlxObject
 	 */
 	public function setGraphicSize(Width:Int = 0, Height:Int = 0):Void
 	{
-		if (Width <= 0 && Height <= 0) {
+		if (Width <= 0 && Height <= 0)
+		{
 			return;
 		}
 		
@@ -661,10 +664,12 @@ class FlxSprite extends FlxObject
 		var newScaleY:Float = Height / frameHeight;
 		scale.set(newScaleX, newScaleY);
 		
-		if (Width <= 0) {
+		if (Width <= 0)
+		{
 			scale.x = newScaleY;
 		}
-		else if (Height <= 0) {
+		else if (Height <= 0)
+		{
 			scale.y = newScaleX;
 		}	
 	}

tests/src/TestSuite.hx
@@ -8,6 +8,7 @@ import flixel.FlxStateTest;
 import flixel.group.FlxGroupTest;
 import flixel.math.FlxVectorTest;
 import flixel.tweens.FlxTweenTest;
+import flixel.ui.FlxButtonTest;
 import flixel.util.FlxSignalTest;
 import FlxTest;
 
@@ -31,6 +32,7 @@ class TestSuite extends massive.munit.TestSuite
 		add(flixel.group.FlxGroupTest);
 		add(flixel.math.FlxVectorTest);
 		add(flixel.tweens.FlxTweenTest);
+		add(flixel.ui.FlxButtonTest);
 		add(flixel.util.FlxSignalTest);
 		add(FlxTest);
 	}

tests/src/flixel/ui/FlxButtonTest.hx
@@ -1,0 +1,55 @@
+package flixel.ui;
+
+import flash.display.BitmapData;
+import flixel.FlxSprite;
+import flixel.ui.FlxButton;
+import massive.munit.Assert;
+
+class FlxButtonTest
+{
+	var button:FlxButton;
+	
+	@Before
+	function before():Void
+	{
+		button = new FlxButton();
+	}
+	
+	@Test
+	function testDefaultStatusAnimations():Void
+	{
+		assertStatusAnimationsExist();
+	}
+	
+	@Test
+	function testLoadGraphicStatusAnimations():Void
+	{
+		var graphic = new BitmapData(3, 1);
+		button.loadGraphic(graphic, true, 1, 1);
+		
+		assertStatusAnimationsExist();
+	}
+	
+	@Test
+	function testLoadGraphicFromSpriteStatusAnimations():Void
+	{
+		var sprite = new FlxSprite();
+		var graphic = new BitmapData(3, 1);
+		sprite.loadGraphic(graphic, true, 1, 1);
+		
+		button.loadGraphicFromSprite(sprite);
+		
+		assertStatusAnimationsExist();
+	}
+	
+	function assertStatusAnimationsExist():Void
+	{
+		var normalName:String = button.statusAnimations[FlxButton.NORMAL];
+		var highlightName:String = button.statusAnimations[FlxButton.HIGHLIGHT];
+		var pressedName:String = button.statusAnimations[FlxButton.PRESSED];
+		
+		Assert.isNotNull(button.animation.getByName(normalName));
+		Assert.isNotNull(button.animation.getByName(highlightName));
+		Assert.isNotNull(button.animation.getByName(pressedName));
+	}
+}

commit 89251db4482dbdfe9ebdda48e2f5f18f3569faea

FlxSprite: added graphicLoaded() callback function
- called whenever a graphic is loaded via any of the loadGraphic() functions
- helpful for FlxButton animation setup + potential other scenarios
- fixes bug with copying animations via loadGraphicFromSprite() introduced in 4a8af9ecd55c1cbd812e47ee8ebaa007b2e4db3d

flixel/FlxSprite.hx
@@ -279,12 +279,13 @@ class FlxSprite extends FlxObject
 			centerOffsets();
 		}
 		
-		antialiasing = Sprite.antialiasing;
-		animation.copyFrom(Sprite.animation);
-		
 		updateFrameData();
 		resetHelpers();
 		
+		antialiasing = Sprite.antialiasing;
+		animation.copyFrom(Sprite.animation);
+		
+		graphicLoaded();
 		return this;
 	}
 	
@@ -345,6 +346,7 @@ class FlxSprite extends FlxObject
 		updateFrameData();
 		resetHelpers();
 		
+		graphicLoaded();
 		return this;
 	}
 	
@@ -497,6 +499,8 @@ class FlxSprite extends FlxObject
 		
 		animation.createPrerotated();
 		resetHelpers();
+		
+		graphicLoaded();
 		return this;
 	}
 	
@@ -546,6 +550,8 @@ class FlxSprite extends FlxObject
 		
 		resetSizeFromFrame();
 		centerOrigin();
+		
+		graphicLoaded();
 		return this;
 	}
 	
@@ -583,6 +589,7 @@ class FlxSprite extends FlxObject
 		loadRotatedGraphic(frameBitmapData, Rotations, -1, AntiAliasing, AutoBuffer, key);
 		#end
 		
+		graphicLoaded();
 		return this;
 	}
 	
@@ -608,8 +615,16 @@ class FlxSprite extends FlxObject
 		animation.destroyAnimations();
 		updateFrameData();
 		resetHelpers();
+		
+		graphicLoaded();
 		return this;
 	}
+	
+	/**
+	 * Called whenever a new graphic is loaded for this sprite
+	 * - after loadGraphic(), makeGraphic() etc.
+	 */
+	public function graphicLoaded():Void {}
 	
 	/**
 	 * Resets _flashRect variable used for frame bitmapData calculation

flixel/input/FlxInput.hx
@@ -1,5 +1,4 @@
 package flixel.input;
-import flixel.input.FlxInput.FlxInputState;
 
 class FlxInput<T>
 {

flixel/ui/FlxButton.hx
@@ -191,9 +191,9 @@ class FlxTypedButton<T:FlxSprite> extends FlxSprite
 		input = new FlxInput(0);
 	}
 	
-	override public function updateFrameData():Void
+	override public function graphicLoaded():Void
 	{
-		super.updateFrameData();
+		super.graphicLoaded();
 		
 		setupAnimation("normal", FlxButton.NORMAL);
 		setupAnimation("highlight", FlxButton.HIGHLIGHT);

tests/src/flixel/FlxSpriteTest.hx
@@ -1,6 +1,7 @@
 package flixel;
 
 import flash.display.BitmapData;
+import flixel.animation.FlxAnimation;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxArrayUtil;
@@ -193,5 +194,18 @@ class FlxSpriteTest extends FlxTest
 		delay(this, factory, function() { 
 			Assert.isFalse(FlxG.overlap(sprite1, sprite2)); 
 		});
+	}
+	
+	@Test
+	function testLoadGraphicFromSpriteCopyAnimations():Void
+	{
+		var graphic = new BitmapData(3, 1);
+		sprite1.loadGraphic(graphic, true, 1, 1);
+		sprite1.animation.add("animation", [0, 1, 2]);
+		
+		sprite2.loadGraphicFromSprite(sprite1);
+		
+		var animation:FlxAnimation = sprite2.animation.getByName("animation");
+		Assert.areEqual(3, animation.numFrames);
 	}
 }