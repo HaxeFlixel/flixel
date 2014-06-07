HaxeFlixel/flixel

commit 567ac4494fce45db1200219ad7cff2083a9f3390

Improved the behaviour of FlxText#font, #1152
- now correctly initialized with FlxAssets.FONT_DEFAULT instead of null
- now supports fonts that were not embedded via openfl.Assets
- text.font = null now sets it back to the default font

flixel/text/FlxText.hx
@@ -171,7 +171,8 @@ class FlxText extends FlxSprite
 		_textField.selectable = false;
 		_textField.multiline = true;
 		_textField.wordWrap = true;
-		_defaultFormat = new TextFormat(FlxAssets.FONT_DEFAULT, Size, 0xffffff);
+		_defaultFormat = new TextFormat(null, Size, 0xffffff);
+		font = FlxAssets.FONT_DEFAULT;
 		_formatAdjusted = new TextFormat();
 		_textField.defaultTextFormat = _defaultFormat;
 		_textField.text = Text;
@@ -302,14 +303,7 @@ class FlxText extends FlxSprite
 		
 		if (Embedded)
 		{
-			if (Font == null)
-			{
-				_defaultFormat.font = FlxAssets.FONT_DEFAULT;
-			}
-			else 
-			{
-				_defaultFormat.font = Assets.getFont(Font).fontName;
-			}
+			font = Font;
 		}
 		else if (Font != null)
 		{
@@ -508,14 +502,26 @@ class FlxText extends FlxSprite
 	
 	private function set_font(Font:String):String
 	{
+		_textField.embedFonts = true;
+		
 		if (Font != null)
 		{
-			_textField.embedFonts = true;
-			_defaultFormat.font = Assets.getFont(Font).fontName;
-			_textField.defaultTextFormat = _defaultFormat;
-			updateFormat(_defaultFormat);
-			dirty = true;
+			var newFontName:String = Font;
+			if (Assets.exists(Font, AssetType.FONT))
+			{
+				newFontName = Assets.getFont(Font).fontName;
+			}
+			
+			_defaultFormat.font = newFontName;
 		}
+		else
+		{
+			_defaultFormat.font = font = FlxAssets.FONT_DEFAULT;
+		}
+		
+		_textField.defaultTextFormat = _defaultFormat;
+		updateFormat(_defaultFormat);
+		dirty = true;
 		return font = Font;
 	}
 	

tests/src/flixel/text/FlxTextTest.hx
@@ -1,0 +1,21 @@
+package flixel.text;
+
+import flixel.text.FlxText;
+import massive.munit.Assert;
+
+class FlxTextTest extends FlxTest
+{
+	var text:FlxText;
+	
+	@Before
+	function before():Void
+	{
+		text = new FlxText();
+	}
+	
+	@Test
+	function testFontDefaultValue():Void
+	{
+		Assert.areEqual(text.font, "Nokia Cellphone FC Small");
+	}
+}

flixel/tile/FlxTilemapBuffer.hx
@@ -62,13 +62,14 @@ class FlxTilemapBuffer
 	/**
 	 * Instantiates a new camera-specific buffer for storing the visual tilemap data.
 	 * 
-	 * @param	TileWidth		The width of the tiles in this tilemap.
-	 * @param	TileHeight	The height of the tiles in this tilemap.
-	 * @param	WidthInTiles	How many tiles wide the tilemap is.
-	 * @param	HeightInTiles	How many tiles tall the tilemap is.
-	 * @param	Camera		Which camera this buffer relates to.
+	 * @param   TileWidth       The width of the tiles in this tilemap.
+	 * @param   TileHeight      The height of the tiles in this tilemap.
+	 * @param   WidthInTiles    How many tiles wide the tilemap is.
+	 * @param   HeightInTiles   How many tiles tall the tilemap is.
+	 * @param   Camera          Which camera this buffer relates to.
 	 */
-	public function new(TileWidth:Int, TileHeight:Int, WidthInTiles:Int, HeightInTiles:Int, Camera:FlxCamera = null, ScaleX:Float = 1.0, ScaleY:Float = 1.0)
+	public function new(TileWidth:Int, TileHeight:Int, WidthInTiles:Int, HeightInTiles:Int,
+		?Camera:FlxCamera,ScaleX:Float = 1.0, ScaleY:Float = 1.0)
 	{
 		updateColumns(TileWidth, WidthInTiles, ScaleX, Camera);
 		updateRows(TileHeight, HeightInTiles, ScaleY, Camera);

tests/src/TestSuite.hx
@@ -7,6 +7,7 @@ import flixel.FlxSpriteTest;
 import flixel.FlxStateTest;
 import flixel.group.FlxGroupTest;
 import flixel.math.FlxVectorTest;
+import flixel.text.FlxTextTest;
 import flixel.tile.FlxTilemapTest;
 import flixel.tweens.FlxTweenTest;
 import flixel.ui.FlxButtonTest;
@@ -32,6 +33,7 @@ class TestSuite extends massive.munit.TestSuite
 		add(flixel.FlxStateTest);
 		add(flixel.group.FlxGroupTest);
 		add(flixel.math.FlxVectorTest);
+		add(flixel.text.FlxTextTest);
 		add(flixel.tile.FlxTilemapTest);
 		add(flixel.tweens.FlxTweenTest);
 		add(flixel.ui.FlxButtonTest);

commit fd6c6d312c7968aa0eb0610dd575bb427738036d

Fix issue with FlxText#destroy()
introduced in 567ac4494fce45db1200219ad7cff2083a9f3390

flixel/text/FlxText.hx
@@ -39,7 +39,7 @@ class FlxText extends FlxSprite
 	/**
 	 * The font used for this text (assuming that it's using embedded font).
 	 */
-	public var font(default, set):String;
+	public var font(get, set):String;
 	
 	/**
 	 * Whether this text field uses an embedded font (by default) or not. 
@@ -138,6 +138,8 @@ class FlxText extends FlxSprite
 	private var _widthInc:Int = 0;
 	private var _heightInc:Int = 0;
 	
+	private var _font:String;
+	
 	/**
 	 * Creates a new FlxText object at the specified position.
 	 * 
@@ -217,7 +219,7 @@ class FlxText extends FlxSprite
 	override public function destroy():Void
 	{
 		_textField = null;
-		font = null;
+		_font = null;
 		_defaultFormat = null;
 		_formatAdjusted = null;
 		_filters = null;
@@ -501,6 +503,11 @@ class FlxText extends FlxSprite
 		return Color;
 	}
 	
+	private inline function get_font():String
+	{
+		return _font;
+	}
+	
 	private function set_font(Font:String):String
 	{
 		_textField.embedFonts = true;
@@ -517,13 +524,13 @@ class FlxText extends FlxSprite
 		}
 		else
 		{
-			_defaultFormat.font = font = FlxAssets.FONT_DEFAULT;
+			_defaultFormat.font = FlxAssets.FONT_DEFAULT;
 		}
 		
 		_textField.defaultTextFormat = _defaultFormat;
 		updateFormat(_defaultFormat);
 		dirty = true;
-		return font = Font;
+		return _font = _defaultFormat.font;
 	}
 	
 	private inline function get_embedded():Bool

tests/src/flixel/text/FlxTextTest.hx
@@ -1,5 +1,6 @@
 package flixel.text;
 
+import flash.errors.Error;
 import flixel.text.FlxText;
 import massive.munit.Assert;
 
@@ -17,5 +18,19 @@ class FlxTextTest extends FlxTest
 	function testFontDefaultValue():Void
 	{
 		Assert.areEqual(text.font, "Nokia Cellphone FC Small");
+	}
+	
+	@Test
+	function testDestroy():Void
+	{
+		try
+		{
+			text.destroy();
+			text.destroy();
+		}
+		catch (e:Error)
+		{
+			Assert.fail(e.message);
+		}
 	}
 }