290a141365d022f58953e4d580fa42db35825a1c
steverichey <stevenpatrickrichey@gmail.com>
2014-06-30 10:33:30 -0500

    FlxTypeText Demo: Update for sounds change

--- User Interface/FlxTypeText/source/MenuState.hx
+++ User Interface/FlxTypeText/source/MenuState.hx
@@ -42,9 +42,14 @@ class MenuState extends FlxState
 		_typeText.autoErase = true;
 		_typeText.waitTime = 2.0;
 		_typeText.setTypingVariation(0.75, true);
-		_typeText.useDefaultSound = true;
 		_typeText.color = 0x8811EE11;
 		_typeText.skipKeys = ["SPACE"];
+		
+		#if flash
+		_typeText.sounds = [FlxG.sound.load("assets/type01.mp3"), FlxG.sound.load("assets/type02.mp3")];
+		#else
+		_typeText.sounds = [FlxG.sound.load("assets/type01.ogg"), FlxG.sound.load("assets/type02.ogg")];
+		#end
 		
 		_status = new FlxTypeText(15, FlxG.height - 102, FlxG.width - 20, "None", 16);
 		_status.color = 0x8800AA00;
@@ -99,7 +104,7 @@ class MenuState extends FlxState
 	
 	private function startCallback():Void
 	{
-		_typeText.start(0.02, false, false, null, null, onComplete.bind("Fully typed"));
+		_typeText.start(0.02, false, false, null, onComplete.bind("Fully typed"));
 	}
 	
 	private function pauseCallback():Void
@@ -109,12 +114,12 @@ class MenuState extends FlxState
 	
 	private function eraseCallback():Void
 	{
-		_typeText.erase(0.01, false, null, null, onComplete.bind("Fully erased"));
+		_typeText.erase(0.01, false, null, onComplete.bind("Fully erased"));
 	}
 	
 	private function forceStartCallback():Void
 	{
-		_typeText.start(0.03, true, true, null, null, onComplete.bind("Typed, erasing..."));
+		_typeText.start(0.03, true, true, null, onComplete.bind("Typed, erasing..."));
 	}
 	
 	private function cursorCallback():Void
@@ -124,7 +129,7 @@ class MenuState extends FlxState
 	
 	private function forceEraseCallback():Void
 	{
-		_typeText.erase(0.02, true, null, null, onComplete.bind("Erased"));
+		_typeText.erase(0.02, true, null, onComplete.bind("Erased"));
 	}
 	
 	private function onComplete(Text:String):Void
