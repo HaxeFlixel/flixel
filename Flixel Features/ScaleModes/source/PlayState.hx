b9e7d16b0c53ca98491bd57ec54f78e40a50df64
Gama11 <jensfischer95@gmail.com>
2014-07-10 13:52:58 +0200

    ScaleMode: update for HaxeFlixel/flixel@79f464a146f07db1f7eb64591df3f27e4c85dfee + cleanup

--- Flixel Features/ScaleModes/source/PlayState.hx
+++ Flixel Features/ScaleModes/source/PlayState.hx
@@ -3,32 +3,23 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.group.FlxGroup;
+import flixel.math.FlxMath;
+import flixel.math.FlxRandom;
 import flixel.system.scaleModes.FillScaleMode;
 import flixel.system.scaleModes.FixedScaleMode;
 import flixel.system.scaleModes.RatioScaleMode;
 import flixel.system.scaleModes.RelativeScaleMode;
 import flixel.text.FlxText;
-import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
-import flixel.math.FlxRandom;
 
 class PlayState extends FlxState
 {
 	private var currentPolicy:FlxText;
-	
-	private var fill:FillScaleMode;
-	private var ratio:RatioScaleMode;
-	private var relative:RelativeScaleMode;
-	private var fixed:FixedScaleMode;
+	private var scaleModes:Array<ScaleMode> = [RATIO_DEFAULT, RATIO_FILL_SCREEN, FIXED, RELATIVE, FILL];
+	private var scaleModeIndex:Int = 0;
 	
 	override public function create():Void
 	{
-		fill = new FillScaleMode();
-		ratio = new RatioScaleMode();
-		relative = new RelativeScaleMode(0.75, 0.75);
-		fixed = new FixedScaleMode();
-		
 		add(new FlxSprite(0, 0, "assets/bg.png"));
 		
 		for (i in 0...20)
@@ -36,13 +27,12 @@ class PlayState extends FlxState
 			add(new Ship(FlxRandom.int(50, 100), FlxRandom.int(0, 360)));
 		}
 		
-		FlxG.scaleMode = ratio;
-		currentPolicy = new FlxText(0, 10, FlxG.width, "ratio");
+		currentPolicy = new FlxText(0, 10, FlxG.width, ScaleMode.RATIO_DEFAULT);
 		currentPolicy.alignment = CENTER;
 		currentPolicy.size = 16;
 		add(currentPolicy);
 		
-		var info:FlxText = new FlxText(0, FlxG.height - 40, FlxG.width, "press space or click to change the scale mode");
+		var info:FlxText = new FlxText(0, FlxG.height - 40, FlxG.width, "Press space or click to change the scale mode");
 		info.setFormat(null, 14, FlxColor.WHITE, CENTER);
 		info.alpha = 0.75;
 		add(info);
@@ -52,26 +42,43 @@ class PlayState extends FlxState
 	{
 		if (FlxG.keys.justPressed.SPACE || FlxG.mouse.justPressed)
 		{
-			switch (currentPolicy.text)
-			{
-				case "fill":
-					FlxG.scaleMode = ratio;
-					currentPolicy.text = "ratio";
-				
-				case "ratio":
-					FlxG.scaleMode = fixed;
-					currentPolicy.text = "fixed";
-					
-				case "fixed":
-					FlxG.scaleMode = relative;
-					currentPolicy.text = "relative 75%";
-				
-				default:
-					FlxG.scaleMode = fill;
-					currentPolicy.text = "fill";
-			}
+			scaleModeIndex = FlxMath.wrapValue(scaleModeIndex, 1, scaleModes.length);
+			setScaleMode(scaleModes[scaleModeIndex]);
 		}
 		
 		super.update();
 	}
+	
+	private function setScaleMode(scaleMode:ScaleMode)
+	{
+		currentPolicy.text = scaleMode;
+		
+		FlxG.scaleMode = switch (scaleMode)
+		{
+			case ScaleMode.RATIO_DEFAULT:
+				new RatioScaleMode();
+				
+			case ScaleMode.RATIO_FILL_SCREEN:
+				new RatioScaleMode(true);
+				
+			case ScaleMode.FIXED:
+				new FixedScaleMode();
+				
+			case ScaleMode.RELATIVE:
+				new RelativeScaleMode(0.75, 0.75);
+				
+			case ScaleMode.FILL:
+				new FillScaleMode();
+		}
+	}
+}
+
+@:enum
+abstract ScaleMode(String) to String
+{
+	var RATIO_DEFAULT = "ratio";
+	var RATIO_FILL_SCREEN = "ratio (screenfill)";
+	var FIXED = "fixed";
+	var RELATIVE = "relatvive 75%";
+	var FILL = "fill";
 }
