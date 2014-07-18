98cf17e9eb935229015f0436d39fcc59cb0cdf78
Gama11 <jensfischer95@gmail.com>
2014-07-18 22:39:32 +0200

    FlxTextFormat: add an example for applyMarkup() (HaxeFlixel/flixel#1229)

--- User Interface/FlxTextFormat/source/PlayState.hx
+++ User Interface/FlxTextFormat/source/PlayState.hx
@@ -6,29 +6,44 @@ import flixel.text.FlxText;
 import flixel.util.FlxColor;
 using flixel.util.FlxSpriteUtil;
 
-/**
- * A FlxState which can be used for the actual gameplay.
- */
 class PlayState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
 	override public function create():Void
 	{
 		super.create();
 		
-		var text = new FlxText(0, 60, FlxG.width, "Hello HaxeFlixel Community!");
-		text.setFormat(null, 48, FlxColor.BLACK, CENTER);
+		var format1 = new FlxTextFormat(0xE6E600, false, false, 0xFF8000);
+		var format2 = new FlxTextFormat(0xFFAE5E, false, false, 0xFF8000);
+		var format3 = new FlxTextFormat(0x008040, false, false, null);
+		var format4 = new FlxTextFormat(0x0080C0, false, false, null);
+		var format5 = new FlxTextFormat(0x00E6E6, false, false, null);
+		var format6 = new FlxTextFormat(0x0080FF, false, false, 0xFFFFFF);
+		
+		var text1:FlxText = createText(40, "Formatted via addFormat()");
+		text1.addFormat(format1, 0, 5);
+		text1.addFormat(format2, 5, 10);
+		text1.addFormat(format3, 10, 11);
+		text1.addFormat(format4, 11, 12);
+		text1.addFormat(format5, 12, 13);
+		text1.addFormat(format6, 17, 100);
+		add(text1);
+		
+		var text2:FlxText = createText(120);
+		text2.applyMarkup("*Forma*_tted_ $v$§i§!a! apply:Markup():", [
+			new FlxTextFormatMarkerPair(format1, "*"),
+			new FlxTextFormatMarkerPair(format2, "_"),
+			new FlxTextFormatMarkerPair(format3, "$"),
+			new FlxTextFormatMarkerPair(format4, "§"),
+			new FlxTextFormatMarkerPair(format5, "!"),
+			new FlxTextFormatMarkerPair(format6, ":")]);
+		add(text2);
+	}
+	
+	function createText(y:Float, ?text:String)
+	{
+		var text = new FlxText(60, y, FlxG.width, text);
+		text.setFormat(null, 24, FlxColor.BLACK);
 		text.setBorderStyle(OUTLINE, FlxColor.WHITE, 2);
-		
-		text.addFormat(new FlxTextFormat(0xE6E600, false, false, 0xFF8000, 6, 8));
-		text.addFormat(new FlxTextFormat(0xFFAE5E, false, false, 0xFF8000, 8, 10));
-		text.addFormat(new FlxTextFormat(0x008040, false, false, null, 10, 12));
-		text.addFormat(new FlxTextFormat(0x0080C0, false, false, null, 12, 14));
-		text.addFormat(new FlxTextFormat(0x00E6E6, false, false, null, 14, 16));
-		text.addFormat(new FlxTextFormat(0x0080FF, false, false, 0xFFFFFF, 16, 100));
-		
-		add(text);
+		return text;
 	}
 }
