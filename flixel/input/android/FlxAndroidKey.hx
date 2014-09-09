30567c2e40c2146d3359f84ffbc73c511e07dd81
Gama11 <jensfischer95@gmail.com>
2014-09-09 23:11:34 +0200

    Add FlxKeyManager and FlxBaseKeyList for less redundancy across FlxKeyboard and FlxAndroidKeys

--- flixel/input/android/FlxAndroidKey.hx
+++ flixel/input/android/FlxAndroidKey.hx
@@ -5,25 +5,37 @@ package flixel.input.android;
  * Maps enum values and strings to integer keycodes.
  */
 @:enum
-abstract FlxAndroidKey(Int) from Int to Int
+abstract FlxAndroidKey(FlxBaseKey) from Int to Int
 {
 	public static var keyNameMap:Map<String, FlxAndroidKey> = [
-		"ANY" =>            ANY,
-		"MENU" =>           MENU,
-		"BACK" =>           BACK
+		"ANY" =>  ANY,
+		"MENU" => MENU,
+		"BACK" => BACK
 	];
 	
-	// Key Indicies
-	var ANY            = -2;
-	var NONE           = -1;
-	var MENU           = 16777234;
-	var BACK           = 27;
+	var ANY  = -2;
+	var NONE = -1;
+	var MENU = 16777234;
+	var BACK = 27;
 	
 	@:from
 	public static inline function fromString(s:String):FlxAndroidKey
 	{
 		s = s.toUpperCase();
 		return keyNameMap.exists(s) ? keyNameMap.get(s) : NONE;
+	}
+	
+	@:to
+	public static function toString(i:Int):String
+	{
+		for (key in keyNameMap.keys())
+		{
+			if (i == keyNameMap.get(key))
+			{
+				return key;
+			}
+		}
+		return "NONE";
 	}
 }
 #end
