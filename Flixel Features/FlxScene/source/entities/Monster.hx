1e7757e17a8a38f95c03e539c4d767524a3eb0c4
Gama11 <jensfischer95@gmail.com>
2014-09-21 12:26:08 +0200

    Fix indentation

--- Flixel Features/FlxScene/source/entities/Monster.hx
+++ Flixel Features/FlxScene/source/entities/Monster.hx
@@ -5,10 +5,10 @@ import flixel.FlxG;
 
 class Monster extends FlxSprite
 {
-    public function new()
-    {
-        super();
-        loadGraphic("assets/monster.png");
+	public function new()
+	{
+		super();
+		loadGraphic("assets/monster.png");
 		angularVelocity = 100;
-    }
+	}
 }
