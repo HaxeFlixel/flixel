5578b3538f30c07bc6ed873c3b247f4bf2b8476b
Zaphod <zaphy@yandex.ru>
2014-09-25 15:07:42 +0400

    bitmap text demo update

--- User Interface/FlxBitmapTextField/source/PlayState.hx
+++ User Interface/FlxBitmapTextField/source/PlayState.hx
@@ -2,9 +2,9 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.graphics.frames.BitmapFont;
 import flixel.text.FlxBitmapTextField;
-import flixel.text.pxText.PxBitmapFont;
-import flixel.text.pxText.PxTextAlign;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import openfl.Assets;
 
@@ -19,20 +19,24 @@ class PlayState extends FlxState
 		
 		FlxG.camera.bgColor = FlxColor.WHITE;
 		
-		var font:PxBitmapFont = new PxBitmapFont().loadPixelizer(Assets.getBitmapData("assets/fontData10pt.png"), " !\"#$%&'()*+,-./" + "0123456789:;<=>?" + "@ABCDEFGHIJKLMNO" + "PQRSTUVWXYZ[]^_" + "abcdefghijklmno" + "pqrstuvwxyz{|}~\\");
+		var letters:String = " !\"#$%&'()*+,-./" + "0123456789:;<=>?" + "@ABCDEFGHIJKLMNO" + "PQRSTUVWXYZ[]^_" + "abcdefghijklmno" + "pqrstuvwxyz{|}~\\";
+		var font:BitmapFont = BitmapFont.fromXNA(Assets.getBitmapData("assets/fontData10pt.png"), letters);
 		
 		var textBytes = Assets.getText("assets/NavTitle.fnt");
 		var XMLData = Xml.parse(textBytes);
-		var font2:PxBitmapFont = new PxBitmapFont().loadAngelCode(Assets.getBitmapData("assets/NavTitle.png"), XMLData);
+		var font2:BitmapFont = BitmapFont.fromAngelCode(Assets.getBitmapData("assets/NavTitle.png"), XMLData);
 		
 		tf = new FlxBitmapTextField(font);
 		tf.text = "Hello World!\nand this is\nmultiline!!!";
-		tf.color = 0xffffff;
-		tf.fixedWidth = false;
+		tf.textColor = 0xff000000;
+		tf.useTextColor = true;
+		tf.autoSize = true;
 		tf.multiLine = true;
-		tf.alignment = PxTextAlign.LEFT;
+		tf.alignment = FlxTextAlign.LEFT;
 		tf.lineSpacing = 5;
 		tf.padding = 5;
+		tf.background = true;
+		tf.backgroundColor = 0xff00ff00;
 		add(tf);
 		
 	
@@ -40,17 +44,17 @@ class PlayState extends FlxState
 		tf2.y = 100;
 		tf2.useTextColor = false;
 		tf2.text = "Hello World!\nand this is\nmultiline!!!";
-		tf2.shadow = true;
-		tf2.outlineColor = 0xff0000;
+		tf2.borderStyle = FlxTextBorderStyle.SHADOW;
+		tf2.borderColor = 0xffff0000;
 		tf2.width = 610;
-		tf2.alignment = PxTextAlign.CENTER;
+		tf2.alignment = FlxTextAlign.CENTER;
 		tf2.lineSpacing = 5;
 		tf2.padding = 20;
 		tf2.letterSpacing = 25;
 		tf2.autoUpperCase = true;
 		tf2.multiLine = true;
 		tf2.wordWrap = false;
-		tf2.fixedWidth = false;
+		tf2.autoSize = true;
 		add(tf2);
 	}
 	
