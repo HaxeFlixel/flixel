bf0c15c7123dfffaf58f48e3a16697545d738d64
Zaphod <zaphy@yandex.ru>
2014-10-05 21:31:09 +0300

    additional compiler conditional, since BitmapData's threshold method isn't implemented on html5 target

--- flixel/graphics/frames/FlxBitmapFont.hx
+++ flixel/graphics/frames/FlxBitmapFont.hx
@@ -224,6 +224,7 @@ class FlxBitmapFont extends FlxFramesCollection
 	
 	/**
 	 * Load bitmap font in XNA/Pixelizer format.
+	 * May work incorrectly on html5 target.
 	 * 
 	 * @param	source			Source image for this font.
 	 * @param	letters			String of glyphs contained in the source image, in order (ex. " abcdefghijklmnopqrstuvwxyz"). Defaults to DEFAULT_GLYPHS.
@@ -314,8 +315,10 @@ class FlxBitmapFont extends FlxFramesCollection
 		var point:Point = FlxPoint.point;
 		point.x = point.y = 0;
 		var bgColor32:Int = bmd.getPixel32(0, 0);
+		#if !js
+		// TODO: make it work on html5 target
 		bmd.threshold(bmd, bmd.rect, point, "==", bgColor32, 0x00000000, 0xFFFFFFFF, true);
-		
+		#end
 		if (glyphBGColor != FlxColor.TRANSPARENT)
 		{
 			bmd.threshold(bmd, bmd.rect, point, "==", glyphBGColor, FlxColor.TRANSPARENT, FlxColor.WHITE, true);
