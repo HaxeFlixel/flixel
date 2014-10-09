2694c61bf65465ee5d6aec9d114ef0c344fbe99a
Zaphod <zaphy@yandex.ru>
2014-10-09 14:06:50 +0300

    added monospace bitmap font example

--- User Interface/FlxBitmapTextField/source/PlayState.hx
+++ User Interface/FlxBitmapTextField/source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.graphics.frames.FlxBitmapFont;
+import flixel.math.FlxPoint;
 import flixel.text.FlxBitmapTextField;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
@@ -20,13 +21,17 @@ class PlayState extends FlxState
 		FlxG.camera.bgColor = FlxColor.WHITE;
 		
 		var letters:String = " !\"#$%&'()*+,-./" + "0123456789:;<=>?" + "@ABCDEFGHIJKLMNO" + "PQRSTUVWXYZ[]^_" + "abcdefghijklmno" + "pqrstuvwxyz{|}~\\";
-		var font:FlxBitmapFont = FlxBitmapFont.fromXNA(Assets.getBitmapData("assets/fontData10pt.png"), letters);
+		var fontXNA:FlxBitmapFont = FlxBitmapFont.fromXNA(Assets.getBitmapData("assets/fontData10pt.png"), letters);
 		
 		var textBytes = Assets.getText("assets/NavTitle.fnt");
 		var XMLData = Xml.parse(textBytes);
-		var font2:FlxBitmapFont = FlxBitmapFont.fromAngelCode(Assets.getBitmapData("assets/NavTitle.png"), XMLData);
+		var fontAngelCode:FlxBitmapFont = FlxBitmapFont.fromAngelCode(Assets.getBitmapData("assets/NavTitle.png"), XMLData);
 		
-		tf = new FlxBitmapTextField(font);
+		var monospaceLetters:String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!().,";
+		var charSize:FlxPoint = FlxPoint.get(48, 50);
+		var fontMonospace:FlxBitmapFont = FlxBitmapFont.fromMonospace(Assets.getBitmapData("assets/260.png"), monospaceLetters, charSize);
+		
+		tf = new FlxBitmapTextField(fontXNA);
 		tf.text = "Hello World!\nand this is\nmultiline!!!";
 		tf.textColor = 0xff000000;
 		tf.useTextColor = true;
@@ -40,7 +45,7 @@ class PlayState extends FlxState
 		add(tf);
 		
 	
-		tf2 = new FlxBitmapTextField(font2);
+		tf2 = new FlxBitmapTextField(fontAngelCode);
 		tf2.y = 100;
 		tf2.useTextColor = false;
 		tf2.text = "Hello World!\nand this is\nmultiline!!!";
@@ -56,6 +61,12 @@ class PlayState extends FlxState
 		tf2.wordWrap = false;
 		tf2.autoSize = true;
 		add(tf2);
+		
+		var tf3:FlxBitmapTextField = new FlxBitmapTextField(fontMonospace);
+		tf3.y = 300;
+		tf3.autoUpperCase = true;
+		tf3.text = "Robocop rules!!!";
+		add(tf3);
 	}
 	
 	override public function update(elapsed:Float):Void
