cceffb68b3282b51528ee8f607c7fab6f8a72156
Zaphod <zaphy@yandex.ru>
2014-10-14 16:08:05 +0300

    FlxSpriteButton

---
+++ flixel/ui/FlxSpriteButton.hx
@@ -0,0 +1,31 @@
+package flixel.ui;
+
+import flixel.input.IFlxInput;
+import flixel.ui.FlxButton.FlxTypedButton;
+
+/**
+ * A simple button class that calls a function when clicked by the mouse.
+ */
+class FlxSpriteButton extends FlxTypedButton<FlxSprite> implements IFlxInput
+{
+	/**
+	 * Creates a new FlxButton object with a gray background
+	 * and a callback function on the UI thread.
+	 * 
+	 * @param   X          The x position of the button.
+	 * @param   Y          The y position of the button.
+	 * @param   Text       The text that you want to appear on the button.
+	 * @param   OnClick    The function to call whenever the button is clicked.
+	 */
+	public function new(X:Float = 0, Y:Float = 0, ?Label:FlxSprite, ?OnClick:Void->Void)
+	{
+		super(X, Y, OnClick);
+		
+		for (point in labelOffsets)
+		{
+			point.set(point.x - 1, point.y + 3);
+		}
+		
+		label = Label;
+	}
+}
