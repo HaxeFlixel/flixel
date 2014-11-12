ab6fd575b259c77c865b952272797977b9d74ce3
Steve Richey <srichey@floatlearning.com>
2014-11-11 19:41:03 -0600

    add missing mouse checks to FlxMouseEventManager

--- flixel/input/mouse/FlxMouseEventManager.hx
+++ flixel/input/mouse/FlxMouseEventManager.hx
@@ -1,5 +1,6 @@
 package flixel.input.mouse;
 
+#if !FLX_NO_MOUSE
 import flash.errors.Error;
 import flixel.FlxBasic;
 import flixel.FlxCamera;
@@ -520,3 +521,4 @@ private class ObjectMouseData<T:FlxObject>
 		this.mouseButtons = (mouseButtons == null) ? [FlxMouseButtonID.LEFT] : mouseButtons;
 	}
 }
+#end
