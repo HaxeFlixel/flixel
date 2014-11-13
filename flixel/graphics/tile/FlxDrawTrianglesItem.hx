0267752bd4aca2d2c8ab4704016fe07d8761512c
Zaphod <zaphy@yandex.ru>
2014-11-13 22:16:39 +0300

    starting touches for draw triangles item

--- flixel/graphics/tile/FlxDrawTrianglesItem.hx
+++ flixel/graphics/tile/FlxDrawTrianglesItem.hx
@@ -1,6 +1,9 @@
 package flixel.graphics.tile;
 
+import flixel.FlxCamera;
 import flixel.graphics.tile.FlxDrawBaseItem.FlxDrawItemType;
+import openfl.Vector;
+import openfl.display.TriangleCulling;
 
 /**
  * ...
@@ -8,11 +11,42 @@ import flixel.graphics.tile.FlxDrawBaseItem.FlxDrawItemType;
  */
 class FlxDrawTrianglesItem extends FlxDrawBaseItem
 {
-
+	public var vertices:Vector<Float>;
+	public var indices:Vector<Int>;
+	public var uvt:Vector<Float>;
+	
 	public function new() 
 	{
 		super();
 		type = FlxDrawItemType.TRIANGLES;
+		
+		vertices = new Vector<Float>();
+		indices = new Vector<Int>();
+		uvt = new Vector<Float>();
 	}
 	
+	override public function render(camera:FlxCamera):Void 
+	{
+		camera.canvas.graphics.beginBitmapFill(graphics.bitmap, null, true, (camera.antialiasing || antialiasing));
+		camera.canvas.graphics.drawTriangles(vertices, indices, uvt, TriangleCulling.NONE);
+		camera.canvas.graphics.endFill();
+	}
+	
+	override public function reset():Void 
+	{
+		super.reset();
+		
+		vertices.splice(0, vertices.length);
+		indices.splice(0, indices.length);
+		uvt.splice(0, uvt.length);
+	}
+	
+	override public function dispose():Void 
+	{
+		super.dispose();
+		
+		vertices = null;
+		indices = null;
+		uvt = null;
+	}
 }
