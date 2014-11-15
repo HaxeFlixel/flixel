bb435ee14c6ec2097c7af15b2f284de0c5d0914d
Zaphod <zaphy@yandex.ru>
2014-11-16 00:33:13 +0300

    second try on issue #1375

--- flixel/tile/FlxBaseTilemap.hx
+++ flixel/tile/FlxBaseTilemap.hx
@@ -177,7 +177,9 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 		// Figure out the map dimensions based on the data string
 		_data = new Array<Int>();
 		var columns:Array<String>;
-		var rows:Array<String> = StringTools.trim(StringTools.replace(MapData, "\r", "\n")).split("\n");
+		MapData = StringTools.replace(MapData, "\r", "\n");
+		MapData = StringTools.replace(MapData, "\n\n", "\n");
+		var rows:Array<String> = StringTools.trim(MapData).split("\n");
 		heightInTiles = rows.length;
 		widthInTiles = 0;
 		var row:Int = 0;
