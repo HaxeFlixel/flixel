93c6f2d9248190e7e9e32d6d0a3d261f14a86ffd
Gama11 <jensfischer95@gmail.com>
2014-11-19 21:52:22 +0100

    Added unit tests for #1375

--- tests/src/flixel/tile/FlxTilemapTest.hx
+++ tests/src/flixel/tile/FlxTilemapTest.hx
@@ -5,16 +5,25 @@ import flash.errors.ArgumentError;
 import flixel.graphics.FlxGraphic;
 import flixel.tile.FlxTilemap;
 import massive.munit.Assert;
+using StringTools;
 
 class FlxTilemapTest extends FlxTest
 {
 	var tilemap:FlxTilemap;
+	var sampleMapString:String;
+	var sampleMapArray:Array<Int>;
 	
 	@Before
 	function before()
 	{
 		tilemap = new FlxTilemap();
 		destroyable = tilemap;
+		
+		sampleMapString = "0,1,0,1[nl]1,1,1,1[nl]1,0,0,1[nl]";
+		sampleMapArray = [
+			0, 1, 0, 1,
+			1, 1, 1, 1,
+			1, 0, 0, 1];
 	}
 	
 	@Test
@@ -56,6 +65,33 @@ class FlxTilemapTest extends FlxTest
 		Assert.areEqual(3, tilemap.widthInTiles);
 		Assert.areEqual(2, tilemap.heightInTiles);
 		FlxAssert.arraysAreEqual([0, 1, 0, 1, 1, 1], tilemap.getData());
+	}
+	
+	@Test
+	function testLoadMapFromCSVWindowsNewlines()
+	{
+		testLoadMapFromCSVWithNewline("\r\n");
+	}
+	
+	@Test
+	function testLoadMapFromCSVUnixNewlines()
+	{
+		testLoadMapFromCSVWithNewline("\n");
+	}
+	
+	@Test // issue 1375
+	function testLoadMapFromCSVMacNewlines()
+	{
+		testLoadMapFromCSVWithNewline("\r");
+	}
+	
+	function testLoadMapFromCSVWithNewline(newlines:String)
+	{
+		tilemap.loadMapFromCSV(sampleMapString.replace("[nl]", newlines), getBitmapData(), 8, 8);
+		
+		Assert.areEqual(4, tilemap.widthInTiles);
+		Assert.areEqual(3, tilemap.heightInTiles);
+		FlxAssert.arraysAreEqual(sampleMapArray, tilemap.getData());
 	}
 	
 	function getBitmapData()
