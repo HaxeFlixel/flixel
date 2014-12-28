HaxeFlixel/flixel

commit 7b71f59b13ea186bc2be740fa0859a9ff0032d3d

FlxObject: some cleanup for drawDebug()

flixel/FlxObject.hx
@@ -453,6 +453,7 @@ class FlxObject extends FlxBasic
 	#end
 	
 	private var _point:FlxPoint;
+	private var _rect:FlxRect;
 	
 	/**
 	 * @param	X		The X-coordinate of the point in space.
@@ -481,6 +482,7 @@ class FlxObject extends FlxBasic
 		last = FlxPoint.get(x, y);
 		scrollFactor = FlxPoint.get(1, 1);
 		_point = FlxPoint.get();
+		_rect = FlxRect.get();
 		
 		initMotionVars();
 	}
@@ -511,6 +513,7 @@ class FlxObject extends FlxBasic
 		scrollFactor = FlxDestroyUtil.put(scrollFactor);
 		last = FlxDestroyUtil.put(last);
 		_point = FlxDestroyUtil.put(_point);
+		_rect = FlxDestroyUtil.put(_rect);
 	}
 	
 	/**
@@ -858,12 +861,17 @@ class FlxObject extends FlxBasic
 		height = Height;
 	}
 	
-	#if !FLX_NO_DEBUG
+#if !FLX_NO_DEBUG
 	public function drawDebug():Void
 	{
-		if (!ignoreDrawDebug)
+		if (ignoreDrawDebug)
 		{
-			for (camera in cameras)
+			return;
+		}
+		
+		for (camera in cameras)
+		{
+			if (camera.visible && camera.exists && isOnScreen(camera))
 			{
 				drawDebugOnCamera(camera);
 			}
@@ -876,27 +884,9 @@ class FlxObject extends FlxBasic
 	 * 
 	 * @param	Camera	Which camera to draw the debug visuals to.
 	 */
-	public function drawDebugOnCamera(Camera:FlxCamera):Void
+	public function drawDebugOnCamera(camera:FlxCamera):Void
 	{
-		if (!Camera.visible || !Camera.exists || !isOnScreen(Camera))
-		{
-			return;
-		}
-		
-		//get bounding box coordinates
-		var boundingBoxX:Float = x - (Camera.scroll.x * scrollFactor.x); //copied from getScreenXY()
-		var boundingBoxY:Float = y - (Camera.scroll.y * scrollFactor.y);
-		
-		if (isPixelPerfectRender(Camera))
-		{
-			boundingBoxX = Math.floor(boundingBoxX);
-			boundingBoxY = Math.floor(boundingBoxY);
-		}
-		
-		#if FLX_RENDER_BLIT
-		var boundingBoxWidth:Int = Std.int(width);
-		var boundingBoxHeight:Int = Std.int(height);
-		#end
+		var rect = getBoundingBox(camera);
 		
 		// Find the color to use
 		var color:Null<Int> = debugBoundingBoxColor;
@@ -904,46 +894,51 @@ class FlxObject extends FlxBasic
 		{
 			if (allowCollisions != FlxObject.NONE)
 			{
-				if (allowCollisions != ANY)
-				{
-					color = FlxColor.PINK;
-				}
-				if (immovable)
-				{
-					color = FlxColor.GREEN;
-				}
-				else
-				{
-					color = FlxColor.RED;
-				}
+				color = immovable ? FlxColor.GREEN : FlxColor.RED;
 			}
-			
-			// if there's still no color...
-			if (color == null)
+			else
 			{
 				color = FlxColor.BLUE;
 			}
 		}
 		
 		//fill static graphics object with square shape
+		var gfx:Graphics = beginDrawDebug(camera);
+		gfx.lineStyle(1, color, 0.5);
+		gfx.drawRect(rect.x, rect.y, rect.width, rect.height);
+		endDrawDebug(camera);
+	}
+#end
+	
+	private inline function beginDrawDebug(camera:FlxCamera):Graphics
+	{
 		#if FLX_RENDER_BLIT
-		var gfx:Graphics = FlxSpriteUtil.flashGfx;
-		gfx.clear();
-		gfx.moveTo(boundingBoxX, boundingBoxY);
-		gfx.lineStyle(1, color, 0.5);
-		gfx.lineTo(boundingBoxX + boundingBoxWidth, boundingBoxY);
-		gfx.lineTo(boundingBoxX + boundingBoxWidth, boundingBoxY + boundingBoxHeight);
-		gfx.lineTo(boundingBoxX, boundingBoxY + boundingBoxHeight);
-		gfx.lineTo(boundingBoxX, boundingBoxY);
-		//draw graphics shape to camera buffer
-		Camera.buffer.draw(FlxSpriteUtil.flashGfxSprite);
+			FlxSpriteUtil.flashGfx.clear();
+			return FlxSpriteUtil.flashGfx;
 		#else
-		var gfx:Graphics = Camera.debugLayer.graphics;
-		gfx.lineStyle(1, color, 0.5);
-		gfx.drawRect(boundingBoxX, boundingBoxY, width, height);
+			return camera.debugLayer.graphics;
 		#end
 	}
-	#end
+	
+	private inline function endDrawDebug(camera:FlxCamera)
+	{
+		#if FLX_RENDER_BLIT
+			camera.buffer.draw(FlxSpriteUtil.flashGfxSprite);
+		#end
+	}
+	
+	private function getBoundingBox(camera:FlxCamera):FlxRect
+	{
+		getScreenPosition(_point, camera);
+		_rect.set(_point.x, _point.y, width, height);
+		
+		if (isPixelPerfectRender(camera))
+		{
+			_rect.floor();
+		}
+		
+		return _rect;
+	}
 	
 	/**
 	 * Convert object to readable string name. Useful for debugging, save games, etc.
@@ -1032,14 +1027,7 @@ class FlxObject extends FlxBasic
 	
 	private function set_solid(Solid:Bool):Bool
 	{
-		if (Solid)
-		{
-			allowCollisions = ANY;
-		}
-		else
-		{
-			allowCollisions = NONE;
-		}
+		allowCollisions = Solid ? ANY : NONE;
 		return Solid;
 	}
 	

flixel/math/FlxRect.hx
@@ -105,6 +105,16 @@ class FlxRect implements IFlxPooled
 	}
 	
 	/**
+	 * Shortcut for setting both x and y.
+	 */
+	public inline function setPosition(x:Float, y:Float):FlxRect
+	{
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+	
+	/**
 	 * Fill this rectangle with the data provided.
 	 * 
 	 * @param	X		The X-coordinate of the point in space.

flixel/tile/FlxTilemap.hx
@@ -302,9 +302,8 @@ class FlxTilemap extends FlxBaseTilemap<FlxTile>
 	{
 		#if FLX_RENDER_TILE
 		var buffer:FlxTilemapBuffer = null;
-		var l:Int = FlxG.cameras.list.length;
 		
-		for (i in 0...l)
+		for (i in 0...FlxG.cameras.list.length)
 		{
 			if (FlxG.cameras.list[i] == Camera)
 			{
@@ -337,22 +336,8 @@ class FlxTilemap extends FlxBaseTilemap<FlxTile>
 		var screenColumns:Int = buffer.columns;
 		
 		// Bound the upper left corner
-		if (screenXInTiles < 0)
-		{
-			screenXInTiles = 0;
-		}
-		if (screenXInTiles > widthInTiles - screenColumns)
-		{
-			screenXInTiles = widthInTiles - screenColumns;
-		}
-		if (screenYInTiles < 0)
-		{
-			screenYInTiles = 0;
-		}
-		if (screenYInTiles > heightInTiles - screenRows)
-		{
-			screenYInTiles = heightInTiles - screenRows;
-		}
+		screenXInTiles = Std.int(FlxMath.bound(screenXInTiles, 0, widthInTiles - screenColumns));
+		screenYInTiles = Std.int(FlxMath.bound(screenYInTiles, 0, heightInTiles - screenRows));
 		
 		var rowIndex:Int = screenYInTiles * widthInTiles + screenXInTiles;
 		_flashPoint.y = 0;

commit acc09edefc526ac749e4c6bd854c66b26de5a7d1

Fix FlxTilemap's drawDebug on FLX_RENDER_BLIT
Bug was introduced in 7b71f59b13ea186bc2be740fa0859a9ff0032d3d.

flixel/FlxObject.hx
@@ -868,10 +868,7 @@ class FlxObject extends FlxBasic
 		
 		for (camera in cameras)
 		{
-			if (camera.visible && camera.exists && isOnScreen(camera))
-			{
-				drawDebugOnCamera(camera);
-			}
+			drawDebugOnCamera(camera);
 		}
 	}
 	
@@ -883,6 +880,11 @@ class FlxObject extends FlxBasic
 	 */
 	public function drawDebugOnCamera(camera:FlxCamera):Void
 	{
+		if (!camera.visible || !camera.exists || !isOnScreen(camera))
+		{
+			return;
+		}
+		
 		var rect = getBoundingBox(camera);
 		
 		// Find the color to use