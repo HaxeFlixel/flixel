9a0888e7f78ac0a9ed1c544413f36ebcbe50ecec
Zaphod <zaphy@yandex.ru>
2015-03-22 19:19:19 +0300

    renamed one method

--- Platformers/FlxTilemapExt/source/PlayState.hx
+++ Platformers/FlxTilemapExt/source/PlayState.hx
@@ -66,7 +66,7 @@ class PlayState extends FlxState
 		add(level.loadMapFromCSV(Assets.getText("assets/slopemap.txt"), "assets/colortiles.png", 10, 10));
 		
 		// tile tearing problem fix
-		var levelTiles:FlxTileFrames = FlxTileFrames.fromBitmapWithSpacesAndBorders("assets/colortiles.png", new FlxPoint(10, 10), new FlxPoint(2, 2), new FlxPoint(2, 2));
+		var levelTiles:FlxTileFrames = FlxTileFrames.fromBitmapAddSpacesAndBorders("assets/colortiles.png", new FlxPoint(10, 10), new FlxPoint(2, 2), new FlxPoint(2, 2));
 		level.frames = levelTiles;
 		level.useScaleHack = false;
 		
