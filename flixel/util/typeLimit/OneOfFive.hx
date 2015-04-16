43a3895d9479f8fdff9296637ef4fab25c473ecb
Gama11 <jensfischer95@gmail.com>
2015-04-16 14:13:08 +0200

    Looks like OneOfFive isn't actually used anywhere anymore

--- flixel/util/typeLimit/OneOfFive.hx
+++
@@ -1,9 +0,0 @@
-package flixel.util.typeLimit;
-
-/**
- * Useful to limit a Dynamic function argument's type to the specified
- * type parameters. This does NOT make the use of Dynamic type-safe in
- * any way (the underlying type is still Dynamic and Std.is() checks +
- * casts are necessary).
- */
-abstract OneOfFour<T1, T2, T3, T4, T5>(Dynamic) from T1 from T2 from T3 from T4 from T5 to T1 to T2 to T3 to T4 to T5 {}
