90f32deed29e09e20247de0eff062f42bb19467f
Lars A. Doucet <lars.doucet@gmail.com>
2015-05-21 14:29:51 -0500

    formatting

--- flixel/input/gamepad/ButtonID.hx
+++ flixel/input/gamepad/ButtonID.hx
@@ -15,30 +15,43 @@ abstract ButtonID(Int) from Int to Int
 		= FlxMacroUtil.buildMap("flixel.input.gamepad.ButtonID", true);
 	
 	// Button Indices
-	var ANY               = -2;		
-	var NONE              = -1;		
-	var A                 =  0;		//BOTTOM face button
-	var B                 =  1;		//RIGHT  face button
-	var X                 =  2;		//LEFT   face button
-	var Y                 =  3;		//TOP    face button
-	var LEFT_SHOULDER     =  4;		//left "bumper"
-	var RIGHT_SHOULDER    =  5;		//right "bumper"
-	var BACK              =  6;		//a.k.a "select", left center button
-	var START             =  7;		//right center button
-	var LEFT_STICK_BTN    =  8;		//Click left stick
-	var RIGHT_STICK_BTN   =  9;		//Click right stick
-	var GUIDE             = 10;		//a.k.a "xbox" or "PS" or "home" button in center
+	var ANY               = -2;
+	var NONE              = -1;
+	
+	/**BOTTOM face button*/ 
+	var A                 =  0;
+	/**RIGHT face button*/  
+	var B                 =  1;
+	/**LEFT face button*/
+	var X                 =  2;
+	/**TOP face button*/
+	var Y                 =  3;
+	/**left digital "bumper"*/
+	var LEFT_SHOULDER     =  4;
+	/**right digital "bumper"*/
+	var RIGHT_SHOULDER    =  5;
+	/**also known as "select", the leftmost center button*/
+	var BACK              =  6;
+	/**the rightmost center button*/
+	var START             =  7;
+	/**digital click of the left analog stick*/
+	var LEFT_STICK_BTN    =  8;
+	/**digital click of the right analog stick*/
+	var RIGHT_STICK_BTN   =  9;
+	/**this is the "XBox" or "PS" or "home" button in the center*/
+	var GUIDE             = 10;
 	var DPAD_UP           = 11;
 	var DPAD_DOWN         = 12;
 	var DPAD_LEFT         = 13;
 	var DPAD_RIGHT        = 14;
-	var LEFT_TRIGGER_BTN  = 15;		//digital button at end of left analog trigger full squeeze (if available)
-	var RIGHT_TRIGGER_BTN = 16;		//digital button at end of right analog trigger full squeeze (if available)
+	/**digital click at end of left analog trigger's squeeze (if available)*/
+	var LEFT_TRIGGER_BTN  = 15;
+	/**digital click at end of right analog trigger's squeeze (if available)*/
+	var RIGHT_TRIGGER_BTN = 16;
 	
 	var LEFT_TRIGGER       = 17;
 	var RIGHT_TRIGGER      = 18;
 	
-	// FlxGamepadAnalogSticks (wraps up the _X/_Y into a structure)
 	var LEFT_ANALOG_STICK  = 19;
 	var RIGHT_ANALOG_STICK = 20;
 	
