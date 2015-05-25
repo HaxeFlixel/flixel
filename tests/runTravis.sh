0a48e000b6f7451c13a1de2c1bbb854185c19952
Gama11 <jensfischer95@gmail.com>
2015-05-25 18:36:18 +0200

    Move unit tests to tests/unit

--- tests/runTravis.sh
+++ tests/runTravis.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# unit tests
+cd unit
+
 if [ $TARGET == "flash" ]; then
 	haxelib run munit test
 elif [ $TARGET != "html5" ]; then
 	haxelib run lime test $TARGET
 fi
 
+cd ..
+
+# demos
 if [ $TARGET == "cpp" ]; then
 	haxelib run flixel-tools td $TARGET Mode "RPG Interface" FlxNape
 else
--- tests/test.bat
+++
@@ -1,3 +0,0 @@
-haxelib run lime test neko
-haxelib run lime test cpp
-haxelib run munit test
