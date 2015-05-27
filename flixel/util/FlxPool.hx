d0c8957226ab9d3e401508a076fb9541703abbd0
Gama11 <jensfischer95@gmail.com>
2015-05-27 19:15:16 +0200

    FlxPool: just remove @:generic entirely
    Apart from causing issues with docs and completion, it causes a compiler error with Haxe 3.1.3 and FlxFSM.

--- flixel/util/FlxPool.hx
+++ flixel/util/FlxPool.hx
@@ -6,9 +6,6 @@ import flixel.util.FlxDestroyUtil.IFlxDestroyable;
  * A generic container that facilitates pooling and recycling of objects.
  * WARNING: Pooled objects must have parameterless constructors: function new()
  */
-#if !display
-@:generic
-#end
 class FlxPool<T:IFlxDestroyable>
 {
 	public var length(get, never):Int;
