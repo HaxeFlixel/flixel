f28353c978fa272251f12235cb767d3919bd82ac
Gama11 <jensfischer95@gmail.com>
2015-06-21 21:40:35 +0200

    DynamicShadows: some cleanup

--- Effects/DynamicShadows/source/PlayState.hx
+++ Effects/DynamicShadows/source/PlayState.hx
@@ -11,14 +11,12 @@ import flixel.tile.FlxTilemap;
 import flixel.util.FlxColor;
 import nape.geom.Vec2;
 import nape.geom.Vec2List;
+import nape.phys.Body;
 import openfl.display.BlendMode;
-import openfl.geom.ColorTransform;
-import openfl.geom.Matrix;
 import openfl.display.FPS;
-
 using flixel.util.FlxSpriteUtil;
 
-/*
+/**
  * This was based on a guide from this forum post: http://forums.tigsource.com/index.php?topic=8803.0
  * Ported to HaxeFlixel by Xerosugar
  * 
@@ -32,12 +30,12 @@ using flixel.util.FlxSpriteUtil;
  * 
  * @author Tommy Elfving
  */
-
 class PlayState extends FlxState
 {
 	public static inline var TILE_SIZE:Int = 16;
-	private static inline var PROP_BARREL:Int = 5;
-	private static inline var PROP_GEM:Int = 6;
+	
+	private static inline var SHADOW_COLOR = 0xff2a2963;
+	private static inline var OVERLAY_COLOR = 0xff887fff;
 	
 	/**
 	 * Only contains non-collidabe tiles
@@ -64,13 +62,11 @@ class PlayState extends FlxState
 	 */
 	private var gem:Gem;
 	
-	private var shadowColor = 0xff2a2963;
-	private var overlayColor = 0xff887fff;
-	
 	/**
-	 * If there's a small gap between something (could be two tiles, even if they're right next to each other), this should cover it up for us
+	 * If there's a small gap between something (could be two tiles,
+	 * even if they're right next to each other), this should cover it up for us
 	 */
-	private var lineStyle:LineStyle;
+	private var lineStyle:LineStyle = { color: SHADOW_COLOR, thickness: 1 };
 	
 	private var infoText:FlxText;
 	private var fps:FPS;
@@ -80,53 +76,35 @@ class PlayState extends FlxState
 		super.create();
 		
 		FlxG.camera.bgColor = 0x5a81ad;
-		lineStyle = { color:shadowColor, thickness:1 };
 		
 		FlxNapeSpace.init();
 		FlxNapeSpace.space.gravity.setxy(0, 1200);
 		FlxNapeSpace.drawDebug = false; // You can toggle this on/off one by pressing 'D'
 		
 		var background:FlxTilemap = new FlxTilemap();
-		background.loadMapFromCSV("assets/data/background.txt", "assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
+		background.loadMapFromCSV("assets/data/background.txt",
+			"assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
 		add(background);
 		
-		// Note: The tilemap used in this demo was drawn with 'Tiled' (http://www.mapeditor.org/), but the level data was picked from the .tmx file and put into two separate
-		// .txt files for simplicity. If you wish to learn how to use Tiled with your project, have a look at this demo: http://haxeflixel.com/demos/TiledEditor/
+		// Note: The tilemap used in this demo was drawn with 'Tiled' (http://www.mapeditor.org/),
+		// but the level data was picked from the .tmx file and put into two separate
+		// .txt files for simplicity. If you wish to learn how to use Tiled with your project,
+		// have a look at this demo: http://haxeflixel.com/demos/TiledEditor/
 		
-		// If we add the shadows *before* all of the foreground elements (stage included) they will only cover the background, which is usually what you'd want I'd guess :)
+		// If we add the shadows *before* all of the foreground elements (stage included)
+		// they will only cover the background, which is usually what you'd want I'd guess :)
 		shadowCanvas = new FlxSprite();
 		shadowCanvas.blend = BlendMode.MULTIPLY;
 		shadowCanvas.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT, true);
 		add(shadowCanvas);
 		
 		foreground = new FlxNapeTilemap();
-		foreground.loadMapFromCSV("assets/data/foreground.txt", "assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
+		foreground.loadMapFromCSV("assets/data/foreground.txt",
+			"assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
 		add(foreground);
 		
 		foreground.setupTileIndices([4]);
-		
-		var tileIndex:Int = 0;
-		for (tileY in 0...foreground.heightInTiles)
-		{
-			for (tileX in 0...foreground.widthInTiles)
-			{
-				tileIndex = foreground.getTile(tileX, tileY);
-				var xPos:Float = tileX * TILE_SIZE;
-				var yPos:Float = tileY * TILE_SIZE;
-				
-				if (tileIndex == PROP_BARREL)
-				{					
-					add(new Barrel(xPos, yPos));
-					cleanTile(tileX, tileY);
-				}
-				else if (tileIndex == PROP_GEM)
-				{
-					gem = new Gem(xPos, yPos);
-					add(gem);
-					cleanTile(tileX, tileY);
-				}
-			}
-		}
+		createProps();
 		
 		shadowOverlay = new FlxSprite();
 		shadowOverlay.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT, true);
@@ -142,27 +120,48 @@ class PlayState extends FlxState
 		fps.visible = false;
 	}
 	
+	private function createProps():Void
+	{
+		for (tileY in 0...foreground.heightInTiles)
+		{
+			for (tileX in 0...foreground.widthInTiles)
+			{
+				var tileIndex = foreground.getTile(tileX, tileY);
+				var xPos:Float = tileX * TILE_SIZE;
+				var yPos:Float = tileY * TILE_SIZE;
+				
+				if (tileIndex == Prop.BARREL)
+				{					
+					add(new Barrel(xPos, yPos));
+					cleanTile(tileX, tileY);
+				}
+				else if (tileIndex == Prop.GEM)
+				{
+					gem = new Gem(xPos, yPos);
+					add(gem);
+					cleanTile(tileX, tileY);
+				}
+			}
+		}
+	}
+	
 	/**
 	 * The tile in question was replaced by an actual object, this function will clean up the tile for us
 	 */
-	private function cleanTile(_x:Int, _y:Int):Void
+	private function cleanTile(x:Int, y:Int):Void
 	{
-		foreground.setTile(_x, _y, 0);
+		foreground.setTile(x, y, 0);
 	}
 	
 	override public function update(elapsed:Float):Void
 	{
-		infoText.text = "FPS: " +fps.currentFPS +"\n\nObjects can be dragged/thrown around.\n\nPress 'R' to restart.";
+		infoText.text = "FPS: " + fps.currentFPS + "\n\nObjects can be dragged/thrown around.\n\nPress 'R' to restart.";
 		
 		if (FlxG.keys.justPressed.R)
-		{
 			FlxG.resetState();
-		}
 		
 		if (FlxG.keys.justPressed.D)
-		{
 			FlxNapeSpace.drawDebug = !FlxNapeSpace.drawDebug;
-		}
 		
 		processShadows();
 		super.update(elapsed);
@@ -171,77 +170,87 @@ class PlayState extends FlxState
 	public function processShadows():Void
 	{
 		shadowCanvas.fill(FlxColor.TRANSPARENT);
-		
-		shadowOverlay.fill(overlayColor);
-		shadowOverlay.drawCircle(gem.body.position.x +FlxG.random.float(-.6, .6), gem.body.position.y +FlxG.random.float(-.6, .6), (FlxG.random.bool(5) ? 16 : 16.5), 0xffff5f5f);		// outer red circle
-		shadowOverlay.drawCircle(gem.body.position.x +FlxG.random.float(-.25, .25), gem.body.position.y +FlxG.random.float(-.25, .25), (FlxG.random.bool(5) ? 13 : 13.5), 0xffff7070); // inner red circle
-		
-		for (_body in FlxNapeSpace.space.bodies)
+		shadowOverlay.fill(OVERLAY_COLOR);
+
+		shadowOverlay.drawCircle( // outer red circle
+			gem.body.position.x + FlxG.random.float( -.6, .6),
+			gem.body.position.y + FlxG.random.float( -.6, .6),
+			(FlxG.random.bool(5) ? 16 : 16.5), 0xffff5f5f);
+
+		shadowOverlay.drawCircle( // inner red circle
+			gem.body.position.x + FlxG.random.float( -.25, .25),
+			gem.body.position.y + FlxG.random.float( -.25, .25),
+			(FlxG.random.bool(5) ? 13 : 13.5), 0xffff7070);
+
+		for (body in FlxNapeSpace.space.bodies)
 		{
-			if (_body.userData.type == "Gem") // We don't want to draw any shadows around the gem, since it's the light source
-			{
-				continue;
-			}
+			// We don't want to draw any shadows around the gem, since it's the light source
+			if (body.userData.type != "Gem")
+				processBodyShapes(body);
+		}
+	}
+	
+	private function processBodyShapes(body:Body)
+	{
+		for (shape in body.shapes) 
+		{
+			var verts:Vec2List = shape.castPolygon.worldVerts;
 			
-			for (shape in _body.shapes) 
+			for (i in 0...verts.length) 
 			{
-				var verts:Vec2List = shape.castPolygon.worldVerts;
-				var startVertex:Vec2, endVertex:Vec2;
-				
-				for (i in 0...verts.length) 
-				{
-					if (i == 0) 
-					{
-						startVertex = verts.at(verts.length - 1);
-					}
-					else
-					{
-						startVertex = verts.at(i - 1);
-					}
-					
-					endVertex = verts.at(i);
-					
-					var tempLightOrigin:Vec2 = Vec2.get(gem.body.position.x +FlxG.random.float(-.3, 3), gem.body.position.y +FlxG.random.float(-.3, .3));
-					
-					if (doesEdgeCastShadow(startVertex, endVertex, tempLightOrigin))
-					{
-						var projectedPoint:Vec2 = projectPoint(startVertex, tempLightOrigin);
-						var prevProjectedPt:Vec2 = projectPoint(endVertex, tempLightOrigin);
-						var vts:Array<FlxPoint> = [
-							FlxPoint.weak(startVertex.x, startVertex.y),
-							FlxPoint.weak(projectedPoint.x, projectedPoint.y),
-							FlxPoint.weak(prevProjectedPt.x, prevProjectedPt.y),
-							FlxPoint.weak(endVertex.x, endVertex.y)
-						];
-						
-						shadowCanvas.drawPolygon(vts, shadowColor, lineStyle);
-					}
-				}
+				var startVertex:Vec2 = (i == 0) ? verts.at(verts.length - 1) : verts.at(i - 1);
+				processShapeVertex(startVertex, verts.at(i));
 			}
 		}
 	}
 	
-	private function projectPoint(point_:Vec2, light_:Vec2):Vec2
+	private function processShapeVertex(startVertex:Vec2, endVertex:Vec2):Void
 	{
-		var lightToPoint:Vec2 = point_.copy();
-		lightToPoint.subeq(light_);
+		var tempLightOrigin:Vec2 = Vec2.get(
+			gem.body.position.x + FlxG.random.float( -.3, 3),
+			gem.body.position.y + FlxG.random.float(-.3, .3));
 		
-		var projectedPoint:Vec2 = point_.copy();
-		projectedPoint.addeq(lightToPoint.muleq(.45));
-		
-		return projectedPoint;
+		if (doesEdgeCastShadow(startVertex, endVertex, tempLightOrigin))
+		{
+			var projectedPoint:Vec2 = projectPoint(startVertex, tempLightOrigin);
+			var prevProjectedPt:Vec2 = projectPoint(endVertex, tempLightOrigin);
+			var vts:Array<FlxPoint> = [
+				FlxPoint.weak(startVertex.x, startVertex.y),
+				FlxPoint.weak(projectedPoint.x, projectedPoint.y),
+				FlxPoint.weak(prevProjectedPt.x, prevProjectedPt.y),
+				FlxPoint.weak(endVertex.x, endVertex.y)
+			];
+			
+			shadowCanvas.drawPolygon(vts, SHADOW_COLOR, lineStyle);
+		}
 	}
 	
-	private function doesEdgeCastShadow(start_:Vec2, end_:Vec2, light_:Vec2):Bool
+	private function projectPoint(point:Vec2, light:Vec2):Vec2
 	{
-		var startToEnd:Vec2 = end_.copy();
-		startToEnd.subeq(start_);
+		var lightToPoint:Vec2 = point.copy();
+		lightToPoint.subeq(light);
+		
+		var projectedPoint:Vec2 = point.copy();
+		return projectedPoint.addeq(lightToPoint.muleq(.45));
+	}
+	
+	private function doesEdgeCastShadow(start:Vec2, end:Vec2, light:Vec2):Bool
+	{
+		var startToEnd:Vec2 = end.copy();
+		startToEnd.subeq(start);
 		
 		var normal:Vec2 = new Vec2(startToEnd.y, -1 * startToEnd.x);
 		
-		var lightToStart:Vec2 = start_.copy();
-		lightToStart.subeq(light_);
+		var lightToStart:Vec2 = start.copy();
+		lightToStart.subeq(light);
 	 
 		return normal.dot(lightToStart) > 0;
 	}
+}
+
+@:enum
+abstract Prop(Int) to Int
+{
+	var BARREL = 5;
+	var GEM = 6;
 }
