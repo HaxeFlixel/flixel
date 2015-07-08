4ed318cafb182bfe12014f81e315a33f0877945a
Gama11 <jensfischer95@gmail.com>
2015-07-08 23:54:01 +0200

    FlixelCoverageTests: make use of --macro include

--- tests/coverage/source/Main.hx
+++ tests/coverage/source/Main.hx
@@ -1,15 +1,1 @@
-package;
-
-class Main
-{
-	static public function main()
-	{ 
-		importFlixel();
-	}
-	
-	macro static function importFlixel()
-	{
-		haxe.macro.Compiler.include("flixel");
-		return haxe.macro.Context.makeExpr(0, haxe.macro.Context.currentPos());
-	}
-}
+class Main {}
