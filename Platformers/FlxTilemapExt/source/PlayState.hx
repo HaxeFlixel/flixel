ccbeeef5e760e68a89a57e46a8c512e91a82bca2
Adriano Lima <adrianulima@hotmail.com>
2015-09-23 16:37:07 -0300

    Using new FlxTilemapExt
    Using slopes of 22.5 and 67.5 degrees, also added jumpWalls and clouds

--- Platformers/FlxTilemapExt/source/PlayState.hx
+++ Platformers/FlxTilemapExt/source/PlayState.hx
@@ -8,6 +8,7 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
 import flixel.text.FlxText;
+import flixel.tile.FlxTile;
 import flixel.util.FlxColor;
 import flixel.graphics.frames.FlxTileFrames;
 import flixel.math.FlxPoint;
@@ -49,12 +50,12 @@ class PlayState extends FlxState
 		FlxG.cameras.bgColor = 0xff050509;
 		
 		// Create player (a red box)
-		_player = new FlxSprite(70);
-		_player.makeGraphic(8, 10, FlxColor.RED);
+		_player = new FlxSprite(70, 20);
+		_player.makeGraphic(8, 14, FlxColor.LIME);
 		
 		// Max velocities on player.  If it's a platformer, Y should be high, like 200.
 		// Otherwise, set them to something like 80.
-		_player.maxVelocity.set(120, 200);
+		_player.maxVelocity.set(120, 220);
 		
 		// Simulate Gravity on the Player
 		_player.acceleration.y = 200;
@@ -70,20 +71,24 @@ class PlayState extends FlxState
 		level.frames = levelTiles;
 		level.useScaleHack = false;
 		
-		var tempFL:Array<Int> = [5, 21];
-		var tempFR:Array<Int> = [6, 22];
-		var tempCL:Array<Int> = [7, 23];
-		var tempCR:Array<Int> = [8, 24];
+		var tempFL:Array<Int> = [5, 9, 10, 13, 15];
+		var tempFR:Array<Int> = [6, 11, 12, 14, 16];
+		var tempCL:Array<Int> = [7, 17, 18, 21, 23];
+		var tempCR:Array<Int> = [8, 19, 20, 22, 24];
 		
-		var tempC:Array<Int> = [4, 12, 20];
+		//var tempC:Array<Int> = [4];
 		
 		level.setSlopes(tempFL, tempFR, tempCL, tempCR);
-		level.setClouds([4]);
-		level.setClouds([20], true);
-		level.setConditionals([21, 22, 23, 24]);
+		//level.setClouds(tempC);
+		
+		level.setSlopes22([10, 11, 18, 19], [9, 12, 17, 20]);
+		level.setSlopes67([13, 14, 21, 22], [15, 16, 23, 24]);
+		
+		level.setTileProperties(4, FlxObject.NONE, downClouds);
+		level.setTileProperties(3, level.getTileCollisions(3), wallJump);
 		
 		// Make the Camera follow the player.
-		FlxG.camera.setScrollBoundsRect(0, 0, 970, 500, true);
+		FlxG.camera.setScrollBoundsRect(0, 0, 500, 500, true);
 		FlxG.camera.follow(_player, PLATFORMER);
 		
 		//HUD
@@ -91,9 +96,23 @@ class PlayState extends FlxState
 		
 		_levelText = new FlxText(FlxG.width - 100, 0, 100);
 		_levelText.setFormat(null, 8, 0xFFFFFFFF, RIGHT);
-		_levelText.text = "Slope Test";
+		_levelText.text = "G -> Gravity";
 		_levelText.scrollFactor.set();
 		_hud.add(_levelText);
+	}
+	
+	private function downClouds(Tile:FlxObject, Object:FlxObject):Void
+	{
+		Tile.allowCollisions = (FlxG.keys.anyPressed([DOWN, S])) ? FlxObject.NONE : FlxObject.CEILING;
+	}
+	
+	private function wallJump(Tile:FlxObject, Object:FlxObject):Void
+	{
+		if (Object.velocity.y > 0)
+		{
+			Object.velocity.y *= 0.9;
+			Object.touching |= FlxObject.FLOOR;
+		}
 	}
 	
 	override public function destroy():Void
@@ -109,27 +128,35 @@ class PlayState extends FlxState
 	
 	override public function update(elapsed:Float):Void
 	{
-		level.throughDownClouds = FlxG.keys.anyPressed([DOWN, S]);
-		level.throughFloorSlopes = !FlxG.keys.anyPressed([UP, W]);
-		level.throughCeilingSlopes = FlxG.keys.anyPressed([UP, W]);
-		
 		_player.acceleration.x = 0;
 		
 		if (FlxG.keys.anyPressed([LEFT, A]))
 		{
-			_player.acceleration.x = -_player.maxVelocity.x * 4;
+			_player.acceleration.x = -_player.maxVelocity.x * ((_player.isTouching(FlxObject.FLOOR))?4:3);
 		}
 		if (FlxG.keys.anyPressed([RIGHT, D]))
 		{
-			_player.acceleration.x = _player.maxVelocity.x * 4;
+			_player.acceleration.x = _player.maxVelocity.x * ((_player.isTouching(FlxObject.FLOOR))?4:3);
 		}
-		if (FlxG.keys.anyPressed([SPACE]) && _player.isTouching(FlxObject.FLOOR))
+		if (FlxG.keys.anyPressed([SPACE, W, UP]) && _player.isTouching(FlxObject.FLOOR))
 		{
 			_player.velocity.y = -_player.maxVelocity.y / 2;
+			if (_player.isTouching(FlxObject.RIGHT))
+			{
+				_player.velocity.x = -_player.maxVelocity.x;
+			}
+			else if (_player.isTouching(FlxObject.LEFT))
+			{
+				_player.velocity.x = _player.maxVelocity.x;
+			}
+		}
+		
+		if (FlxG.keys.anyJustPressed([G])) {
+			_player.acceleration.y = -_player.acceleration.y;
 		}
 		
 		super.update(elapsed);
 		
-		FlxG.collide(level, _player);	
+		FlxG.collide(level, _player);
 	}	
 }
