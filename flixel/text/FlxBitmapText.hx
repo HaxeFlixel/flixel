f7339565d7eb9555445b0c6fdeddce5e8e851232
Zaphod <zaphy@yandex.ru>
2015-10-24 18:24:04 +0300

    fix for the issue #1639

--- flixel/text/FlxBitmapText.hx
+++ flixel/text/FlxBitmapText.hx
@@ -553,7 +553,7 @@ class FlxBitmapText extends FlxSprite
 			txtWidth = fieldWidth;
 		}
 		
-		frameWidth = txtWidth;
+		frameWidth = (txtWidth == 0) ? 1 : txtWidth;
 		frameHeight = (txtHeight == 0) ? 1 : txtHeight;
 	}
 	
