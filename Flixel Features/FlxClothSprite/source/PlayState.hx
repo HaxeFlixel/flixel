cc4fe55d2824b580d9b69b7c60b0e4d81dcb172f
Adriano Lima <adrianulima@hotmail.com>
2015-10-27 16:07:28 -0200

    Updates to work with last commit of FlxClothSprite
    Update `meshVelocity` and `pinnedSide` var names

--- Flixel Features/FlxClothSprite/source/PlayState.hx
+++ Flixel Features/FlxClothSprite/source/PlayState.hx
@@ -22,15 +22,15 @@ class PlayState extends FlxState
 		
 		super.create();
 		
-		// This show how to load graphics with animation, set pinSide, setMesh and gravity
+		// This show how to load graphics with animation, set pinnedSide, setMesh and gravity
 		flag = new FlxClothSprite(0, 0);
 		flag.loadGraphic("assets/images/flags.png", true, 140, 140);
 		flag.animation.add("anim", [0, 1, 2, 3], 3, true);
 		flag.animation.play("anim");
-		flag.pinSide = FlxObject.LEFT;
+		flag.pinnedSide = FlxObject.LEFT;
 		flag.setMesh(10, 10);
 		flag.maxVelocity.set(200, 200);
-		flag.gravity.y = 40;
+		flag.meshVelocity.y = 40;
 		add(flag);
 		
 		// This show how to set mesh scale, custom pinned points and set iterations
@@ -38,23 +38,23 @@ class PlayState extends FlxState
 		flag2.loadGraphic("assets/images/flags.png", true, 140, 140);
 		flag2.animation.add("anim", [0, 1, 2, 3], 3, true);
 		flag2.animation.play("anim");
-		flag2.pinSide = FlxObject.NONE;
+		flag2.pinnedSide = FlxObject.NONE;
 		flag2.meshScale.set(1.5, 1);
 		flag2.setMesh(15, 10, 0, 0, [0, 14]);
 		flag2.iterations = 8;
 		flag2.maxVelocity.set(200, 200);
-		flag2.gravity.y = 40;
+		flag2.meshVelocity.y = 40;
 		add(flag2);
 		
 		// This show how to load a simple graphic sprite with crossing constraints
 		rope = new FlxClothSprite(FlxG.width / 2 + 200, 10, "assets/images/rope.png", 2, 10, FlxObject.UP, true);
 		rope.maxVelocity.set(40, 40);
-		rope.gravity.y = 40;
+		rope.meshVelocity.y = 40;
 		add(rope);
 		
 		rope2 = new FlxClothSprite(FlxG.width / 2 - 200, 10, "assets/images/rope.png", 2, 10, FlxObject.UP, true);
 		rope2.maxVelocity.set(40, 40);
-		rope2.gravity.y = 40;
+		rope2.meshVelocity.y = 40;
 		add(rope2);
 		
 		var _txtInstruct:FlxText = new FlxText(0, FlxG.height - 20, FlxG.width, "Click -> Apply wind");
@@ -85,17 +85,17 @@ class PlayState extends FlxState
 		// Apply a random wind
 		if (FlxG.mouse.pressed)
 		{
-			flag.gravity.x = Math.random() * flag.maxVelocity.x;
-			flag2.gravity.x = Math.random() * flag2.maxVelocity.x;
-			rope.gravity.x = Math.random() * rope.maxVelocity.x;
-			rope2.gravity.x = Math.random() * rope2.maxVelocity.x;
+			flag.meshVelocity.x = Math.random() * flag.maxVelocity.x;
+			flag2.meshVelocity.x = Math.random() * flag2.maxVelocity.x;
+			rope.meshVelocity.x = Math.random() * rope.maxVelocity.x;
+			rope2.meshVelocity.x = Math.random() * rope2.maxVelocity.x;
 		}
 		else
 		{
-			flag.gravity.x = 0;
-			flag2.gravity.x = 0;
-			rope.gravity.x = 0;
-			rope2.gravity.x = 0;
+			flag.meshVelocity.x = 0;
+			flag2.meshVelocity.x = 0;
+			rope.meshVelocity.x = 0;
+			rope2.meshVelocity.x = 0;
 		}
 		
 		super.update(elapsed);
