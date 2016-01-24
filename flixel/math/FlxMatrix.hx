89408ae4979c9f89773db0b13f6b3d7685df69be
Gama11 <jensfischer95@gmail.com>
2016-01-24 20:50:43 +0100

    FlxMatrix: remove unnecessary constructor override
    This was a workaround for a bug in Haxe 3.1, which is not supported anymore.

--- flixel/math/FlxMatrix.hx
+++ flixel/math/FlxMatrix.hx
@@ -15,11 +15,6 @@ class FlxMatrix extends Matrix
 	 */
 	public static var matrix:FlxMatrix = new FlxMatrix();
 	
-	public function new(a:Float = 1, b:Float = 0, c:Float = 0, d:Float = 1, tx:Float = 0, ty:Float = 0)
-	{
-		super(a, b, c, d, tx, ty);
-	}
-	
 	/**
 	 * Rotates this matrix, but takes the values of sine and cosine,
 	 * so it might be useful when you rotate multiple matrices by the same angle
