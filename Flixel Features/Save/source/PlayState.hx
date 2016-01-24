27528863c543a48c2fd1d645f39da95a002f5d94
Gama11 <jensfischer95@gmail.com>
2016-01-24 17:50:03 +0100

    Save: make use of FlxButons' IFlxInput getters

--- Flixel Features/Save/source/PlayState.hx
+++ Flixel Features/Save/source/PlayState.hx
@@ -66,7 +66,7 @@ class PlayState extends FlxState
 			if (_gameSave.data.boxPositions != null)
 			{
 				box = new FlxButton(_gameSave.data.boxPositions[i].x, _gameSave.data.boxPositions[i].y, Std.string(i + 1));
-				//I'm using a FlxButton in this instance because I can use if (button.state == FlxButton.PRESSED) 
+				//I'm using a FlxButton in this instance because I can use if (button.pressed) 
 				//to detect if the mouse is held down on a button
 				_topText.text = "Loaded positions";
 			}
@@ -120,7 +120,7 @@ class PlayState extends FlxState
 		{
 			for (box in _boxGroup) 
 			{
-				if (box.status == FlxButton.PRESSED) 
+				if (box.pressed) 
 				{
 					// The offset is used to make the box stick to the cursor and not snap to the corner
 					dragOffset.set(box.x - FlxG.mouse.x, box.y - FlxG.mouse.y);
