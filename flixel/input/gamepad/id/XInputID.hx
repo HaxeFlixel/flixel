0f860cf11cadd69b8573a81b50961e8c987b594e
Lars Doucet <lars.doucet@gmail.com>
2016-02-05 10:49:26 -0600

    remove redundant #if !web block

--- flixel/input/gamepad/id/XInputID.hx
+++ flixel/input/gamepad/id/XInputID.hx
@@ -112,102 +112,101 @@ class XInputID
 	
 		
 	#elseif FLX_JOYSTICK_API
-		#if !web
 		
-			#if (windows || linux)
+		#if (windows || linux)
+		
+			// Button IDs
+			public static inline var A:Int = 0;
+			public static inline var B:Int = 1;
+			public static inline var X:Int = 2;
+			public static inline var Y:Int = 3;
 			
-				// Button IDs
-				public static inline var A:Int = 0;
-				public static inline var B:Int = 1;
-				public static inline var X:Int = 2;
-				public static inline var Y:Int = 3;
-				
-				public static inline var LB:Int = 4;
-				public static inline var RB:Int = 5;
-				
-				public static inline var BACK:Int = 6;
-				public static inline var START:Int = 7;
-				
-				#if linux
-
-				public static inline var LEFT_STICK_CLICK:Int = 9;
-				public static inline var RIGHT_STICK_CLICK:Int = 10;
-				public static inline var GUIDE:Int = 8;
-
-				#elseif windows
-
-				public static inline var LEFT_STICK_CLICK:Int = 8;
-				public static inline var RIGHT_STICK_CLICK:Int = 9;
-				public static inline var GUIDE:Int = 10;
-
-				#end
-				
-				//"fake" IDs, we manually watch for hat axis changes and then send events using these otherwise unused joystick button codes
-				public static inline var DPAD_UP:Int = 11;
-				public static inline var DPAD_DOWN:Int = 12;
-				public static inline var DPAD_LEFT:Int = 13;
-				public static inline var DPAD_RIGHT:Int = 14;
-				
-				// Axis indices
-				public static inline var LEFT_TRIGGER:Int = 2;
-				public static inline var RIGHT_TRIGGER:Int = 5;
-				
-				public static var LEFT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(0, 1);
-				public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(3, 4);
+			public static inline var LB:Int = 4;
+			public static inline var RB:Int = 5;
 			
-			#elseif mac
-				
-				// Button IDs
-				public static inline var A:Int = 0;
-				public static inline var B:Int = 1;
-				public static inline var X:Int = 2;
-				public static inline var Y:Int = 3;
-				
-				public static inline var LB:Int = 4;
-				public static inline var RB:Int = 5;
-				
-				public static inline var BACK:Int = 9;
-				public static inline var START:Int = 8;
-				
-				public static inline var LEFT_STICK_CLICK:Int = 6;
-				public static inline var RIGHT_STICK_CLICK:Int = 7;
-				
-				public static inline var GUIDE:Int = 10;
-				
-				public static inline var DPAD_UP:Int = 11;
-				public static inline var DPAD_DOWN:Int = 12;
-				public static inline var DPAD_LEFT:Int = 13;
-				public static inline var DPAD_RIGHT:Int = 14;
-				
-				// Axis indices
-				public static var LEFT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(0, 1);
-				public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(3, 4);
-				public static inline var LEFT_TRIGGER:Int = 2;
-				public static inline var RIGHT_TRIGGER:Int = 5;
-				
+			public static inline var BACK:Int = 6;
+			public static inline var START:Int = 7;
+			
+			#if linux
+
+			public static inline var LEFT_STICK_CLICK:Int = 9;
+			public static inline var RIGHT_STICK_CLICK:Int = 10;
+			public static inline var GUIDE:Int = 8;
+
+			#elseif windows
+
+			public static inline var LEFT_STICK_CLICK:Int = 8;
+			public static inline var RIGHT_STICK_CLICK:Int = 9;
+			public static inline var GUIDE:Int = 10;
+
 			#end
 			
-			//Analog stick and trigger values overlap with regular buttons so we remap to "fake" button ID's
-			public static function axisIndexToRawID(index:Int):Int
-			{
-				return   if (index == LEFT_ANALOG_STICK.x) LEFT_ANALOG_STICK_FAKE_X;
-					else if (index == LEFT_ANALOG_STICK.y) LEFT_ANALOG_STICK_FAKE_Y;
-					else if (index == RIGHT_ANALOG_STICK.x) RIGHT_ANALOG_STICK_FAKE_X;
-					else if (index == RIGHT_ANALOG_STICK.y) RIGHT_ANALOG_STICK_FAKE_Y;
-					else if (index == LEFT_TRIGGER) LEFT_TRIGGER_FAKE;
-					else if (index == RIGHT_TRIGGER) RIGHT_TRIGGER_FAKE;
-					else index;
-			}
-			//"fake" IDs
-			public static inline var LEFT_ANALOG_STICK_FAKE_X:Int = 15;
-			public static inline var LEFT_ANALOG_STICK_FAKE_Y:Int = 16;
+			//"fake" IDs, we manually watch for hat axis changes and then send events using these otherwise unused joystick button codes
+			public static inline var DPAD_UP:Int = 11;
+			public static inline var DPAD_DOWN:Int = 12;
+			public static inline var DPAD_LEFT:Int = 13;
+			public static inline var DPAD_RIGHT:Int = 14;
 			
-			public static inline var RIGHT_ANALOG_STICK_FAKE_X:Int = 17;
-			public static inline var RIGHT_ANALOG_STICK_FAKE_Y:Int = 18;
+			// Axis indices
+			public static inline var LEFT_TRIGGER:Int = 2;
+			public static inline var RIGHT_TRIGGER:Int = 5;
 			
-			public static inline var LEFT_TRIGGER_FAKE:Int = 19;
-			public static inline var RIGHT_TRIGGER_FAKE:Int = 20;
+			public static var LEFT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(0, 1);
+			public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(3, 4);
+		
+		#elseif mac
 			
+			// Button IDs
+			public static inline var A:Int = 0;
+			public static inline var B:Int = 1;
+			public static inline var X:Int = 2;
+			public static inline var Y:Int = 3;
+			
+			public static inline var LB:Int = 4;
+			public static inline var RB:Int = 5;
+			
+			public static inline var LEFT_STICK_CLICK:Int = 6;
+			public static inline var RIGHT_STICK_CLICK:Int = 7;
+			
+			public static inline var BACK:Int = 9;
+			public static inline var START:Int = 8;
+			
+			public static inline var GUIDE:Int = 10;
+			
+			public static inline var DPAD_UP:Int = 11;
+			public static inline var DPAD_DOWN:Int = 12;
+			public static inline var DPAD_LEFT:Int = 13;
+			public static inline var DPAD_RIGHT:Int = 14;
+			
+			// Axis indices
+			public static var LEFT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(0, 1);
+			public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(3, 4);
+			public static inline var LEFT_TRIGGER:Int = 2;
+			public static inline var RIGHT_TRIGGER:Int = 5;
+		
 		#end
+		
+		//Analog stick and trigger values overlap with regular buttons so we remap to "fake" button ID's
+		public static function axisIndexToRawID(index:Int):Int
+		{
+			return   if (index == LEFT_ANALOG_STICK.x) LEFT_ANALOG_STICK_FAKE_X;
+				else if (index == LEFT_ANALOG_STICK.y) LEFT_ANALOG_STICK_FAKE_Y;
+				else if (index == RIGHT_ANALOG_STICK.x) RIGHT_ANALOG_STICK_FAKE_X;
+				else if (index == RIGHT_ANALOG_STICK.y) RIGHT_ANALOG_STICK_FAKE_Y;
+				else if (index == LEFT_TRIGGER) LEFT_TRIGGER_FAKE;
+				else if (index == RIGHT_TRIGGER) RIGHT_TRIGGER_FAKE;
+				else index;
+		}
+		
+		//"fake" IDs
+		public static inline var LEFT_ANALOG_STICK_FAKE_X:Int = 15;
+		public static inline var LEFT_ANALOG_STICK_FAKE_Y:Int = 16;
+		
+		public static inline var RIGHT_ANALOG_STICK_FAKE_X:Int = 17;
+		public static inline var RIGHT_ANALOG_STICK_FAKE_Y:Int = 18;
+		
+		public static inline var LEFT_TRIGGER_FAKE:Int = 19;
+		public static inline var RIGHT_TRIGGER_FAKE:Int = 20;
+		
 	#end
 }
