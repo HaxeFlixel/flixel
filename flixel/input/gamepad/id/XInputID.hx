d87230d028ccf4f4acf0b6a0c3097a5d882c2c69
Lars Doucet <lars.doucet@gmail.com>
2016-02-04 21:37:25 -0600

    Update XInputID.hx

--- flixel/input/gamepad/id/XInputID.hx
+++ flixel/input/gamepad/id/XInputID.hx
@@ -5,12 +5,20 @@ import flixel.input.gamepad.FlxGamepad;
 /**
  * IDs for generic XInput controllers
  * 
+ * Compatible with the Xbox 360 controller, the Xbox One controller, and anything that masquerades as either of those.
+ *
  *-------
  * NOTES
  *-------
- * LINUX: we assumes the user is using the xpad driver, specifically Valve's version, steamos-xpad-dkms
+ *
+ * WINDOWS: we assume the user is using the default drivers that ship with windows.
+ *
+ * LINUX: we assume the user is using the xpad driver, specifically Valve's version, steamos-xpad-dkms
  * (we got weird errors when using xboxdrv). For full instructions on installation, see:
  * http://askubuntu.com/questions/165210/how-do-i-get-an-xbox-360-controller-working/441548#441548
+ *
+ * MAC: we assume the user is using the 360 Controller driver, specifically this one:
+ * https://github.com/360Controller/360Controller/releases
  */
 
 
