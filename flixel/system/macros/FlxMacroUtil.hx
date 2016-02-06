90aebe0fdd0c3ec568c6d2a7c3f9c60be9a6c635
Gama11 <jensfischer95@gmail.com>
2016-02-06 20:41:42 +0100

    FlxMacroUtil#buildMap(): reverse -> invert
    Doesn't really have anything to do with reversing, maps are unordered.

--- flixel/system/macros/FlxMacroUtil.hx
+++ flixel/system/macros/FlxMacroUtil.hx
@@ -8,8 +8,10 @@ class FlxMacroUtil
 {
 	/**
 	 * Builds a map from static uppercase inline variables in an abstract type.
+	 * 
+	 * @param	invert	Use the field value as the key and the name as the value
 	 */
-	public static macro function buildMap(typePath:String, reverse:Bool = false, ?exclude:Array<String>):Expr
+	public static macro function buildMap(typePath:String, invert:Bool = false, ?exclude:Array<String>):Expr
 	{
 		var type = Context.getType(typePath);
 		var values = [];
@@ -43,10 +45,11 @@ class FlxMacroUtil
 		}
 		
 		var finalExpr;
-		if (!reverse)
-			finalExpr = values.map(function(v) return macro $v{v.name} => $v{v.value});
-		else
+		if (invert)
 			finalExpr = values.map(function(v) return macro $v{v.value} => $v{v.name});
+		else
+			finalExpr = values.map(function(v) return macro $v{v.name} => $v{v.value});
+			
 			
 		return macro $a{finalExpr};
 	}
