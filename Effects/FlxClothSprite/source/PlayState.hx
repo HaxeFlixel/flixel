835d3544a10d86b89f8d5dc9a4927e5bc059f863
Gama11 <jensfischer95@gmail.com>
2016-02-10 19:17:16 +0100

    FlxClothSprite: remove the animations, minor cleanup

--- Effects/FlxClothSprite/source/PlayState.hx
+++ Effects/FlxClothSprite/source/PlayState.hx
@@ -1,9 +1,9 @@
 package;
 
-import flixel.addons.display.FlxClothSprite;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.addons.display.FlxClothSprite;
 import flixel.text.FlxText;
 
 /**
@@ -23,10 +23,7 @@ class PlayState extends FlxState
 		super.create();
 		
 		// This show how to load graphics with animation, set pinnedSide, setMesh and gravity
-		flag = new FlxClothSprite(0, 0);
-		flag.loadGraphic("assets/images/flags.png", true, 140, 140);
-		flag.animation.add("anim", [0, 1, 2, 3], 3, true);
-		flag.animation.play("anim");
+		flag = new FlxClothSprite(0, 0, "assets/images/flag.png");
 		flag.pinnedSide = FlxObject.LEFT;
 		flag.setMesh(10, 10);
 		flag.maxVelocity.set(200, 200);
@@ -34,10 +31,7 @@ class PlayState extends FlxState
 		add(flag);
 		
 		// This show how to set mesh scale, custom pinned points and set iterations
-		flag2 = new FlxClothSprite(FlxG.width / 2 - 105, 10);
-		flag2.loadGraphic("assets/images/flags.png", true, 140, 140);
-		flag2.animation.add("anim", [0, 1, 2, 3], 3, true);
-		flag2.animation.play("anim");
+		flag2 = new FlxClothSprite(FlxG.width / 2 - 105, 10, "assets/images/flag.png");
 		flag2.pinnedSide = FlxObject.NONE;
 		flag2.meshScale.set(1.5, 1);
 		flag2.setMesh(15, 10, 0, 0, [0, 14]);
@@ -57,38 +51,30 @@ class PlayState extends FlxState
 		rope2.meshVelocity.y = 40;
 		add(rope2);
 		
-		var _txtInstruct:FlxText = new FlxText(0, FlxG.height - 20, FlxG.width, "Click -> Apply wind");
-		_txtInstruct.alignment = CENTER;
-		add(_txtInstruct);
-	}
-	
-	override public function destroy():Void
-	{
-		flag = null;
-		flag2 = null;
-		rope = null;
-		rope2 = null;
-		
-		super.destroy();
+		var instruction = new FlxText(0, FlxG.height - 20, FlxG.width, "Click -> Apply wind");
+		instruction.alignment = CENTER;
+		add(instruction);
 	}
 	
 	override public function update(elapsed:Float):Void
 	{
-		// Apply a volocity to the object based on mojuse position
+		var halfHeight = flag.frameHeight / 2;
+		
+		// Apply a volocity to the object based on mouse position
 		flag.velocity.x = FlxG.mouse.x - flag.x;
-		flag.velocity.y = FlxG.mouse.y - flag.y;
+		flag.velocity.y = FlxG.mouse.y - flag.y - halfHeight;
 		
 		// Move the flag in the direction of mouse
-		flag.x += (FlxG.mouse.x - flag.x) / 4;
-		flag.y += (FlxG.mouse.y - flag.y) / 4;
+		flag.x += (FlxG.mouse.x - flag.x);
+		flag.y += (FlxG.mouse.y - flag.y - halfHeight);
 		
 		// Apply a random wind
 		if (FlxG.mouse.pressed)
 		{
-			flag.meshVelocity.x = Math.random() * flag.maxVelocity.x;
-			flag2.meshVelocity.x = Math.random() * flag2.maxVelocity.x;
-			rope.meshVelocity.x = Math.random() * rope.maxVelocity.x;
-			rope2.meshVelocity.x = Math.random() * rope2.maxVelocity.x;
+			flag.meshVelocity.x = FlxG.random.float(0, flag.maxVelocity.x);
+			flag2.meshVelocity.x = FlxG.random.float(0, flag2.maxVelocity.x);
+			rope.meshVelocity.x = FlxG.random.float(0, rope.maxVelocity.x);
+			rope2.meshVelocity.x = FlxG.random.float(0, rope2.maxVelocity.x);
 		}
 		else
 		{
