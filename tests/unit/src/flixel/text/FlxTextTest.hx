edf3b0ad84516ce3b88423976850a36a53997317
Adriano Lima <adrianulima@hotmail.com>
2016-02-18 16:49:07 -0200

    Added a unit test
    Test if in the frame of the text have one pixel different of white and
    transparent

--- tests/unit/src/flixel/text/FlxTextTest.hx
+++ tests/unit/src/flixel/text/FlxTextTest.hx
@@ -3,6 +3,7 @@ package flixel.text;
 import flixel.system.FlxAssets;
 import flixel.text.FlxText;
 import flixel.text.FlxText.FlxTextAlign;
+import flixel.util.FlxColor;
 import massive.munit.Assert;
 
 class FlxTextTest extends FlxTest
@@ -39,5 +40,23 @@ class FlxTextTest extends FlxTest
 		Assert.isNotNull(text.graphic);
 		Assert.areNotEqual(0, text.frameWidth);
 		Assert.areNotEqual(0, text.frameHeight);
+	}
+	
+	@Test // #1422
+	function testBlurryLines()
+	{
+		text = new FlxText(0, 0, 120, "Text with some blur\none line without blur\nand another with blur");
+		text.alignment = FlxTextAlign.CENTER;
+		text.drawFrame();
+		
+		var graphic = text.updateFramePixels();
+		for (x in 0...graphic.width)
+		{
+			for (y in 0...graphic.height)
+			{
+				var color = graphic.getPixel32(x, y);
+				Assert.isFalse(color != FlxColor.WHITE && color != FlxColor.TRANSPARENT);
+			}
+		}
 	}
 }
