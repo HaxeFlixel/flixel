888f742d020a1d4d0d51d80e66c4dbfbb5323d08
Lars Doucet <lars.doucet@gmail.com>
2016-02-26 11:45:17 -0600

    d'oh (+8 squashed commit)
    Squashed commit:
    
    [08aa03d] better "null" check
    
    [dc9a674] fix ifs
    
    [1c9febd] fix crash on legacy
    
    [ffe6838] minor cleanup
    
    [f72cace] this needs to be outside the #if flash conditional, that's probably why I had it at the bottom before
    
    [3ded9b2] style fixes / cleanup
    
    [4d1dd74] quick fix
    
    [7c77870] Make analog sticks emit "fake" digital actions for left/right/up/down movement, and add ID's that make it simple to use them. Makes it easy to use an Analog stick as if it were a DPAD, a very popular input pattern.
    
    This behavior was already implemented on legacy, but the API for it was buried. I added top level FlxGamepadInputID's for the digital events, and refactored things so that Next can emit these events as well.
    Also, I added a "value" property to the FlxGamepadButton for storing last frame's analog input value, which should remove the requirement for FlxActions to call check() every frame in order to get accurate JUST_MOVED / JUST_STOPPED behavior. (+6 squashed commit)
    
    Squashed commit:
    
    [1c9febd] fix crash on legacy
    
    [ffe6838] minor cleanup
    
    [f72cace] this needs to be outside the #if flash conditional, that's probably why I had it at the bottom before
    
    [3ded9b2] style fixes / cleanup
    
    [4d1dd74] quick fix
    
    [7c77870] Make analog sticks emit "fake" digital actions for left/right/up/down movement, and add ID's that make it simple to use them. Makes it easy to use an Analog stick as if it were a DPAD, a very popular input pattern.
    
    This behavior was already implemented on legacy, but the API for it was buried. I added top level FlxGamepadInputID's for the digital events, and refactored things so that Next can emit these events as well.
    Also, I added a "value" property to the FlxGamepadButton for storing last frame's analog input value, which should remove the requirement for FlxActions to call check() every frame in order to get accurate JUST_MOVED / JUST_STOPPED behavior. (+4 squashed commit)
    
    Squashed commit:
    
    [f72cace] this needs to be outside the #if flash conditional, that's probably why I had it at the bottom before
    
    [3ded9b2] style fixes / cleanup
    
    [4d1dd74] quick fix
    
    [7c77870] Make analog sticks emit "fake" digital actions for left/right/up/down movement, and add ID's that make it simple to use them. Makes it easy to use an Analog stick as if it were a DPAD, a very popular input pattern.
    
    This behavior was already implemented on legacy, but the API for it was buried. I added top level FlxGamepadInputID's for the digital events, and refactored things so that Next can emit these events as well.
    Also, I added a "value" property to the FlxGamepadButton for storing last frame's analog input value, which should remove the requirement for FlxActions to call check() every frame in order to get accurate JUST_MOVED / JUST_STOPPED behavior. (+2 squashed commit)
    
    Squashed commit:
    
    [4d1dd74] quick fix
    
    [7c77870] Make analog sticks emit "fake" digital actions for left/right/up/down movement, and add ID's that make it simple to use them. Makes it easy to use an Analog stick as if it were a DPAD, a very popular input pattern.
    
    This behavior was already implemented on legacy, but the API for it was buried. I added top level FlxGamepadInputID's for the digital events, and refactored things so that Next can emit these events as well.
    Also, I added a "value" property to the FlxGamepadButton for storing last frame's analog input value, which should remove the requirement for FlxActions to call check() every frame in order to get accurate JUST_MOVED / JUST_STOPPED behavior.

--- flixel/input/gamepad/id/MFiID.hx
+++ flixel/input/gamepad/id/MFiID.hx
@@ -25,8 +25,8 @@ class MFiID
 	public static inline var DPAD_LEFT:Int = 19;
 	public static inline var DPAD_RIGHT:Int = 20;
 	
-	public static var LEFT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(0, 1);
-	public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(2, 3);
+	public static var LEFT_ANALOG_STICK (default, null) = new FlxGamepadAnalogStick(0, 1, {up:21, down:22, left:23, right:24});
+	public static var RIGHT_ANALOG_STICK(default, null) = new FlxGamepadAnalogStick(2, 3, {up:25, down:26, left:27, right:28});
 	
 	public static inline var LEFT_TRIGGER:Int = 4;
 	public static inline var RIGHT_TRIGGER:Int = 5;
