9aa43952f66f1ddf067ed8589decdab03747fb49
Gama11 <jensfischer95@gmail.com>
2016-02-27 22:05:04 +0100

    Code style: remove byte order marks
    This causes issues with checkstyle (adireddy/haxe-checkstyle#44).
    [skip ci]

--- flixel/tweens/motion/Motion.hx
+++ flixel/tweens/motion/Motion.hx
@@ -1,4 +1,4 @@
-﻿package flixel.tweens.motion;
+package flixel.tweens.motion;
 
 import flixel.FlxObject;
 import flixel.tweens.FlxTween;
--- flixel/tweens/motion/QuadMotion.hx
+++ flixel/tweens/motion/QuadMotion.hx
@@ -1,4 +1,4 @@
-﻿package flixel.tweens.motion;
+package flixel.tweens.motion;
 
 import flixel.math.FlxPoint;
 
