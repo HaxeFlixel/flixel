081f39461cb5288f59b71f1d54cabf5fc3d6576f
Gama11 <jensfischer95@gmail.com>
2016-03-01 10:38:05 +0100

    checkstyle: set allowEmptyLineAfterMultiLineComment to false

--- tests/unit/src/TestSuite.hx
+++ tests/unit/src/TestSuite.hx
@@ -53,7 +53,6 @@ import FlxTest;
  * Auto generated Test Suite for MassiveUnit.
  * Refer to munit command line tool for more information (haxelib run munit)
  */
-
 class TestSuite extends massive.munit.TestSuite
 {		
 
