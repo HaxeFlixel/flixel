2776fe37261612042e2204cdeedd35d3206cd1ff
Seanw265 <seanw265@gmail.com>
2016-04-22 14:28:18 -0400

    Update FlxMouse.hx

--- flixel/input/mouse/FlxMouse.hx
+++ flixel/input/mouse/FlxMouse.hx
@@ -397,8 +397,8 @@ class FlxMouse extends FlxPointer implements IFlxInputManager
 		//actually position the flixel mouse cursor graphic
 		if (visible)
 		{
-			cursorContainer.x = FlxG.game.mouseX * FlxG.scaleMode.mouseMultiplier.x;
-			cursorContainer.y = FlxG.game.mouseY * FlxG.scaleMode.mouseMultiplier.y;
+			cursorContainer.x = FlxG.game.mouseX * FlxG.scaleMode.pointerMultiplier.x;
+			cursorContainer.y = FlxG.game.mouseY * FlxG.scaleMode.pointerMultiplier.y;
 		}
 		#end
 		
