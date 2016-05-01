62c0d21403cc38d7d97402b830b233f9ac6cd313
Gama11 <jensfischer95@gmail.com>
2016-05-01 10:33:16 +0200

    FlxSound docs improvements, minor style cleanup
    The description of time was a bit wordy.

--- flixel/system/debug/console/ConsoleUtil.hx
+++ flixel/system/debug/console/ConsoleUtil.hx
@@ -165,13 +165,15 @@ private class Interp extends hscript.Interp
 	
 	override function get(o:Dynamic, f:String):Dynamic
 	{
-		if (o == null) error(EInvalidAccess(f));
+		if (o == null)
+			error(EInvalidAccess(f));
 		return Reflect.getProperty(o, f);
 	}
 	
 	override function set(o:Dynamic, f:String, v:Dynamic):Dynamic
 	{
-		if (o == null) error(EInvalidAccess(f));
+		if (o == null)
+			error(EInvalidAccess(f));
 		Reflect.setProperty(o, f, v);
 		return v;
 	}
