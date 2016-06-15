HaxeFlixel/flixel

commit df981da19f9bf1f74e673d20c88a961c48a0016c

FlxPreloader: make sure overrides still compile on HTML5
closes #1803

flixel/system/FlxBasePreloader.hx
@@ -16,7 +16,6 @@ import flixel.util.FlxStringUtil;
 
 class FlxBasePreloader extends NMEPreloader
 {
-	#if !js
 	/**
 	 * Add this string to allowedURLs array if you want to be able to test game with enabled site-locking on local machine 
 	 */
@@ -29,13 +28,13 @@ class FlxBasePreloader extends NMEPreloader
 	
 	/**
 	 * List of allowed URLs for built-in site-locking.
-	 * Set it in FlxPreloader's constructor as: ['http://adamatomic.com/canabalt/', FlxPreloader.LOCAL];
+	 * Set it in FlxPreloader's constructor as: `['http://adamatomic.com/canabalt/', FlxPreloader.LOCAL]`;
 	 */
 	public var allowedURLs:Array<String>;
 
 	/**
 	 * The index of which URL in allowedURLs will be triggered when a user clicks on the Site-lock Message.
-	 * For example, if allowedURLs is ['mysite.com', 'othersite.com'], and siteLockURLIndex = 1, then
+	 * For example, if allowedURLs is `['mysite.com', 'othersite.com']`, and `siteLockURLIndex = 1`, then
 	 * the user will go to 'othersite.com' when they click the message, but sitelocking will allow either of those URLs to work.
 	 * Defaults to 0.
 	 */
@@ -48,7 +47,7 @@ class FlxBasePreloader extends NMEPreloader
 	private var _urlChecked:Bool = false;
 	
 	/**
-	 * FlxPreloaderBase Constructor.
+	 * FlxBasePreloader Constructor.
 	 * @param	MinDisplayTime	Minimum time the preloader should be shown. (Default = 0)
 	 * @param	AllowedURLs		Allowed URLs used for Site-locking. If the game is run anywhere else, a message will be displayed on the screen (Default = [])
 	 */
@@ -56,6 +55,7 @@ class FlxBasePreloader extends NMEPreloader
 	{
 		super();
 		
+		#if !js
 		removeChild(progress);
 		removeChild(outline);
 		
@@ -64,6 +64,7 @@ class FlxBasePreloader extends NMEPreloader
 			allowedURLs = AllowedURLs;
 		else
 			allowedURLs = [];
+		#end
 	}
 	
 	/**
@@ -77,12 +78,14 @@ class FlxBasePreloader extends NMEPreloader
 	override public function onInit() 
 	{
 		super.onInit();
+		
+		#if !js
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		Lib.current.stage.align = StageAlign.TOP_LEFT;
 		create();
 		checkSiteLock();
 		addEventListener(Event.ENTER_FRAME, onEnterFrame);
-
+		#end
 	}
 	
 	/**
@@ -91,10 +94,12 @@ class FlxBasePreloader extends NMEPreloader
 	 */
 	override public function onUpdate(bytesLoaded:Int, bytesTotal:Int) 
 	{
-		#if web
+		#if flash
 		if (root.loaderInfo.bytesTotal == 0)
 			bytesTotal = 50000;
-		_percent = (bytesTotal != 0)?bytesLoaded / bytesTotal : 0;
+		_percent = (bytesTotal != 0) ? bytesLoaded / bytesTotal : 0;
+		#else
+		super.onUpdate(bytesLoaded, bytesTotal);
 		#end
 	}
 	
@@ -137,9 +142,13 @@ class FlxBasePreloader extends NMEPreloader
 	 * Highly recommended you DO NO override.
 	 */
 	override public function onLoaded() 
-	{	
+	{
+		#if flash
 		_loaded = true;
 		_percent = 1;
+		#else
+		super.onLoaded();
+		#end
 	}
 	
 	/**
@@ -216,6 +225,5 @@ class FlxBasePreloader extends NMEPreloader
 		}
 		return false;
 	}
-	#end
 	#end
 }

flixel/system/FlxPreloader.hx
@@ -22,7 +22,6 @@ private class GraphicLogoCorners extends BitmapData {}
  */
 class FlxPreloader extends FlxBasePreloader
 {
-	#if !js
 	private var _buffer:Sprite;
 	private var _bmpBar:Bitmap;
 	private var _text:TextField;
@@ -31,14 +30,15 @@ class FlxPreloader extends FlxBasePreloader
 	
 	/**
 	 * Initialize your preloader here.
+	 * 
+	 * ```haxe
+	 * super(0, ["test.com", FlxPreloaderBase.LOCAL]); // example of site-locking
+	 * super(10); // example of long delay (10 seconds)
+	 * ```
 	 */
 	override public function new(MinDisplayTime:Float = 0, ?AllowedURLs:Array<String>):Void
 	{
 		super(MinDisplayTime, AllowedURLs);
-		
-		// super(0, ["test.com", FlxPreloaderBase.LOCAL]); // example of site-locking
-		
-		// super(10); // example of long delay (10 seconds)
 	}
 	
 	/**
@@ -48,6 +48,7 @@ class FlxPreloader extends FlxBasePreloader
 	 */
 	override private function create():Void
 	{
+		#if !js
 		_buffer = new Sprite();
 		_buffer.scaleX = _buffer.scaleY = 2;
 		addChild(_buffer);
@@ -107,6 +108,7 @@ class FlxPreloader extends FlxBasePreloader
 		bitmap.blendMode = BlendMode.OVERLAY;
 		bitmap.alpha = 0.25;
 		_buffer.addChild(bitmap);
+		#end
 		
 		super.create();
 	}
@@ -117,6 +119,7 @@ class FlxPreloader extends FlxBasePreloader
 	 */
 	override private function destroy():Void
 	{
+		#if !js
 		if (_buffer != null)	
 		{
 			removeChild(_buffer);
@@ -127,6 +130,7 @@ class FlxPreloader extends FlxBasePreloader
 		_logo = null;
 		_logoGlow = null;
 		super.destroy();
+		#end
 	}
 	
 	/**
@@ -135,6 +139,7 @@ class FlxPreloader extends FlxBasePreloader
 	 */
 	override public function update(Percent:Float):Void
 	{
+		#if !js
 		_bmpBar.scaleX = Percent * (_width - 8);
 		_text.text = Std.string(FlxG.VERSION) + " " + Std.int(Percent * 100) + "%";
 		
@@ -172,6 +177,6 @@ class FlxPreloader extends FlxBasePreloader
 		{
 			_buffer.alpha = 1 - (Percent - 0.9) / 0.1;
 		}
+		#end
 	}
-	#end
 }

tests/unit/src/flixel/system/FlxPreloaderTest.hx
@@ -1,0 +1,33 @@
+package flixel.system;
+
+class FlxPreloaderTest 
+{
+	@Test // #1803
+	function testOverridePreloader()
+	{
+		new OverridenPreloader();
+	}
+}
+
+class OverridenPreloader extends FlxPreloader
+{
+	public function new(minDisplayTime:Float = 0, ?allowedUrls:Array<String>)
+    {
+        super(minDisplayTime, allowedUrls);
+    }
+	
+	override private function create()
+	{
+		super.create();
+	}
+	
+	override private function destroy()
+	{
+		super.destroy();
+	}
+	
+	override public function update(percent:Float):Void
+	{
+		super.update(percent);
+	}
+}

commit feed6c5265c78b065820b49f59535d65e324138e

Fix keyboard input with OpenFL next, closes #1827
Caused by df981da.

flixel/system/FlxBasePreloader.hx
@@ -143,11 +143,11 @@ class FlxBasePreloader extends NMEPreloader
 	 */
 	override public function onLoaded() 
 	{
-		#if flash
+		#if js
+		super.onLoaded();
+		#else
 		_loaded = true;
 		_percent = 1;
-		#else
-		super.onLoaded();
 		#end
 	}
 	