HaxeFlixel/flixel

commit 00a39e6135af66ffd188e7b97f3f7a2ed98fcf43

FlxGitSHA: simplify getProcessOutput()
(a lot)

flixel/system/macros/FlxGitSHA.hx
@@ -17,7 +17,7 @@ class FlxGitSHA
 	{
 		var fields:Array<Field> = Context.getBuildFields();
 		var libraryPath:String;
-		var sha:String;
+		var sha:String = "";
 		
 		// make sure the build isn't cancelled if a Sys call fails
 		try
@@ -25,10 +25,7 @@ class FlxGitSHA
 			libraryPath = getLibraryPath(library);
 			sha = getGitSHA(libraryPath);
 		}
-		catch (_:Dynamic)
-		{
-			sha = "";
-		}
+		catch (_:Dynamic) {}
 		
 		fields.push({
 			name: "sha",
@@ -68,9 +65,7 @@ class FlxGitSHA
 		var sha = getProcessOutput("git", ["rev-parse", "HEAD"]);
 		var shaRegex = ~/[a-f0-9]{40}/g;
 		if (!shaRegex.match(sha))
-		{
 			sha = "";
-		}
 		
 		Sys.setCwd(oldWd);
 		return sha;
@@ -78,36 +73,7 @@ class FlxGitSHA
 	
 	public static function getProcessOutput(cmd:String, args:Array<String>):String
 	{
-		var output = "";
-		
-		try
-		{
-			var process = new Process(cmd, args);
-			var buffer = new BytesOutput();
-			
-			while (true)
-			{
-				try
-				{
-					var currentOutput = process.stdout.readAll(1024);
-					buffer.write(currentOutput);
-					if (currentOutput.length == 0)
-					{
-						break;
-					}
-				}
-				catch (e:Eof)
-				{
-					break;
-				}
-			}
-			
-			process.close();
-			output = buffer.getBytes().toString();
-		}
-		catch (e:Dynamic) {}
-		
-		return output;
+		return new Process(cmd, args).stdout.readAll().toString();
 	}
 }
 #end

flixel/system/macros/FlxAssetPaths.hx
@@ -1,5 +1,6 @@
 package flixel.system.macros;
 
+#if macro
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import sys.FileSystem;
@@ -82,3 +83,4 @@ private class FileReference
 		this.documentation = "\"" + value + "\" (auto generated).";
 	}
 }
+#end

commit 745170b062107ffce3df4b9b6f2180c3e5454284

FlxGitSHA: fix "Type not found : NMEPreloader" compiler errors
This happened only if Git was not available on the command line, which led to a "process creation failure" exception. This meant that the macro wouldn't return to the previous working directory, throwing off Lime's build process.

Caused by 00a39e6.

flixel/system/macros/FlxGitSHA.hx
@@ -71,7 +71,14 @@ class FlxGitSHA
 	
 	public static function getProcessOutput(cmd:String, args:Array<String>):String
 	{
-		return new Process(cmd, args).stdout.readAll().toString();
+		try
+		{
+			return new Process(cmd, args).stdout.readAll().toString();
+		}
+		catch (_:Dynamic)
+		{
+			return "";
+		}
 	}
 }
 #end