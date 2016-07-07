c216cf9b58911b20c27b324a9cd76fe1c879a384
Nallebeorn <mafortion.benjamin@gmail.com>
2016-07-07 13:11:13 +0200

    Decrease FlxGroup.length in FlxGroup.remove(). Also, increase length in add() even when replacing a null entry.

--- flixel/group/FlxGroup.hx
+++ flixel/group/FlxGroup.hx
@@ -189,10 +189,7 @@ class FlxTypedGroup<T:FlxBasic> extends FlxBasic
 		{
 			members[index] = Object;
 			
-			if (index >= length)
-			{
-				length = index + 1;
-			}
+			length++;
 			
 			return Object;
 		}
@@ -363,6 +360,8 @@ class FlxTypedGroup<T:FlxBasic> extends FlxBasic
 		else
 			members[index] = null;
 		
+		length--;
+			
 		return Object;
 	}
 	
