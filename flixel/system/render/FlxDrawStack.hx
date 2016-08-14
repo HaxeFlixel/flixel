ee30d7107c6f46475d5af351d395f66df9e50067
Zaphod <zaphy@yandex.ru>
2016-08-14 18:50:38 +0300

    starting work on draw stack which should work for both hardware renderers

---
+++ flixel/system/render/FlxDrawStack.hx
@@ -0,0 +1,17 @@
+package flixel.system.render;
+
+/**
+ * ...
+ * @author Zaphod
+ */
+class FlxDrawStack
+{
+
+	// TODO: use this class in FlxTilesheetView and FlxGlView
+	
+	public function new() 
+	{
+		
+	}
+	
+}
