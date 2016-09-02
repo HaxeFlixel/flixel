HaxeFlixel/flixel

commit e27749909db3a561bb2a8fd2cec14be42cd77455

Mode: use the AssetPaths macro for images

Platformers/Mode/source/MenuState.hx
@@ -29,9 +29,7 @@ class MenuState extends FlxState
 		FlxG.cameras.bgColor = 0xff131c1b;
 		
 		if (FlxG.sound.music != null)
-		{
 			FlxG.sound.music.stop();
-		}
 		
 		// Simple use of flixel save game object.
 		// Tracks number of times the game has been played.
@@ -40,13 +38,9 @@ class MenuState extends FlxState
 		if (save.bind("Mode"))
 		{
 			if (save.data.plays == null)
-			{
 				save.data.plays = 0.0;
-			}
 			else
-			{
 				save.data.plays++;
-			}
 			
 			FlxG.log.add("Number of plays: " + save.data.plays);
 			save.close();
@@ -59,7 +53,7 @@ class MenuState extends FlxState
 		_gibs.velocity.set(0, -200, 0, -20);
 		_gibs.angularVelocity.set( -720, 720);
 		_gibs.acceleration.set(0, 100);
-		_gibs.loadParticles(Reg.SPAWNER_GIBS, 650, 32, true);
+		_gibs.loadParticles(AssetPaths.spawner_gibs__png, 650, 32, true);
 		_gibs.lifespan.set(5, 5);
 		add(_gibs);
 		
@@ -86,7 +80,7 @@ class MenuState extends FlxState
 		_attractMode = false;
 		
 		#if !FLX_NO_MOUSE
-		FlxG.mouse.load(Reg.CURSOR, 2);
+		FlxG.mouse.load(AssetPaths.cursor__png, 2);
 		FlxG.mouse.visible = true;
 		#end
 		
@@ -134,8 +128,7 @@ class MenuState extends FlxState
 			// Then we're going to add the text and buttons and things that appear
 			// If we were hip we'd use our own button animations, but we'll just recolor
 			// the stock ones for now instead.
-			var text:FlxText;
-			text = new FlxText(FlxG.width / 2 - 50, FlxG.height / 3 + 39, 100, "by Adam Atomic");
+			var text = new FlxText(FlxG.width / 2 - 50, FlxG.height / 3 + 39, 100, "by Adam Atomic");
 			text.alignment = CENTER;
 			text.color = 0x3a5c39;
 			add(text);
@@ -145,18 +138,16 @@ class MenuState extends FlxState
 			text.alignment = CENTER;
 			add(text);
 			
-			var flixelButton:FlxButton = new FlxButton(FlxG.width / 2 - 40, FlxG.height / 3 + 54, "haxeflixel.com", function()
-			{
-				FlxG.openURL("http://haxeflixel.com");
-			});
+			var flixelButton = new FlxButton(FlxG.width / 2 - 40, FlxG.height / 3 + 54, "haxeflixel.com", function()
+				FlxG.openURL("http://haxeflixel.com")
+			);
 			flixelButton.color = 0xff729954;
 			flixelButton.label.color = 0xffd8eba2;
 			add(flixelButton);
 			
-			var dannyButton:FlxButton = new FlxButton(flixelButton.x, flixelButton.y + 22, "music: dannyB", function()
-			{
-				FlxG.openURL("http://dbsoundworks.com");
-			});
+			var dannyButton = new FlxButton(flixelButton.x, flixelButton.y + 22, "music: dannyB", function()
+				FlxG.openURL("http://dbsoundworks.com")
+			);
 			dannyButton.color = flixelButton.color;
 			dannyButton.label.color = flixelButton.label.color;
 			add(dannyButton);
@@ -172,9 +163,7 @@ class MenuState extends FlxState
 		_timer += elapsed;
 		
 		if (_timer >= 10) //go into demo mode if no buttons are pressed for 10 seconds
-		{
 			_attractMode = true;
-		}
 		
 		#if !FLX_NO_KEYBOARD
 		if (!_fading)
@@ -189,9 +178,7 @@ class MenuState extends FlxState
 			}
 			
 			if (FlxG.keys.pressed.R && !_attractMode)
-			{
 				_attractMode = true;
-			}
 		}
 		#end
 		
@@ -224,9 +211,7 @@ class MenuState extends FlxState
 				new PlayState(), ["ANY"], 22, onDemoComplete);
 		}
 		else
-		{
 			FlxG.switchState(new PlayState());
-		}
 	}
 	
 	/**

Platformers/Mode/Project.xml
@@ -79,8 +79,7 @@
 	
 	<!--Place custom nodes like icons here (higher priority to override the HaxeFlixel icon)-->
 	
-	<assets path="assets/images" rename="images" />
-	<assets path="assets/data" rename="data" />
+	<assets path="assets" exclude="*.ogg|*.mp3" />
 	<icon path="assets/images/bot.png" />
 	
 	<assets path="assets/sounds" if="flash">

Platformers/Mode/source/AssetPaths.hx
@@ -1,0 +1,4 @@
+package;
+
+@:build(flixel.system.FlxAssets.buildFileReferences("assets", true))
+class AssetPaths {}

Platformers/Mode/source/Bullet.hx
@@ -14,7 +14,7 @@ class Bullet extends FlxSprite
 	{
 		super();
 		
-		loadGraphic(Reg.BULLET, true);
+		loadGraphic(AssetPaths.bullet__png, true);
 		width = 6;
 		height = 6;
 		offset.set(1, 1);

Platformers/Mode/source/Enemy.hx
@@ -59,9 +59,9 @@ class Enemy extends FlxSprite
 		super();
 		
 		#if flash
-		loadRotatedGraphic(Reg.BOT, 64, 0, false, false);
+		loadRotatedGraphic(AssetPaths.bot__png, 64, 0, false, false);
 		#else
-		loadGraphic(Reg.BOT);
+		loadGraphic(AssetPaths.bot__png);
 		#end
 
 		// We want the enemy's "hit box" or actual size to be
@@ -73,7 +73,7 @@ class Enemy extends FlxSprite
 		// Here we are setting up the jet particles
 		// that shoot out the back of the ship.
 		_jets = new FlxEmitter();
-		_jets.loadParticles(Reg.JET, 15, 0, false);
+		_jets.loadParticles(AssetPaths.jet__png, 15, 0, false);
 		
 		// These parameters help control the ship's
 		// speed and direction during the update() loop.

Platformers/Mode/source/EnemyBullet.hx
@@ -12,7 +12,7 @@ class EnemyBullet extends FlxSprite
 	public function new()
 	{
 		super();
-		loadGraphic(Reg.BOT_BULLET, true);
+		loadGraphic(AssetPaths.bot_bullet__png, true);
 		animation.add("idle",[0, 1], 50);
 		animation.add("poof",[2, 3, 4], 50, false);
 		speed = 120;

Platformers/Mode/source/PlayState.hx
@@ -68,7 +68,7 @@ class PlayState extends FlxState
 		_littleGibs.angularVelocity.set( -720);
 		_littleGibs.acceleration.set(0, 350);
 		_littleGibs.elasticity.set(0.5);
-		_littleGibs.loadParticles(Reg.GIBS, 100, 10, true);
+		_littleGibs.loadParticles(AssetPaths.gibs__png, 100, 10, true);
 		
 		// Next we create a smaller pool of larger metal bits for exploding.
 		_bigGibs = new FlxEmitter();
@@ -76,7 +76,7 @@ class PlayState extends FlxState
 		_bigGibs.angularVelocity.set( -720, -720);
 		_bigGibs.acceleration.set(0, 350);
 		_bigGibs.elasticity.set(0.35);
-		_bigGibs.loadParticles(Reg.SPAWNER_GIBS, 50, 20, true);
+		_bigGibs.loadParticles(AssetPaths.spawner_gibs__png, 50, 20, true);
 		
 		// Then we'll set up the rest of our object groups or pools
 		_enemies = new FlxTypedGroup<Enemy>(50);
@@ -409,7 +409,7 @@ class PlayState extends FlxState
 		
 		_tileMap = new FlxTilemap();
 		_tileMap.useScaleHack = true;
-		_tileMap.loadMapFromArray(_map, MAP_WIDTH_IN_TILES, MAP_HEIGHT_IN_TILES, Reg.IMG_TILES, 8, 8);
+		_tileMap.loadMapFromArray(_map, MAP_WIDTH_IN_TILES, MAP_HEIGHT_IN_TILES, AssetPaths.img_tiles__png, 8, 8);
 		add(_tileMap);
 	}
 	
@@ -478,7 +478,7 @@ class PlayState extends FlxState
 			_spawners.add(sp);
 			
 			// Then create a dedicated camera to watch the spawner
-			var miniFrame:FlxSprite = new FlxSprite(3 + (_spawners.length - 1) * 16, 3, Reg.MINI_FRAME);
+			var miniFrame:FlxSprite = new FlxSprite(3 + (_spawners.length - 1) * 16, 3, AssetPaths.miniframe__png);
 			_hud.add(miniFrame);
 			
 			var ratio:Float = FlxCamera.defaultZoom / 2;

Platformers/Mode/source/Player.hx
@@ -40,7 +40,7 @@ class Player extends FlxSprite
 	{
 		super(X, Y);
 		
-		loadGraphic(Reg.SPACEMAN, true, 8);
+		loadGraphic(AssetPaths.spaceman__png, true, 8);
 		
 		setFacingFlip(FlxObject.LEFT, true, false);
 		setFacingFlip(FlxObject.RIGHT, false, false);

commit 86c0df8efaa888fc42658b58cc48868991d32ffd

Mode: fix the path to the replay files
Probably broken since e277499

Platformers/Mode/source/MenuState.hx
@@ -207,7 +207,7 @@ class MenuState extends FlxState
 		if (_attractMode)
 		{
 			FlxG.vcr.loadReplay(
-				Assets.getText('data/attract${FlxG.random.int(1, 2)}.fgr'),
+				Assets.getText('assets/data/attract${FlxG.random.int(1, 2)}.fgr'),
 				new PlayState(), ["ANY"], 22, onDemoComplete);
 		}
 		else