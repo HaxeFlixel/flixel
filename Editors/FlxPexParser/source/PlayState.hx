36b99f536d008385e07197d0accb9d86a15e8c3a
Gama11 <jensfischer95@gmail.com>
2016-09-06 23:21:03 +0200

    FlxPexParser: hold right mouse to change scale
    (instead of toggle)
    Also added an instructions text.

--- Editors/FlxPexParser/source/PlayState.hx
+++ Editors/FlxPexParser/source/PlayState.hx
@@ -4,12 +4,12 @@ import flixel.addons.editors.pex.FlxPexParser;
 import flixel.effects.particles.FlxEmitter;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	var emitter:FlxEmitter;
-	var emitterScale:Float = 1;
-	
+
 	override public function create():Void
 	{
 		FlxG.mouse.visible = false;
@@ -18,24 +18,29 @@ class PlayState extends FlxState
 		initEmitter();
 		emitter.start(false, 0.01);
 		add(emitter);
+
+		var instructions = new FlxText(0, 10, 0,
+			"Drag left mouse to move the fire\nHold right mouse to increase scale");
+		instructions.alignment = CENTER;
+		instructions.screenCenter(X);
+		add(instructions);
 	}
 
-	function initEmitter():Void
+	function initEmitter(scale:Int = 1):Void
 	{
-		FlxPexParser.parse("assets/data/particle.pex", "assets/images/texture.png", emitter, emitterScale);
+		FlxPexParser.parse("assets/data/particle.pex", "assets/images/texture.png", emitter, scale);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		if (FlxG.mouse.pressed)
-		{
 			emitter.setPosition(FlxG.mouse.x, FlxG.mouse.y);
-		}
-		else if (FlxG.mouse.pressedRight)
-		{
-			emitterScale = emitterScale == 1 ? 2 : 1;
-			initEmitter();
-		}
+
+		if (FlxG.mouse.justPressedRight)
+			initEmitter(2);
+		else if (FlxG.mouse.justReleasedRight)
+			initEmitter(1);
+
 		super.update(elapsed);
-	}	
+	}
 }
