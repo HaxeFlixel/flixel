HaxeFlixel/flixel

commit f8b43f60c33970bfa8a6cacc2fd58819192f32e1

FlxSound: add endTime, closes #1906 (#1943)

flixel/system/FlxSound.hx
@@ -104,7 +104,11 @@ class FlxSound extends FlxBasic
 	/**
 	 * In case of looping, the point (in milliseconds) from where to restart the sound when it loops back
 	 */
-	public var loopTime:Float;
+	public var loopTime(default, set):Float;
+	/**
+	 * At which point to stop playing the sound, in milliseconds
+	 */
+	public var endTime(default, set):Float;
 	/**
 	 * The tween used to fade this sound's volume in and out (set via `fadeIn()` and `fadeOut()`)
 	 */
@@ -189,6 +193,7 @@ class FlxSound extends FlxBasic
 		_volumeAdjust = 1.0;
 		looped = false;
 		loopTime = 0.0;
+		endTime = 0.0;
 		_target = null;
 		_radius = 0;
 		_proximityPan = false;
@@ -270,8 +275,11 @@ class FlxSound extends FlxBasic
 		{
 			amplitudeLeft = 0;
 			amplitudeRight = 0;
-			amplitude = 0;			
+			amplitude = 0;
 		}
+		
+		if (_time >= endTime) 
+			stopped();
 	}
 	
 	override public function kill():Void
@@ -314,12 +322,13 @@ class FlxSound extends FlxBasic
 		}
 		
 		// NOTE: can't pull ID3 info from embedded sound currently
-		looped = Looped; 
+		looped = Looped;
 		autoDestroy = AutoDestroy;
 		updateTransform();
 		exists = true;
 		onComplete = OnComplete;
 		_length = (_sound == null) ? 0 : _sound.length;
+		endTime = _length;
 		return this;
 	}
 	
@@ -345,6 +354,7 @@ class FlxSound extends FlxBasic
 		exists = true;
 		onComplete = OnComplete;
 		_length = (_sound == null) ? 0 : _sound.length;
+		endTime = _length;
 		return this;
 	}
 	
@@ -371,6 +381,7 @@ class FlxSound extends FlxBasic
 		exists = true;
 		onComplete = OnComplete;
 		_length = (_sound == null) ? 0 : _sound.length;
+		endTime = _length;
 		return this;
 	}
 	#end
@@ -404,8 +415,9 @@ class FlxSound extends FlxBasic
 	 *                         paused when you call play(), it will continue playing from its current
 	 *                         position, NOT start again from the beginning.
 	 * @param   StartTime      At which point to start plaing the sound, in milliseconds
+	 * @param	EndTime        At which point to stop playing the sound, in milliseconds (set to `length` if `null`)
 	 */
-	public function play(ForceRestart:Bool = false, StartTime:Float = 0.0):FlxSound
+	public function play(ForceRestart:Bool = false, StartTime:Float = 0.0, ?EndTime:Float):FlxSound
 	{
 		if (!exists)
 			return this;
@@ -419,7 +431,8 @@ class FlxSound extends FlxBasic
 			resume();
 		else
 			startSound(StartTime);
-
+		
+		endTime = (EndTime == null) ? length : EndTime;
 		return this;
 	}
 	
@@ -565,7 +578,7 @@ class FlxSound extends FlxBasic
 	 * An internal helper function used to help Flash
 	 * clean up finished sounds or restart looped sounds.
 	 */
-	private function stopped(_):Void
+	private function stopped(?_):Void
 	{
 		if (onComplete != null)
 			onComplete();
@@ -573,7 +586,7 @@ class FlxSound extends FlxBasic
 		if (looped)
 		{
 			cleanup(false);
-			play(false, loopTime);
+			play(false, loopTime, endTime);
 		}
 		else
 			cleanup(autoDestroy);
@@ -712,6 +725,16 @@ class FlxSound extends FlxBasic
 			startSound(time);
 		}
 		return _time = time;
+	}
+	
+	private function set_loopTime(value:Float):Float
+	{
+		return loopTime = FlxMath.bound(value, 0, length);
+	}
+	
+	private function set_endTime(value:Float):Float
+	{
+		return endTime = FlxMath.bound(value, 0, length);
 	}
 
 	private inline function get_length():Float

commit 8d9d769155e393f1324657e31fb166c93da90e71

FlxSound: fix sounds being stopped too early sometimes
Apparently, we can't rely on the _sound.length values supplied by OpenFL legacy - in Mode, it's 8000 for Mode.ogg (Neko), but in reality it should be ~8698. This led to the sound being stopped too early.

The workaround is to initialize endTime with null instead of length, in which case the sound completes normally.

Issue introduced in f8b43f6.

flixel/system/FlxSound.hx
@@ -104,11 +104,12 @@ class FlxSound extends FlxBasic
 	/**
 	 * In case of looping, the point (in milliseconds) from where to restart the sound when it loops back
 	 */
-	public var loopTime(default, set):Float;
+	public var loopTime:Float = 0;
 	/**
-	 * At which point to stop playing the sound, in milliseconds
+	 * At which point to stop playing the sound, in milliseconds.
+	 * If not set / `null`, the sound completes normally.
 	 */
-	public var endTime(default, set):Float;
+	public var endTime:Null<Float>;
 	/**
 	 * The tween used to fade this sound's volume in and out (set via `fadeIn()` and `fadeOut()`)
 	 */
@@ -278,7 +279,7 @@ class FlxSound extends FlxBasic
 			amplitude = 0;
 		}
 		
-		if (_time >= endTime) 
+		if (endTime != null && _time >= endTime) 
 			stopped();
 	}
 	
@@ -407,8 +408,9 @@ class FlxSound extends FlxBasic
 	 *                         Default value is false, meaning if the sound is already playing or was
 	 *                         paused when you call play(), it will continue playing from its current
 	 *                         position, NOT start again from the beginning.
-	 * @param   StartTime      At which point to start plaing the sound, in milliseconds
-	 * @param	EndTime        At which point to stop playing the sound, in milliseconds (set to `length` if `null`)
+	 * @param   StartTime      At which point to start plaing the sound, in milliseconds.
+	 * @param   EndTime        At which point to stop playing the sound, in milliseconds.
+	 *                         If not set / `null`, the sound completes normally.
 	 */
 	public function play(ForceRestart:Bool = false, StartTime:Float = 0.0, ?EndTime:Float):FlxSound
 	{
@@ -425,7 +427,7 @@ class FlxSound extends FlxBasic
 		else
 			startSound(StartTime);
 		
-		endTime = (EndTime == null) ? length : EndTime;
+		endTime = EndTime;
 		return this;
 	}
 	
@@ -718,16 +720,6 @@ class FlxSound extends FlxBasic
 			startSound(time);
 		}
 		return _time = time;
-	}
-	
-	private function set_loopTime(value:Float):Float
-	{
-		return loopTime = FlxMath.bound(value, 0, length);
-	}
-	
-	private function set_endTime(value:Float):Float
-	{
-		return endTime = FlxMath.bound(value, 0, length);
 	}
 
 	private inline function get_length():Float