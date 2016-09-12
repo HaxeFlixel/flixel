accc63b2d9ef524a488fc2f632d7acd485c86b1f
Thomas ten Cate <ttencate@gmail.com>
2016-09-12 13:19:39 +0200

    FlxRandom: add shuffle(), deprecate shuffleArray() (#1947)
    closes #1914.

--- tests/unit/src/flixel/math/FlxRandomTest.hx
+++ tests/unit/src/flixel/math/FlxRandomTest.hx
@@ -2,6 +2,7 @@ package flixel.math;
 
 import flixel.util.FlxColor;
 import massive.munit.Assert;
+import org.hamcrest.Matchers.*;
 
 class FlxRandomTest extends FlxTest
 {
@@ -10,7 +11,8 @@ class FlxRandomTest extends FlxTest
 	@Before
 	function before()
 	{
-		random = new FlxRandom();
+		// Use a constant seed for reproducible test results.
+		random = new FlxRandom(1);
 	}
 	
 	@Test // #1172
@@ -33,5 +35,40 @@ class FlxRandomTest extends FlxTest
 		random.color(FlxColor.GRAY, null, null);
 		random.color(null, FlxColor.GRAY, null);
 		random.color(FlxColor.RED, FlxColor.GRAY, null);
+	}
+
+	@Test
+	function testShuffleWithEmptyArray()
+	{
+		var array:Array<Int> = [];
+		random.shuffle(array);
+		assertThat(array, is([]));
+	}
+
+	@Test
+	function testShuffleWithSingleElementArray()
+	{
+		var array = [42];
+		random.shuffle(array);
+		assertThat(array, is([42]));
+	}
+
+	@Test
+	function testShuffleWithThreeElementArray()
+	{
+		var seen = new Map<String, Bool>();
+		// The probability of this failing is (5/6)^20 = 2.6%. But because we use
+		// a fixed seed, once it passes, it will always pass, so there is no need
+		// for more iterations.
+		for (i in 0...20)
+		{
+			var array = [1, 2, 3];
+			random.shuffle(array);
+			seen[array.join(",")] = true;
+		}
+		// We have to turn the Iterator returned by keys() back into an Iterable to
+		// make Hamcrest happy.
+		var keys = [for (key in seen.keys()) key];
+		assertThat(keys, containsInAnyOrder("1,2,3", "1,3,2", "2,1,3", "2,3,1", "3,1,2", "3,2,1"));
 	}
 }
