HaxeFlixel/flixel

commit f16b8c9098df4627462aecc18f00db71ad387a5c

WatchEntry: fix vars being turned into Strings on Neko
For Int, Float and Bool at least.
closes #1911

flixel/system/debug/watch/EditableTextField.hx
@@ -9,23 +9,26 @@ import openfl.text.TextField;
 import openfl.text.TextFieldType;
 import openfl.text.TextFormat;
 import openfl.ui.Keyboard;
+import Type.ValueType;
 
 class EditableTextField extends TextField implements IFlxDestroyable
 {
 	public var isEditing(default, null):Bool;
 
 	private var allowEditing:Bool;
-	private var submitValue:String->Void;
-	
+	private var submitValue:Dynamic->Void;
+	private var expectedType:ValueType;
+
 	private var defaultFormat:TextFormat;
 	private var editFormat:TextFormat;
 	
-	public function new(allowEditing:Bool, defaultFormat:TextFormat, submitValue:Dynamic->Void) 
+	public function new(allowEditing:Bool, defaultFormat:TextFormat, submitValue:Dynamic->Void, expectedType:ValueType)
 	{
 		super();
 		this.allowEditing = allowEditing;
 		this.submitValue = submitValue;
 		this.defaultFormat = defaultFormat;
+		this.expectedType = expectedType;
 		
 		if (allowEditing)
 		{
@@ -101,11 +104,20 @@ class EditableTextField extends TextField implements IFlxDestroyable
 		setIsEditing(false);
 	}
 
-	private function submit():Void
+	public function submit():Void
 	{
+		var value:Dynamic = switch (expectedType)
+		{
+			case TInt: Std.parseInt(text);
+			case TFloat: Std.parseFloat(text);
+			case TBool if (text == "true"): true;
+			case TBool if (text == "false"): false;
+			case _: text;
+		}
+
 		try
 		{
-			submitValue(text);
+			submitValue(value);
 		}
 		catch (e:Dynamic) {}
 		

tests/unit/src/flixel/system/debug/watch/EditableTextFieldTest.hx
@@ -1,0 +1,26 @@
+package flixel.system.debug.watch;
+
+import massive.munit.Assert;
+import openfl.text.TextFormat;
+import Type.ValueType;
+
+class EditableTextFieldTest extends FlxTest
+{
+	@Test // #1911
+	function testKeepType()
+	{
+		testType(ValueType.TInt, "1");
+		testType(ValueType.TFloat, "0.56");
+		testType(ValueType.TBool, "true");
+		testType(ValueType.TBool, "false");
+	}
+
+	function testType(type:ValueType, text:String)
+	{
+		var value:Dynamic = null;
+		var textfield = new EditableTextField(true, new TextFormat(), function(v) value = v, type);
+		textfield.text = text;
+		textfield.submit();
+		Assert.areEqual(Type.typeof(value), type);
+	}
+}

flixel/system/debug/watch/WatchEntry.hx
@@ -40,8 +40,9 @@ class WatchEntry extends Sprite implements IFlxDestroyable
 
 		defaultFormat = new TextFormat(FlxAssets.FONT_DEBUGGER, 12, getTextColor());
 		nameText = initTextField(DebuggerUtil.createTextField());
+		var expectedType = Type.typeof(getValue());
 		valueText = initTextField(DebuggerUtil.initTextField(
-			new EditableTextField(data.match(FIELD(_, _)), defaultFormat, submitValue)));
+			new EditableTextField(data.match(FIELD(_, _)), defaultFormat, submitValue, expectedType)));
 
 		updateName();
 		
@@ -105,9 +106,9 @@ class WatchEntry extends Sprite implements IFlxDestroyable
 		nameText.width = Math.min(currentWidth, MAX_NAME_WIDTH);
 	}
 	
-	private function getValue():String
+	private function getValue():Dynamic
 	{
-		var value:Dynamic = switch (data)
+		return switch (data)
 		{
 			case FIELD(object, field):
 				Reflect.getProperty(object, field);
@@ -119,14 +120,18 @@ class WatchEntry extends Sprite implements IFlxDestroyable
 				#end
 			case QUICK(value):
 				value;
-		};
-		
+		}
+	}
+
+	private function getFormattedValue():String
+	{
+		var value = getValue();
 		if (Std.is(value, Float))
 			value = FlxMath.roundDecimal(cast value, FlxG.debugger.precision);
 		return Std.string(value);
 	}
 	
-	private function submitValue(value:String):Void
+	private function submitValue(value:Dynamic):Void
 	{
 		switch (data)
 		{
@@ -139,7 +144,7 @@ class WatchEntry extends Sprite implements IFlxDestroyable
 	public function updateValue()
 	{
 		if (!valueText.isEditing)
-			valueText.text = getValue();
+			valueText.text = getFormattedValue();
 	}
 	
 	public function getNameWidth():Float

commit e1db4f3a2889e7ff0666664973e33904b408356f

EditableTextField: fix Float editing on Flash
Another issue introduced by f16b8c9. The runtime type of Floats on Flash is TInt if they have an integer value (e.g. 0). This led to expectedType being TInt for a variable that is actually a Float, which made it impossible to enter decimal values.

flixel/system/debug/watch/EditableTextField.hx
@@ -108,10 +108,12 @@ class EditableTextField extends TextField implements IFlxDestroyable
 	{
 		var value:Dynamic = switch (expectedType)
 		{
+			#if neko
 			case TInt: Std.parseInt(text);
 			case TFloat: Std.parseFloat(text);
 			case TBool if (text == "true"): true;
 			case TBool if (text == "false"): false;
+			#end
 			case _: text;
 		}
 

tests/unit/src/flixel/system/debug/watch/EditableTextFieldTest.hx
@@ -6,6 +6,7 @@ import Type.ValueType;
 
 class EditableTextFieldTest extends FlxTest
 {
+	#if neko
 	@Test // #1911
 	function testKeepType()
 	{
@@ -23,4 +24,5 @@ class EditableTextFieldTest extends FlxTest
 		textfield.submit();
 		Assert.areEqual(Type.typeof(value), type);
 	}
+	#end
 }