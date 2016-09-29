0e82784f685fb589604a914c6d3a4de943f1657f
Gama11 <jensfischer95@gmail.com>
2016-09-29 21:04:26 +0200

    Support "haxelib run flixel"
    Just calls "haxelib run flixel-tools" for convenience.
    closes #1950

--- release.bat
+++ release.bat
@@ -1,1 +1,1 @@
-7z a -tzip -i!assets -i!flixel -i!CHANGELOG.md -i!LICENSE.md -i!README.md -i!include.xml -i!haxelib.json -i!showcase.png flixel.zip
+7z a -tzip -i!assets -i!flixel -i!CHANGELOG.md -i!LICENSE.md -i!README.md -i!include.xml -i!haxelib.json -i!showcase.png -i!run.n flixel.zip
