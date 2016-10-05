HaxeFlixel/flixel

commit 272f089fab326fc138f36fa0f9dec01b397f6001

BitmapFrontEnd: optimize getUniqueKey()
see #1965

flixel/system/frontEnds/BitmapFrontEnd.hx
@@ -36,6 +36,8 @@ class BitmapFrontEnd
 	
 	private var _whitePixel:FlxFrame;
 
+	private var _lastUniqueKeyIndex:Int = 0;
+
 	public function new()
 	{
 		clearCache();
@@ -112,7 +114,7 @@ class BitmapFrontEnd
 	 */
 	public inline function checkCache(Key:String):Bool
 	{
-		return _cache.exists(Key) && _cache.get(Key) != null;
+		return get(Key) != null;
 	}
 	
 	/**
@@ -164,10 +166,7 @@ class BitmapFrontEnd
 	 */
 	public inline function addGraphic(graphic:FlxGraphic):FlxGraphic
 	{
-		if (!_cache.exists(graphic.key))
-		{
-			_cache.set(graphic.key, graphic);
-		}
+		_cache.set(graphic.key, graphic);
 		return graphic;
 	}
 	
@@ -176,7 +175,7 @@ class BitmapFrontEnd
 	 * @param	key	Key for FlxGraphic object (its name)
 	 * @return	FlxGraphic with the key name, or null if there is no such object
 	 */
-	public function get(key:String):FlxGraphic
+	public inline function get(key:String):FlxGraphic
 	{
 		return _cache.get(key);
 	}
@@ -239,18 +238,17 @@ class BitmapFrontEnd
 		if (baseKey == null)
 			baseKey = "pixels";
 		
-		if (checkCache(baseKey))
+		var i:Int = _lastUniqueKeyIndex;
+		var uniqueKey:String;
+		do
 		{
-			var inc:Int = 0;
-			var ukey:String;
-			do
-			{
-				ukey = baseKey + inc++;
-			}
-			while (checkCache(ukey));
-			baseKey = ukey;
+			i++;
+			uniqueKey = baseKey + i;
 		}
-		return baseKey;
+		while (checkCache(uniqueKey));
+		
+		_lastUniqueKeyIndex = i;
+		return uniqueKey;
 	}
 	
 	/**

commit 248a1ec40ffd2340ef090cee19a3c047ada32e4f

BitmapFrontEnd: fix getUniqueKey() never returning baseKey
Issue introduced in 272f089. This broke flixel-ui.

flixel/system/frontEnds/BitmapFrontEnd.hx
@@ -238,6 +238,9 @@ class BitmapFrontEnd
 		if (baseKey == null)
 			baseKey = "pixels";
 		
+		if (!checkCache(baseKey))
+			return baseKey;
+		
 		var i:Int = _lastUniqueKeyIndex;
 		var uniqueKey:String;
 		do

tests/unit/src/flixel/system/frontEnds/BitmapDataFrontEndTest.hx
@@ -1,0 +1,23 @@
+package flixel.system.frontEnds;
+
+import openfl.display.BitmapData;
+import flixel.graphics.FlxGraphic;
+import massive.munit.Assert;
+
+class BitmapDataFrontEndTest
+{
+	@Test
+	function testGetUniqueKey()
+	{
+		var key = "test";
+
+		Assert.isFalse(FlxG.bitmap.checkCache(key));
+		Assert.areEqual(key, FlxG.bitmap.getUniqueKey(key));
+
+		FlxG.bitmap.add(FlxGraphic.fromBitmapData(new BitmapData(1, 1)), true, key);
+		Assert.isTrue(FlxG.bitmap.checkCache(key));
+		
+		var newKey = FlxG.bitmap.getUniqueKey("test");
+		Assert.areNotEqual(key, newKey);
+	}
+}