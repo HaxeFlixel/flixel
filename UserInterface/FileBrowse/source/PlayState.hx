33a5fdf331b8edcb8b1ce0b2c35461ad7d9757d4
Gama11 <jensfischer95@gmail.com>
2016-10-10 17:27:36 +0200

    FileBrowse: remove trailing whitespace

--- UserInterface/FileBrowse/source/PlayState.hx
+++ UserInterface/FileBrowse/source/PlayState.hx
@@ -1,4 +1,4 @@
-package; 
+package;
 
 import flash.display.Bitmap;
 import flash.display.BitmapData;
@@ -31,64 +31,64 @@ class PlayState extends FlxState
 	private static inline var MIN_SCALE:Float = 0.1;
 	private static inline var MAX_SCALE:Float = 5;
 	private static inline var ZOOM_FACTOR:Int = 15;
-	
+
 	private var _text:FlxText;
 	private var _button:FlxButton;
 	private var _img:FlxSprite;
 	private var _displayWidth:Float;
 	private var _displayHeight:Float;
 	private var _scaleText:FlxText;
-	
-	override public function create():Void 
+
+	override public function create():Void
 	{
 		FlxG.cameras.bgColor = FlxColor.BLACK;
-		
+
 		_img = new FlxSprite(0, 0);
 		_img.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		_img.antialiasing = true;
 		add(_img);
-		
+
 		var gutter:Int = 4;
-		
+
 		var uiBG:FlxSprite = new FlxSprite(0, 0);
 		uiBG.makeGraphic(FlxG.width, 30, FlxColor.BLACK);
 		uiBG.alpha = 0.6;
 		add(uiBG);
-		
-		var _button:FlxButton = new FlxButton(gutter, gutter, "Open Image", _onClick);
+
+		var _button = new FlxButton(gutter, gutter, "Open Image", _onClick);
 		add(_button);
-		
+
 		var bw:Float = _button.width + 15;
 		_text = new FlxText(bw, gutter, Std.int(FlxG.width - bw - 5), "Click the button to load a PNG or JPG!");
 		_text.setFormat(null, 16, FlxColor.WHITE, LEFT);
 		add(_text);
-		
+
 		_scaleText = new FlxText(FlxG.width - 100, FlxG.height - 20, 100, 100);
 		_scaleText.setFormat(null, 16, FlxColor.WHITE, RIGHT);
 		add(_scaleText);
-		
+
 		_showImage(new GraphicLogo(0, 0));
 	}
-	
+
 	override public function update(elapsed:Float):Void
 	{
 		if (FlxG.mouse.wheel != 0)
 		{
 			_updateScale(_img.scale.x + FlxG.mouse.wheel / ZOOM_FACTOR);
 		}
-		
+
 		// Little fading effect for the scale text
 		if (_scaleText.alpha > 0)
 		{
 			_scaleText.alpha -= 0.03;
 		}
-		
+
 		// Reset to a scale of x1
 		if (FlxG.keys.anyPressed([R, SPACE, ONE]))
 		{
 			_updateScale(1);
 		}
-		
+
 		// Scale shortcuts
 		if (FlxG.keys.justPressed.TWO)
 		{
@@ -106,40 +106,40 @@ class PlayState extends FlxState
 		{
 			_updateScale(5);
 		}
-		
+
 		super.update(elapsed);
 	}
-	
+
 	private function _updateScale(NewScale:Float):Void
 	{
 		if (NewScale > MAX_SCALE)
 		{
 			NewScale = MAX_SCALE;
 		}
-		else if (NewScale < MIN_SCALE) 
+		else if (NewScale < MIN_SCALE)
 		{
 			NewScale = MIN_SCALE;
 		}
-		
+
 		_img.scale.set(NewScale, NewScale);
 		_scaleText.text = "x" + FlxMath.roundDecimal(_img.scale.x, 2);
 		_scaleText.alpha = 1;
 		_centerImage();
 	}
-	
+
 	private function _centerImage():Void
 	{
 		_img.offset.x = _displayWidth * _img.scale.x / 2;
 		_img.offset.y = _displayHeight * _img.scale.y / 2;
 		_img.centerOffsets();
 	}
-	
-	private function _onClick():Void 
+
+	private function _onClick():Void
 	{
 		_showFileDialog();
 	}
-	
-	private function _showFileDialog():Void 
+
+	private function _showFileDialog():Void
 	{
 		#if flash
 			var fr:FileReference = new FileReference();
@@ -151,50 +151,51 @@ class PlayState extends FlxState
 			fr.browse();
 		#elseif (cpp || neko)
 			var filters: FILEFILTERS =
-			{ count: 2
-				, descriptions: ["PNG files", "JPEG files"]
-				, extensions: ["*.png","*.jpg;*.jpeg"]	
-			};	
+			{
+				count: 2,
+				descriptions: ["PNG files", "JPEG files"],
+				extensions: ["*.png","*.jpg;*.jpeg"]
+			};
 			var result:Array<String> = Dialogs.openFile(
-				"Select a file please!"
-				, "Please select one or more files, so we can see if this method works"
-				, filters
-				);	
-				
+				"Select a file please!",
+				"Please select one or more files, so we can see if this method works",
+				filters
+			);
+
 			_onSelect(result);
 		#end
 	}
 
 	#if flash
-	private function _onSelect(E:Event):Void 
+	private function _onSelect(E:Event):Void
 	{
 		var fr:FileReference = cast(E.target, FileReference);
-		_text.text = fr.name;			
-		fr.addEventListener(Event.COMPLETE, _onLoad, false, 0, true);			
+		_text.text = fr.name;
+		fr.addEventListener(Event.COMPLETE, _onLoad, false, 0, true);
 		fr.load();
-	}   		
-	
-	private function _onLoad(E:Event):Void 
+	}
+
+	private function _onLoad(E:Event):Void
 	{
 		var fr:FileReference = E.target;
 		fr.removeEventListener(Event.COMPLETE, _onLoad);
-		
+
 		var loader:Loader = new Loader();
 		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, _onImgLoad);
 		loader.loadBytes(fr.data);
 	}
-	
-	private function _onImgLoad(E:Event):Void 
+
+	private function _onImgLoad(E:Event):Void
 	{
 		var loaderInfo:LoaderInfo = E.target;
 		loaderInfo.removeEventListener(Event.COMPLETE, _onImgLoad);
 		var bmp:Bitmap = cast(loaderInfo.content, Bitmap);
 		_showImage(bmp.bitmapData);
 	}
-	
+
 	#elseif (cpp || neko)
-	private function _onSelect(arr:Array<String>):Void 
-	{	   
+	private function _onSelect(arr:Array<String>):Void
+	{
 		if (arr != null && arr.length > 0)
 		{
 			_text.text = arr[0];
@@ -204,43 +205,43 @@ class PlayState extends FlxState
 			#else
 				BitmapData.fromFile(arr[0]);
 			#end
-			
-			if (img != null) 
-			{					
+
+			if (img != null)
+			{
 				_showImage(img);
 			}
 		}
-		else 
+		else
 		{
 			_onCancel(null);
 		}
 	}
 	#end
-	
-	private function _onCancel(_):Void 
-	{	   
+
+	private function _onCancel(_):Void
+	{
 		_text.text = "Cancelled!";
-	}		
-	
-	private function _showImage(Data:BitmapData):Void 
+	}
+
+	private function _showImage(Data:BitmapData):Void
 	{
 		_img.scale.set(1, 1);
-		
+
 		var imgWidth:Float = FlxG.width / Data.width;
 		var imgHeight:Float = FlxG.height / Data.height;
-		
+
 		var scale:Float = imgWidth <= imgHeight ? imgWidth : imgHeight;
-		
+
 		// Cap the scale at x1
 		if (scale > 1)
-		{ 
+		{
 			scale = 1;
 		}
-		
+
 		_displayWidth = Data.width * scale;
 		_displayHeight = Data.height * scale;
 		_img.makeGraphic(Std.int(_displayWidth), Std.int(_displayHeight), FlxColor.BLACK);
-		
+
 		var data2:BitmapData = _img.pixels.clone();
 		var matrix:Matrix = new Matrix();
 		matrix.identity();
@@ -248,7 +249,7 @@ class PlayState extends FlxState
 		data2.fillRect(data2.rect, FlxColor.BLACK);
 		data2.draw(Data, matrix, null, null, null, true);
 		_img.pixels = data2;
-		
+
 		// Center the image
 		_img.x = (FlxG.width - _displayWidth) / 2;
 		_img.y = (FlxG.height - _displayHeight) / 2;
