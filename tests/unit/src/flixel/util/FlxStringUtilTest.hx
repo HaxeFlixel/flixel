57dc46046fb0c51b134ff4013b1f9f864ea130f8
Aaron Hill <seraku24@users.noreply.github.com>
2016-11-06 05:10:00 -0800

    FlxStringUtil.getDomain() fixes + improvements (#1993)
    Replaced logic to properly support extracting the domain from valid URLs.
    Added unit tests to validate behavior against both non-local and local paths.

--- tests/unit/src/flixel/util/FlxStringUtilTest.hx
+++ tests/unit/src/flixel/util/FlxStringUtilTest.hx
@@ -99,4 +99,54 @@ class FlxStringUtilTest
 		Assert.isFalse(FlxStringUtil.isNullOrEmpty("."));
 		Assert.isFalse(FlxStringUtil.isNullOrEmpty("Hello World"));
 	}
+
+	@Test
+	function testGetDomainNonLocal()
+	{
+		var domain = "xn--eckwd4c7c.test";
+
+		// Examples of valid URI components.
+		var schemes = ["http", "https", "fake.but-valid+scheme"];
+		var hosts = ['$domain', 'www.$domain', 'aaa.bbb.ccc.$domain'];
+		var paths = ["", "/", "/index.html", "/path/to/file.extension?query=42"];
+
+		for (scheme in schemes) for (host in hosts) for (path in paths)
+		{
+			var uri = mixedCase('$scheme://$host$path');
+			Assert.areEqual(domain, FlxStringUtil.getDomain(uri));
+		}
+	}
+
+	/**
+	 * Returns a string with a mixture of upper and lower case characters.
+	 */
+	function mixedCase(string:String):String
+	{
+		var result = "", upper = false;
+		for (i in 0...string.length)
+		{
+			var char = string.charAt(i);
+			result += (upper = !upper) ? char.toUpperCase() : char.toLowerCase();
+		}
+		return result;
+	}
+
+	@Test
+	function testGetDomainLocal()
+	{
+		// Examples of some Unix-style and Windows-style local file paths.
+		var paths = [
+			"/root/folder/file.extension",
+			"./folder/file.extension",
+			"~/.extension",
+			"D:\\folder\\file.extension",
+			"D:\\folder\\file.extension:alternate_stream_name",
+			"D:file.extension",
+			"\\\\server\\folder\\file.extension",
+			"\\\\?\\D:\\folder\\file.extension"
+		];
+		
+		for (path in paths)
+			Assert.areEqual("local", FlxStringUtil.getDomain(path));
+	}
 }
