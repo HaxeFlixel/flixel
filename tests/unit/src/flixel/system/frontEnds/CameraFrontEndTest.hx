HaxeFlixel/flixel

commit edf93b57954d943368b5bba9a3c187a455101050

CameraFrontEnd: add added / removed / resized signals
Necessary to fix #1801, and probably otherwise useful as well.

flixel/system/frontEnds/CameraFrontEnd.hx
@@ -5,6 +5,7 @@ import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.util.FlxAxes;
 import flixel.util.FlxColor;
+import flixel.util.FlxSignal.FlxTypedSignal;
 
 class CameraFrontEnd
 {
@@ -18,6 +19,12 @@ class CameraFrontEnd
 	 * The current (global, applies to all cameras) bgColor.
 	 */
 	public var bgColor(get, set):FlxColor;
+	
+	public var cameraAdded(default, null):FlxTypedSignal<FlxCamera->Void> = new FlxTypedSignal<FlxCamera->Void>();
+	
+	public var cameraRemoved(default, null):FlxTypedSignal<FlxCamera->Void> = new FlxTypedSignal<FlxCamera->Void>();
+	
+	public var cameraResized(default, null):FlxTypedSignal<FlxCamera->Void> = new FlxTypedSignal<FlxCamera->Void>();
 	
 	/**
 	 * Allows you to possibly slightly optimize the rendering process IF
@@ -41,6 +48,7 @@ class CameraFrontEnd
 		FlxG.game.addChildAt(NewCamera.flashSprite, FlxG.game.getChildIndex(FlxG.game._inputContainer));
 		FlxG.cameras.list.push(NewCamera);
 		NewCamera.ID = FlxG.cameras.list.length - 1;
+		cameraAdded.dispatch(NewCamera);
 		return NewCamera;
 	}
 	
@@ -53,14 +61,15 @@ class CameraFrontEnd
 	public function remove(Camera:FlxCamera, Destroy:Bool = true):Void
 	{
 		var index:Int = list.indexOf(Camera);
-		if ((Camera != null) && index != -1)
+		if (Camera != null && index != -1)
 		{
 			FlxG.game.removeChild(Camera.flashSprite);
 			list.splice(index, 1);
 		}
 		else
 		{
-			FlxG.log.warn("FlxG.cameras.remove(): The camera you attemped to remove is not a part of the game.");
+			FlxG.log.warn("FlxG.cameras.remove(): The camera you attempted to remove is not a part of the game.");
+			return;
 		}
 		
 		if (FlxG.renderTile)
@@ -72,9 +81,9 @@ class CameraFrontEnd
 		}
 		
 		if (Destroy)
-		{
 			Camera.destroy();
-		}
+		
+		cameraRemoved.dispatch(Camera);
 	}
 		
 	/**
@@ -86,17 +95,12 @@ class CameraFrontEnd
 	public function reset(?NewCamera:FlxCamera):Void
 	{
 		for (camera in list)
-		{
-			FlxG.game.removeChild(camera.flashSprite);
-			camera.destroy();
-		}
+			remove(camera);
 		
 		list.splice(0, list.length);
 		
 		if (NewCamera == null)
-		{
 			NewCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
-		}
 		
 		FlxG.camera = add(NewCamera);
 		NewCamera.ID = 0;

flixel/FlxCamera.hx
@@ -1547,26 +1547,30 @@ class FlxCamera extends FlxBasic
 	
 	private function set_width(Value:Int):Int
 	{
-		if (Value > 0)
+		if (width != Value && Value > 0)
 		{
 			width = Value; 
 			
 			updateFlashOffset();
 			updateScrollRect();
 			updateInternalSpritePositions();
+			
+			FlxG.cameras.cameraResized.dispatch(this);
 		}
 		return Value;
 	}
 	
 	private function set_height(Value:Int):Int
 	{
-		if (Value > 0)
+		if (height != Value && Value > 0)
 		{
 			height = Value;
 			
 			updateFlashOffset();
 			updateScrollRect();
 			updateInternalSpritePositions();
+			
+			FlxG.cameras.cameraResized.dispatch(this);
 		}
 		return Value;
 	}

flixel/system/debug/log/BitmapLog.hx
@@ -246,7 +246,6 @@ class BitmapLog extends Window
 
 	/**
 	 * Clear one bitmap object from the log -- the last one, by default
-	 * @param	Index
 	 */
 	public function clearAt(Index:Int = -1):Void
 	{

commit 1c714ba798a7376353093049712dc0671fea9fa4

FlxG.cameras: fix reset() not removing all cameras (#2016)
Regression from edf93b5.

flixel/system/frontEnds/CameraFrontEnd.hx
@@ -97,11 +97,9 @@ class CameraFrontEnd
 	 */
 	public function reset(?NewCamera:FlxCamera):Void
 	{
-		for (camera in list)
-			remove(camera);
-		
-		list.splice(0, list.length);
-		
+		while (list.length > 0)
+			remove(list[0]);
+
 		if (NewCamera == null)
 			NewCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
 		

tests/unit/src/FlxTest.hx
@@ -17,9 +17,9 @@ class FlxTest
 	
 	public function new() {}
 	
+	@After
 	@:access(flixel)
-	@AfterClass
-	function afterClass()
+	function after()
 	{
 		FlxG.game.getTimer = function()
 		{

tests/unit/src/flixel/system/frontEnds/CameraFrontEndTest.hx
@@ -3,7 +3,7 @@ package flixel.system.frontEnds;
 import flixel.FlxCamera;
 import massive.munit.Assert;
 
-class CameraFrontEndTest
+class CameraFrontEndTest extends FlxTest
 {
 	@Test
 	public function testCameraAdded()
@@ -49,5 +49,17 @@ class CameraFrontEndTest
 		Assert.isTrue(success);
 		
 		FlxG.cameras.cameraResized.removeAll();
+	}
+
+	@Test
+	function testResetCameras()
+	{
+		Assert.areEqual(1, FlxG.cameras.list.length);
+
+		FlxG.cameras.add(new FlxCamera());
+		Assert.areEqual(2, FlxG.cameras.list.length);
+
+		FlxG.cameras.reset();
+		Assert.areEqual(1, FlxG.cameras.list.length);
 	}
 }