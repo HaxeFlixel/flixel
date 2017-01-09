34d96128a2ff565fcfa6b01463f8c026e9c5696f
Lars Doucet <lars.doucet@gmail.com>
2017-01-09 16:34:37 -0600

    fix merge error

--- flixel/input/gamepad/FlxGamepadManager.hx
+++ flixel/input/gamepad/FlxGamepadManager.hx
@@ -1,9 +1,9 @@
 package flixel.input.gamepad;
 
 import flixel.input.FlxInput.FlxInputState;
+import flixel.util.FlxSignal.FlxTypedSignal;
 import flixel.input.gamepad.FlxGamepad.FlxGamepadModel;
 import flixel.util.FlxDestroyUtil;
-import flixel.util.FlxSignal.FlxTypedSignal;
 
 #if FLX_JOYSTICK_API
 import flixel.FlxG;
