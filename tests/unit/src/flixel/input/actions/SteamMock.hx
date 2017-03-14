9e28795f3cc2cfd5a56d655ac6b63dc3fef54c08
Lars Doucet <lars.doucet@gmail.com>
2017-03-14 09:43:47 -0500

    Added FlxActionManager tests

---
+++ tests/unit/src/flixel/input/actions/SteamMock.hx
@@ -0,0 +1,24 @@
+package flixel.input.actions;
+
+/**
+ * ...
+ * @author 
+ */
+class SteamMock
+{
+
+	public function new() 
+	{
+		
+	}
+	
+	public static function setDigitalAction(actionHandle:Int, bool:Bool)
+	{
+		
+	}
+	
+	public static function update():Void
+	{
+		
+	}
+}
