240a9b1355213f7cd755d2e21b4943285ebcc4e2
Ashiq A <ashes999@yahoo.com>
2017-07-13 22:51:56 -0400

    Clarify FlxFlicker.Duration's unit

--- flixel/effects/FlxFlicker.hx
+++ flixel/effects/FlxFlicker.hx
@@ -23,7 +23,7 @@ class FlxFlicker implements IFlxDestroyable
 	 * A simple flicker effect for sprites using a ping-pong tween by toggling visibility.
 	 * 
 	 * @param   Object               The object.
-	 * @param   Duration             How long to flicker for.
+	 * @param   Duration             How long to flicker for (in seconds).
 	 * @param   Interval             In what interval to toggle visibility. Set to `FlxG.elapsed` if `<= 0`!
 	 * @param   EndVisibility        Force the visible value when the flicker completes,
 	 *                               useful with fast repetitive use.
