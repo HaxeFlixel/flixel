3e8410a57b13a67cefa264eec046329238fa764f
Lepeltier kévin <kevin@lepeltier.info>
2018-01-12 17:52:34 +0100

    Fix for codeclimate, attempt 1

--- flixel/tile/FlxBaseTilemap.hx
+++ flixel/tile/FlxBaseTilemap.hx
@@ -23,22 +23,22 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 	
 	private var offsetAutoTile:Array<Int> = 
 		[
-		 	  0,  0,  0,  0,  2,  2,  0,  3,  0,  0,  0,  0,  0,  0,  0,  0,
-		 	 11, 11,  0,  0, 13, 13,  0, 14,  0,  0,  0,  0, 18, 18,  0, 19,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			 51, 51,  0,  0, 53, 53,  0, 54,  0,  0,  0,  0,  0,  0,  0,  0,
-			 62, 62,  0,  0, 64, 64,  0, 65,  0,  0,  0,  0, 69, 69,  0, 70,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			 86, 86,  0,  0, 88, 88,  0, 89,  0,  0,  0,  0, 93, 93,  0, 94,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,159,  0,  0,  0,162,  0,163,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,172,  0,  0,  0,175,  0,176,  0,  0,  0,  0,  0,181,  0,182,
-			  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-			  0,199,  0,  0,  0,202,  0,203,  0,  0,  0,  0,  0,208,  0,209
+			0, 0, 0, 0, 2, 2, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+			11, 11, 0, 0, 13, 13, 0, 14, 0, 0, 0, 0, 18, 18, 0, 19,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			51, 51, 0, 0, 53, 53, 0, 54, 0, 0, 0, 0, 0, 0, 0, 0,
+			62, 62, 0, 0, 64, 64, 0, 65, 0, 0, 0, 0, 69, 69, 0, 70,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			86, 86, 0, 0, 88, 88, 0, 89, 0, 0, 0, 0, 93, 93, 0, 94,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 159, 0, 0, 0, 162, 0, 163, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 172, 0, 0, 0, 175, 0, 176, 0, 0, 0, 0, 0, 181, 0, 182,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 199, 0, 0, 0, 202, 0, 203, 0, 0, 0, 0, 0, 208, 0, 209
 		];
 	
 	public var widthInTiles(default, null):Int = 0;
@@ -523,40 +523,39 @@ class FlxBaseTilemap<Tile:FlxObject> extends FlxObject
 		
 		_data[Index] = 0;
 
-		var wallUP:Bool = Index - widthInTiles < 0;
+		var wallUp:Bool = Index - widthInTiles < 0;
 		var wallRight:Bool = Index % widthInTiles >= widthInTiles - 1;
-		var wallDOWN:Bool = Std.int(Index + widthInTiles) >= totalTiles;
-		var wallLEFT:Bool = Index % widthInTiles <= 0;
+		var wallDown:Bool = Std.int(Index + widthInTiles) >= totalTiles;
+		var wallLeft:Bool = Index % widthInTiles <= 0;
 
-		var UP = _data[Index - widthInTiles] > 0;
-		var UPRIGHT = _data[Index - widthInTiles + 1] > 0;
-		var RIGHT = _data[Index + 1] > 0;
-		var RIGHTDOWN = _data[Index + widthInTiles + 1] > 0;
-		var DOWN = _data[Index + widthInTiles] > 0;
-		var DOWNLEFT = _data[Index + widthInTiles - 1] > 0;
-		var LEFT = _data[Index - 1] > 0;
-		var LEFTUP = _data[Index - widthInTiles - 1] > 0;
+		var up = _data[Index - widthInTiles] > 0;
+		var upRight = _data[Index - widthInTiles + 1] > 0;
+		var right = _data[Index + 1] > 0;
+		var rightDown = _data[Index + widthInTiles + 1] > 0;
+		var down = _data[Index + widthInTiles] > 0;
+		var downLeft = _data[Index + widthInTiles - 1] > 0;
+		var left = _data[Index - 1] > 0;
+		var leftUp = _data[Index - widthInTiles - 1] > 0;
 
+		if ( wallUp || up ) _data[Index] += 1; // UP
 		
-		if ( wallUP || UP ) _data[Index] += 1; // UP
+		if ( wallUp || wallRight || ( upRight && up && right ) ) _data[Index] += 2; // UP - RIGHT
 		
-		if ( wallUP || wallRight || ( UPRIGHT && UP && RIGHT ) ) _data[Index] += 2; // UP - RIGHT
+		if ( wallRight || right ) _data[Index] += 4; // RIGHT
 		
-		if ( wallRight || RIGHT ) _data[Index] += 4; // RIGHT
+		if ( wallRight || wallDown || ( rightDown && right && down ) ) _data[Index] += 8; // RIGHT - DOWN
 		
-		if ( wallRight || wallDOWN || ( RIGHTDOWN && RIGHT && DOWN ) ) _data[Index] += 8; // RIGHT - DOWN
+		if ( wallDown || down )  _data[Index] += 16; // DOWN
 		
-		if ( wallDOWN || DOWN )  _data[Index] += 16; // DOWN
+		if ( wallDown || wallLeft || ( downLeft && down && left ) ) _data[Index] += 32; // DOWN - LEFT
 		
-		if ( wallDOWN || wallLEFT || ( DOWNLEFT && DOWN && LEFT ) ) _data[Index] += 32; // DOWN - LEFT
+		if ( wallLeft || left ) _data[Index] += 64; // LEFT
 		
-		if ( wallLEFT || LEFT ) _data[Index] += 64; // LEFT
-		
-		if ( wallLEFT || wallUP || ( LEFTUP && LEFT && UP ) ) _data[Index] += 128; // LEFT - UP
+		if ( wallLeft || wallUp || ( leftUp && left && up ) ) _data[Index] += 128; // LEFT - UP
 
 		_data[Index] += 1;
 
-		_data[Index] -= offsetAutoTile[_data[Index]-1];
+		_data[Index] -= offsetAutoTile[_data[Index] - 1];
 	}
 
 	/**
