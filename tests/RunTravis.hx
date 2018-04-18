c82fa4d876f05a5c5acbbdc07c3f29fc62bd43c9
Aurel Bílý <aurel.bily@gmail.com>
2018-04-18 22:59:42 +0100

    apply travis script fixes from dev

--- tests/RunTravis.hx
+++ tests/RunTravis.hx
@@ -18,19 +18,15 @@ abstract ExitCode(Int) from Int to Int
 
 class RunTravis
 {
-	static var importantDemos = ["Mode"];
-	static var dryRun:Bool;
-
+	static var importantDemos = ["Mode", "RPG Interface", "FlxNape"];
+	
 	public static function main():Void
 	{
 		var target:Target = Sys.args()[0];
 		if (target == null)
 			target = Target.FLASH;
 		
-		dryRun = Sys.args().indexOf("-dry-run") != -1;
-	
 		Sys.exit(getResult([
-			setupHxcpp(target),
 			runUnitTests(target),
 			buildCoverageTests(target),
 			buildSwfVersionTests(target),
@@ -40,38 +36,8 @@ class RunTravis
 		]));
 	}
 	
-	static function setupHxcpp(target:Target):ExitCode
-	{
-		if (target != Target.CPP)
-			return ExitCode.SUCCESS;
-
-		#if (haxe_ver >= "3.3")
-		var hxcppDir = Sys.getEnv("HOME") + "/haxe/lib/hxcpp/git/";
-		return getResult([
-			runCommand("haxelib", ["git", "hxcpp", "https://github.com/HaxeFoundation/hxcpp"]),
-			runCommandInDir(hxcppDir + "tools/run", "haxe", ["compile.hxml"]),
-			runCommandInDir(hxcppDir + "tools/hxcpp", "haxe", ["compile.hxml"]),
-			runCommandInDir(hxcppDir + "project", "neko", ["build.n"])
-		]);
-		#else
-		return runCommand("haxelib", ["install", "hxcpp", "3.3.49"]);
-		#end
-	}
-	
-	static function runCommandInDir(dir:String, cmd:String, args:Array<String>):ExitCode
-	{
-		return runInDir(dir, function() return runCommand(cmd, args));
-	}
-	
 	static function runUnitTests(target:Target):ExitCode
 	{
-		if (target == Target.FLASH)
-			return ExitCode.SUCCESS;
-
-		runInDir("unit", function()
-			return haxelibRun(["munit", "gen"])
-		);
-
 		if (target == Target.FLASH || target == Target.HTML5)
 		{	
 			// can't run / display results without a browser,
@@ -106,7 +72,7 @@ class RunTravis
 	
 	static function buildNextDemos(target:Target):ExitCode
 	{
-		if (target == Target.FLASH || target == Target.HTML5)
+		if (target != Target.NEKO)
 			return ExitCode.SUCCESS;
 		
 		Sys.println("\nBuilding demos for OpenFL Next...\n");
@@ -119,14 +85,14 @@ class RunTravis
 			return ExitCode.SUCCESS;
 		
 		Sys.println("\nBuilding mechanics demos...\n");
-		runCommand("git", ["clone", "https://github.com/HaxeFlixel/haxeflixel-mechanics"]);
+		Sys.command("git", ["clone", "https://github.com/HaxeFlixel/haxeflixel-mechanics"]);
 		
 		return buildProjects(target, ["-dir", "haxeflixel-mechanics"]);
 	}
 	
 	static function buildProjects(target:Target, args:Array<String>):ExitCode
 	{
-		return haxelibRun(["flixel-tools", "bp", target].concat(args));
+		return return haxelibRun(["flixel-tools", "bp", target].concat(args));
 	}
 	
 	static function buildSwfVersionTests(target:Target):ExitCode
@@ -157,7 +123,7 @@ class RunTravis
 	
 	static function haxelibRun(args:Array<String>):ExitCode
 	{
-		return runCommand("haxelib", ["run"].concat(args));
+		return Sys.command("haxelib", ["run"].concat(args));
 	}
 	
 	static function getResult(results:Array<ExitCode>):ExitCode
@@ -171,24 +137,9 @@ class RunTravis
 	static function runInDir(dir:String, func:Void->ExitCode):ExitCode
 	{
 		var oldCwd = Sys.getCwd();
-		cd(dir);
+		Sys.setCwd(dir);
 		var result = func();
-		cd(oldCwd);
+		Sys.setCwd(oldCwd);
 		return result;
-	}
-	
-	static function cd(dir:String)
-	{
-		Sys.println("cd " + dir);
-		if (!dryRun)
-			Sys.setCwd(dir);
-	}
-	
-	static function runCommand(cmd:String, args:Array<String>):ExitCode
-	{
-		Sys.println(cmd + " " + args.join(" "));
-		if (dryRun)
-			return ExitCode.SUCCESS;
-		return Sys.command(cmd, args);
 	}
 }
