5f08f677a2d8e92512b529b4dd5deb57cd9f8c52
Lars Doucet <lars.doucet@gmail.com>
2018-09-06 15:56:11 -0500

    actually this one's necessary, dunno why codeclimate complains

--- flixel/input/actions/FlxActionSet.hx
+++ flixel/input/actions/FlxActionSet.hx
@@ -4,6 +4,7 @@ import flixel.input.FlxInput.FlxInputState;
 import flixel.input.actions.FlxAction.FlxActionAnalog;
 import flixel.input.actions.FlxAction.FlxActionDigital;
 import flixel.input.actions.FlxActionInput.FlxInputDevice;
+import flixel.input.actions.FlxActionInput.FlxInputType;
 import flixel.input.actions.FlxActionInputAnalog.FlxActionInputAnalogSteam;
 import flixel.input.actions.FlxActionInputAnalog.FlxAnalogState;
 import flixel.input.actions.FlxActionInputAnalog.FlxAnalogAxis;
