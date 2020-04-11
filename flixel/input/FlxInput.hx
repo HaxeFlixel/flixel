c2a41f06f56780998705f29e098a6f8fe8e473c6
George FunBook <gkurelic@gmail.com>
2020-04-11 13:01:50 -0500

    add FlxAnalogInput

--- flixel/input/FlxInput.hx
+++ flixel/input/FlxInput.hx
@@ -1,55 +1,98 @@
 package flixel.input;
 
-class FlxInput<T> implements IFlxInput
+class FlxTypedInput<TKey, TValue, TState>
 {
-	public var ID:T;
+	public var ID:TKey;
+	
+	public var currentValue:TValue;
+	public var lastValue:TValue;
+	
+	public var current:TState;
+	public var last:TState;
+	
+	public var changed(get, never):Bool;
+	
+	var dirty = false;
+	
+	public function new(ID:TKey)
+	{
+		this.ID = ID;
+		reset();
+	}
+	
+	public function change(newValue:TValue):Void
+	{
+		lastValue = currentValue;
+		currentValue = newValue;
+		dirty = true;
+	}
+	
+	public function update():Void
+	{
+		if (!dirty)
+		{
+			change(currentValue);
+		}
+		dirty = false;
+	}
+	
+	public function hasState(state:TState):Bool
+	{
+		throw "Do not directly instantiate Abstract class FlxTypedInput";
+	}
+	
+	public function reset():Void
+	{
+		throw "Do not directly instantiate Abstract class FlxTypedInput";
+	}
+	
+	inline function get_changed():Bool
+	{
+		return currentValue != lastValue;
+	}
+}
 
+class FlxInput<T> extends FlxTypedInput<T, Bool, FlxInputState> implements IFlxInput
+{
 	public var justReleased(get, never):Bool;
 	public var released(get, never):Bool;
 	public var pressed(get, never):Bool;
 	public var justPressed(get, never):Bool;
-
-	public var current:FlxInputState = RELEASED;
-	public var last:FlxInputState = RELEASED;
-
+	
 	public function new(ID:T)
 	{
-		this.ID = ID;
+		super(ID);
 	}
-
+	
 	public function press():Void
 	{
-		last = current;
-		current = pressed ? PRESSED : JUST_PRESSED;
+		change(true);
 	}
-
+	
 	public function release():Void
 	{
-		last = current;
-		current = pressed ? JUST_RELEASED : RELEASED;
+		change(false);
 	}
-
-	public function update():Void
+	
+	override function change(newValue:Bool)
 	{
-		if (last == JUST_RELEASED && current == JUST_RELEASED)
-		{
-			current = RELEASED;
-		}
-		else if (last == JUST_PRESSED && current == JUST_PRESSED)
-		{
-			current = PRESSED;
-		}
-
 		last = current;
+		if (newValue)
+			current = currentValue ? PRESSED : JUST_PRESSED;
+		else
+			current = !currentValue ? RELEASED : JUST_RELEASED;
+		super.change(newValue);
 	}
-
-	public function reset():Void
+	
+	override function reset()
 	{
+		currentValue = false;
+		lastValue = false;
 		current = RELEASED;
 		last = RELEASED;
 	}
-
-	public function hasState(state:FlxInputState):Bool
+	
+	override function hasState(state:FlxInputState)
 	{
 		return switch (state)
 		{
@@ -59,25 +102,88 @@ class FlxInput<T> implements IFlxInput
 			case JUST_PRESSED: justPressed;
 		}
 	}
-
+	
 	inline function get_justReleased():Bool
 	{
 		return current == JUST_RELEASED;
 	}
-
+	
 	inline function get_released():Bool
 	{
 		return current == RELEASED || justReleased;
 	}
-
+	
 	inline function get_pressed():Bool
 	{
 		return current == PRESSED || justPressed;
 	}
-
+	
 	inline function get_justPressed():Bool
 	{
 		return current == JUST_PRESSED;
+	}
+}
+
+class FlxAnalogInput<K, V:Float> extends FlxTypedInput<K, V, FlxAnalogState>
+{
+	public var justStopped(get, never):Bool;
+	public var stopped(get, never):Bool;
+	public var moved(get, never):Bool;
+	public var justMoved(get, never):Bool;
+	public var delta(get, never):V;
+	
+	override function change(newValue:V)
+	{
+		last = current;
+		if (newValue == currentValue)
+			current = currentValue == lastValue ? STOPPED : JUST_STOPPED;
+		else
+			current = currentValue != lastValue ? MOVED : JUST_MOVED;
+		super.change(newValue);
+	}
+	
+	override function reset()
+	{
+		currentValue = cast 0;
+		lastValue = cast 0;
+		current = STOPPED;
+		last = STOPPED;
+	}
+	
+	override function hasState(state:FlxAnalogState)
+	{
+		return switch (state)
+		{
+			case JUST_STOPPED: justStopped;
+			case STOPPED: stopped;
+			case MOVED: moved;
+			case JUST_MOVED: justMoved;
+		}
+	}
+	
+	inline function get_justStopped():Bool
+	{
+		return current == JUST_STOPPED;
+	}
+	
+	inline function get_stopped():Bool
+	{
+		return current == STOPPED || justStopped;
+	}
+	
+	inline function get_moved():Bool
+	{
+		return current == MOVED || justMoved;
+	}
+	
+	inline function get_justMoved():Bool
+	{
+		return current == JUST_MOVED;
+	}
+	
+	inline function get_delta():V
+	{
+		return cast lastValue - currentValue;
 	}
 }
 
@@ -88,4 +194,13 @@ abstract FlxInputState(Int) from Int
 	var RELEASED = 0;
 	var PRESSED = 1;
 	var JUST_PRESSED = 2;
+}
+
+@:enum
+abstract FlxAnalogState(Int) from Int
+{
+	var JUST_STOPPED = cast FlxInputState.JUST_RELEASED; // became 0 on this frame
+	var STOPPED = cast FlxInputState.RELEASED; // is 0
+	var MOVED = cast FlxInputState.PRESSED; // is !0
+	var JUST_MOVED = cast FlxInputState.JUST_PRESSED; // became !0 on this frame
 }
