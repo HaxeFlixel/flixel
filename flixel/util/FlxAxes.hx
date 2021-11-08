2ca64a6d82680cdedf366d621c6e5b31a101f793
George Kurelic <Gkurelic@gmail.com>
2021-11-07 21:51:06 -0600

    use abstract when < haxe4

--- flixel/util/FlxAxes.hx
+++ flixel/util/FlxAxes.hx
@@ -1,8 +1,18 @@
 package flixel.util;
 
+#if (haxe >= "4")
 enum FlxAxes
 {
 	X;
 	Y;
 	XY;
 }
+#else
+// Use abstract in 3.4.7 to allow default arguments
+@:enum abstract FlxAxes(Int)
+{
+	var X = 1;
+	var Y = 2;
+	var XY = 3;
+}
+#end
