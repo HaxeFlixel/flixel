d76f58fefc05c45aaca7da7bac90a56f0a421f84
George FunBook <gkurelic@gmail.com>
2022-01-24 15:43:52 -0600

    remove switch causing seg faults

--- flixel/input/keyboard/FlxKey.hx
+++ flixel/input/keyboard/FlxKey.hx
@@ -11,8 +11,8 @@ abstract FlxKey(Int) from Int to Int
 	public static var fromStringMap(default, null):Map<String, FlxKey> = FlxMacroUtil.buildMap("flixel.input.keyboard.FlxKey");
 	public static var toStringMap(default, null):Map<FlxKey, String> = FlxMacroUtil.buildMap("flixel.input.keyboard.FlxKey", true);
 	// Key Indicies
-	var ANY = 254;
-	var NONE = 255;
+	var ANY = -2;
+	var NONE = -1;
 	var A = 65;
 	var B = 66;
 	var C = 67;
