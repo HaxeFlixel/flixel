ab49ce9a8d1c4eed8eb6184146fbd406a8495c9c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-07-20 15:00:12 +0300

    Update FlxAtlasFrames.hx

--- flixel/graphics/frames/FlxAtlasFrames.hx
+++ flixel/graphics/frames/FlxAtlasFrames.hx
@@ -5,6 +5,7 @@ import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames.TexturePackerObject;
 import flixel.graphics.frames.FlxFrame.FlxFrameAngle;
 import flixel.graphics.frames.FlxFramesCollection.FlxFrameCollectionType;
+import flixel.util.typeLimit.OneOfTwo;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 import flixel.system.FlxAssets.FlxGraphicAsset;
@@ -113,6 +114,93 @@ class FlxAtlasFrames extends FlxFramesCollection
 		}
 
 		Frames.addAtlasFrame(frameRect, sourceSize, offset, name, angle);
+	}
+
+	
+	/**
+	 * Parsing method for Aseprite atlases in JSON format.
+	 *
+	 * @param   Source        The image source (can be `FlxGraphic`, `String`, or `BitmapData`).
+	 * @param   Description   Contents of JSON file with atlas description.
+	 *                        You can get it with `Assets.getText(path/to/description.json)`.
+	 *                        Or you can just a pass path to the JSON file in the assets directory.
+	 *                        You can also directly pass in the parsed object.
+	 * @return  Newly created `FlxAtlasFrames` collection.
+	 */
+	public static function fromAseprite(Source:FlxGraphicAsset, Description:FlxAsepriteSource):FlxAtlasFrames
+	{
+		var graphic:FlxGraphic = FlxG.bitmap.add(Source, false);
+		if (graphic == null)
+			return null;
+
+		var frames:FlxAtlasFrames = FlxAtlasFrames.findFrame(graphic);
+		if (frames != null)
+			return frames;
+
+		if (graphic == null || Description == null)
+			return null;
+
+		frames = new FlxAtlasFrames(graphic);
+
+		var data:AsepriteData;
+
+		if (Description is String)
+		{
+			var json:String = Description;
+
+			if (Assets.exists(json))
+				json = Assets.getText(json);
+
+			data = Json.parse(json);
+		}
+		else
+			data = Description;
+
+		for (frameName in Reflect.fields(data.frames))
+			asepriteHelper(frameName, Reflect.field(data.frames, frameName), frames);
+
+		return frames;
+	}
+
+	/**
+	 * Parsing method for Aseprite Array in JSON format.
+	 *
+	 * @param   FrameArray    The image array source (can be `Array<FlxGraphic>`, `Array<String>`, or `Array<BitmapData>`).
+	 * @param   Description   Contents of JSON file with atlas description.
+	 *                        You can get it with `Assets.getText(path/to/description.json)`.
+	 *                        Or you can just a pass path to the JSON file in the assets directory.
+	 *                        You can also directly pass in the parsed object.
+	 * @return  Newly created `FlxAtlasFrames` collection.
+	 */
+	public static function fromAsepriteArray(FrameArray:Array<FlxGraphicAsset>, Description:AsepriteData):FlxAtlasFrames
+	{
+		var asepriteFrames:Array<FlxAtlasFrames> = [];
+
+		for (i in 0...FrameArray.length)
+			asepriteFrames.push(fromAseprite(FrameArray[i], Description));
+
+		var parent:FlxAtlasFrames = asepriteFrames[0];
+		asepriteFrames.shift();
+
+		for (i in asepriteFrames)
+			for (j in i.frames)
+				parent.pushFrame(j);
+
+		return parent;
+	}
+
+	/**
+	 * Internal method for Aseprite parsing. Parses the actual frame data.
+	 *
+	 * @param   FrameName   Name of the frame (file name of the original source image).
+	 * @param   FrameData   The Aseprite data excluding "filename".
+	 * @param   Frames      The `FlxAtlasFrames` to add this frame to.
+	 */
+	static function asepriteHelper(FrameName:String, FrameData:Dynamic, Frames:FlxAtlasFrames):Void
+	{
+		Frames.addAtlasFrame(FlxRect.get(FrameData.frame.x, FrameData.frame.y, FrameData.frame.w, FrameData.frame.h),
+			FlxPoint.get(FrameData.spriteSourceSize.w, FrameData.spriteSourceSize.h),
+			FlxPoint.get(FrameData.spriteSourceSize.x, FrameData.spriteSourceSize.y), FrameName, FlxFrameAngle.ANGLE_0, false, false);
 	}
 
 	/**
@@ -421,3 +509,92 @@ typedef TexturePackerObject =
 {
 	frames:Dynamic
 }
+
+typedef AsepriteData =
+{
+	var frames:Dynamic;
+	var meta:
+		{
+			app:String;
+			version:String;
+			image:String;
+			format:String;
+			size:
+				{
+					w:Float;
+					h:Float;
+				};
+			scale:String;
+			frameTags:Array<AsepriteFrameTag>;
+			layers:Array<AsepriteLayers>;
+			slices:Array<AsepriteSlices>;
+		};
+}
+
+typedef AsepriteFrame =
+{
+	var frame:
+		{
+			x:Float;
+			y:Float;
+			w:Float;
+			h:Float;
+		};
+	var rotated:Bool;
+	var trimmed:Bool;
+	var spriteSourceSize:
+		{
+			x:Float;
+			y:Float;
+			w:Float;
+			h:Float;
+		};
+	var sourceSize:
+		{
+			x:Float;
+			y:Float;
+		};
+	var duration:Float;
+}
+
+typedef AsepriteFrameTag =
+{
+	var name:String;
+	var from:Float;
+	var to:Float;
+	var direction:String;
+}
+
+typedef AsepriteLayers =
+{
+	var name:String;
+	var opacity:Float;
+	var blendMode:String;
+}
+
+typedef AsepriteSlices =
+{
+	var name:String;
+	var color:String;
+	var keys:Array<AsepriteSlicesKeys>;
+}
+
+typedef AsepriteSlicesKeys =
+{
+	var frame:Float, var bounds:
+		{
+			x:Float;
+			y:Float;
+			w:Float;
+			h:Float;
+		};
+	var center:
+		{
+			x:Float;
+			y:Float;
+			w:Float;
+			h:Float;
+		};
+}
+
+typedef FlxAsepriteSource = OneOfTwo<String, AsepriteData>;
