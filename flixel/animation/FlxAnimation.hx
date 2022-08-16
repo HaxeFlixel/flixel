b5a91ad06ea8bc6915fcf4fd7889458ebf19a175
Eric Myllyoja <ericmyllyoja@gmail.com>
2022-08-16 14:35:08 -0400

    Added loopPoint to FlxAnimation (#2621)

--- flixel/animation/FlxAnimation.hx
+++ flixel/animation/FlxAnimation.hx
@@ -44,6 +44,12 @@ class FlxAnimation extends FlxBaseAnimation
 	public var looped(default, null):Bool = true;
 
 	/**
+	 * The custom loop point for this animation.
+	 * This allows you to skip the first few frames of an animation when looping.
+	 */
+	public var loopPoint:Int = 0;
+
+	/**
 	 * Whether or not this animation is being played backwards.
 	 */
 	public var reversed(default, null):Bool = false;
@@ -191,7 +197,7 @@ class FlxAnimation extends FlxBaseAnimation
 			_frameTimer -= delay;
 			if (reversed)
 			{
-				if (looped && curFrame == 0)
+				if (looped && curFrame == loopPoint)
 					curFrame = numFrames - 1;
 				else
 					curFrame--;
@@ -199,7 +205,7 @@ class FlxAnimation extends FlxBaseAnimation
 			else
 			{
 				if (looped && curFrame == numFrames - 1)
-					curFrame = 0;
+					curFrame = loopPoint;
 				else
 					curFrame++;
 			}
