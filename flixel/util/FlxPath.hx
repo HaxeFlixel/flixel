a3ccb103fe91401d704174c84cf4bf913e7fe80c
George FunBook <gkurelic@gmail.com>
2022-11-18 13:22:02 -0600

    add deprecated FlxPath for 4.11 migrator

---
+++ flixel/util/FlxPath.hx
@@ -0,0 +1,7 @@
+package flixel.util;
+
+/** 
+ * DEPRECATED - FlxPath was moved to the `flixel.path` package. Update your imports.
+ */
+@:deprecated("FlxPath was moved to the package: `flixel.path`")
+typedef FlxPath = flixel.path.FlxPath;
