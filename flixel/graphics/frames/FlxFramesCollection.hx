13458e658f7ed0420f37c24e262843b96b5e680e
Gabriela Bestial <93128334+BeastlyGabi@users.noreply.github.com>
2023-03-07 21:54:52 -0400

    add "addFrameOffset" and "setFrameOffset" to FlxFramesCollection (#2746)
    * Update FlxAnimationController.hx
    
    * Update FlxAnimationController.hx
    
    * use FlxPoints
    
    * apply offsets in the altas
    
    * revert FlxAnimationController changes
    
    * add warnings
    
    ---------
    
    Co-authored-by: George FunBook <gkurelic@gmail.com>

--- flixel/graphics/frames/FlxFramesCollection.hx
+++ flixel/graphics/frames/FlxFramesCollection.hx
@@ -185,6 +185,40 @@ class FlxFramesCollection implements IFlxDestroyable
 
 		return pushFrame(texFrame);
 	}
+	
+	public function setFrameOffset(name:String, offsetX:Float, offsetY:Float)
+	{
+		if (framesHash.exists(name))
+			framesHash[name].offset.set(offsetX, offsetY);
+		else
+			FlxG.log.warn('No frame called $name');
+	}
+	
+	public function addFrameOffset(name:String, offsetX:Float, offsetY:Float)
+	{
+		if (framesHash.exists(name))
+			framesHash[name].offset.add(offsetX, offsetY);
+		else
+			FlxG.log.warn('No frame called $name');
+	}
+	
+	public function setFramesOffsetByPrefix(prefix:String, offsetX:Float, offsetY:Float)
+	{
+		for (name=>frame in framesHash)
+		{
+			if (name.indexOf(prefix) == 0)
+				frame.offset.set(offsetX, offsetY);
+		}
+	}
+	
+	public function addFramesOffsetByPrefix(prefix:String, offsetX:Float, offsetY:Float)
+	{
+		for (name=>frame in framesHash)
+		{
+			if (name.indexOf(prefix) == 0)
+				frame.offset.add(offsetX, offsetY);
+		}
+	}
 
 	/**
 	 * Checks if frame's area fits into atlas image, and trims if it's out of atlas image bounds.
