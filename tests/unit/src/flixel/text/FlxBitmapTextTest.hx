af0a034fe1759b795b5cd114586bebe4be8add50
George FunBook <gkurelic@gmail.com>
2023-03-08 11:57:35 -0600

    add test

--- tests/unit/src/flixel/text/FlxBitmapTextTest.hx
+++ tests/unit/src/flixel/text/FlxBitmapTextTest.hx
@@ -1,5 +1,8 @@
 package flixel.text;
 
+import flixel.text.FlxBitmapText;
+import flixel.graphics.frames.FlxBitmapFont;
+
 class FlxBitmapTextTest extends FlxTest
 {
 	var text:FlxBitmapText;
@@ -9,5 +12,20 @@ class FlxBitmapTextTest extends FlxTest
 	{
 		text = new FlxBitmapText();
 		destroyable = text;
+	}
+	
+	@Test // #1526 and #2750
+	function testCreateSpriteSkipPosition()
+	{
+		final text1 = new FlxBitmapText("test");
+		final text2 = new FlxBitmapText(FlxBitmapFont.getDefaultFont());
+
+		Assert.areEqual(0, text1.x);
+		Assert.areEqual(0, text1.y);
+		Assert.areEqual(0, text2.x);
+		Assert.areEqual(0, text2.y);
+
+		Assert.isNotNull(text1.text);
+		Assert.areEqual(text1.font, text2.font);
 	}
 }
