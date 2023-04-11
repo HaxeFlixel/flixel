2cff5c3f50b2a539f9e371a3559dea95511ff7d2
DalekCraft <ericdalekcraft@gmail.com>
2023-04-11 11:54:08 -0400

    Correct a doc comment in FlxCamera

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -128,7 +128,7 @@ class FlxCamera extends FlxBasic
 	/**
 	 * Used to smoothly track the camera as it follows:
 	 * The percent of the distance to the follow `target` the camera moves per 1/60 sec.
-	 * Values are bounded between `0.0` and `FlxG.updateFrameRate / 60` for consistency across framerates.
+	 * Values are bounded between `0.0` and `60 / FlxG.updateFramerate` for consistency across framerates.
 	 * The maximum value means no camera easing. A value of `0` means the camera does not move.
 	 */
 	public var followLerp(default, set):Float = 60 / FlxG.updateFramerate;
