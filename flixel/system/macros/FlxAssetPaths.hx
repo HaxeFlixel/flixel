61809290435ab786fbe495be6b76102ab12cab0c
George FunBook <gkurelic@gmail.com>
2023-05-23 13:13:52 -0500

    reorganize and comment

--- flixel/system/macros/FlxAssetPaths.hx
+++ flixel/system/macros/FlxAssetPaths.hx
@@ -32,20 +32,29 @@ class FlxAssetPaths
 
 		Context.registerModuleDependency(Context.getLocalModule(), directory);
 
-		final fileReferences = addFileReferences([], directory, subDirectories, include, exclude, rename);
-		final fields = Context.getBuildFields();
+		final fileReferences = getFileReferences(directory, subDirectories, include, exclude);
+		
+		// determine field names, remove nulls and warn on invalid names
+		nameFileReferences(fileReferences, rename);
+		
+		// create static list of all files
 		final allFiles = [];
+		for (fileRef in fileReferences)
+			allFiles.push(macro $i{fileRef.name});
+		
+		// warns on any duplicates
+		removeDuplicates(fileReferences);
+		
+		final fields = Context.getBuildFields();
 		var listConflictingPath:String = null;
-
-		// create new fields based on file references!
 		for (fileRef in fileReferences)
 		{
-			if (fileRef.duplicateOf == null)
-				fields.push(fileRef.createField());
-
+			// create new fields based on file references!
+			fields.push(fileRef.createField());
+			
+			// make sure no fields conflict with the list field name
 			if (listField != "" && listConflictingPath == null)
 			{
-				allFiles.push(macro $i{fileRef.name});
 				if (fileRef.name == listField)
 					listConflictingPath = fileRef.value;
 			}
@@ -72,9 +81,30 @@ class FlxAssetPaths
 		
 		return fields;
 	}
-
-	static function addFileReferences(fileReferences:Array<FileReference>, directory:String, subDirectories = false, ?include:EReg, ?exclude:EReg,
-			?rename:String->Null<String>):Array<FileReference>
+	
+	/**
+	 * Returns all files in a directory that fit the include/exclude criteria.
+	 * @param   directory       The folder of assets to build fields for
+	 * @param   subDirectories  If true, all subfolders will be included
+	 * @param   include         Regular Expression used to allow files based by name
+	 * @param   exclude         Regular Expression used to omit files based by name
+	 */
+	inline static function getFileReferences(directory:String, subDirectories = false, ?include:EReg, ?exclude:EReg):Array<FileReference>
+	{
+		return addFileReferences([], directory, subDirectories, include, exclude);
+	}
+	
+	/**
+	 * Searches for all files in a directory that fit the include/exclude criteria and adds them to
+	 * the target array
+	 * 
+	 * @param   fileReferences  The array to add the references to
+	 * @param   directory       The folder of assets to build fields for
+	 * @param   subDirectories  If true, all subfolders will be included
+	 * @param   include         Regular Expression used to allow files based by name
+	 * @param   exclude         Regular Expression used to omit files based by name
+	 */
+	static function addFileReferences(fileReferences:Array<FileReference>, directory:String, subDirectories = false, ?include:EReg, ?exclude:EReg):Array<FileReference>
 	{
 		var resolvedPath = #if (ios || tvos) "../assets/" + directory #else directory #end;
 		var directoryInfo = FileSystem.readDirectory(resolvedPath);
@@ -94,41 +124,83 @@ class FlxAssetPaths
 				if (exclude != null && exclude.match(path))
 					continue;
 
-				var reference = FileReference.fromPath(path, rename);
-				if (reference != null)
-				{
-					checkDuplicates(fileReferences, reference);
-					fileReferences.push(reference);
-				}
+				fileReferences.push(new FileReference(path));
 			}
 			else if (subDirectories)
 			{
-				addFileReferences(fileReferences, directory + name + "/", true, include, exclude, rename);
+				addFileReferences(fileReferences, directory + name + "/", true, include, exclude);
 			}
 		}
 
 		return fileReferences;
 	}
 	
-	static function checkDuplicates(fileReferences:Array<FileReference>, file:FileReference)
+	/**
+	 * Creates a name for each file, removes null and warns on invalid names
+	 */
+	static function nameFileReferences(fileReferences:Array<FileReference>, ?rename:String->Null<String>)
 	{
-		for (i in 0...fileReferences.length)
+		var i = fileReferences.length;
+		while (i-- > 0)
 		{
-			if (fileReferences[i].name == file.name)
+			final fileRef = fileReferences[i];
+			
+			// create field name
+			fileRef.createName(rename);
+			
+			if (fileRef.name == null)
+				// remove if null name
+				fileReferences.splice(i, 1);
+		}
+	}
+	
+	/**
+	 * Searches for, removes and warns of all duplicate field names
+	 */
+	static function removeDuplicates(fileReferences:Array<FileReference>)
+	{
+		var ignoredFiles = new Map<String, Array<FileReference>>();
+		function addIgnored(file:FileReference)
+		{
+			if (!ignoredFiles.exists(file.name))
+				ignoredFiles[file.name] = [file];
+			else
+				ignoredFiles[file.name].push(file);
+		}
+		
+		// loop backward so we can delete duplicates
+		var i = fileReferences.length;
+		while (i-- > 0)
+		{
+			final file = fileReferences[i];
+			var j = fileReferences.length;
+			while (j-- > 0)
 			{
-				var oldValue = fileReferences[i].value;
-				// if the old file is nested deeper in the folder structure
-				if (oldValue.split("/").length > file.value.split("/").length)
+				final otherFile = fileReferences[j];
+				final sameName = i != j && otherFile.name == file.name;
+				// ignore the file nested deeper
+				if (sameName && otherFile.value.split("/").length <= file.value.split("/").length)
 				{
-					// replace it with the new one
-					fileReferences[i].duplicateOf = file;
-					file.warn('Duplicate files named "${file.name}" ignoring $oldValue');
+					fileReferences.splice(i, 1);
+					addIgnored(file);
+					break;
 				}
-				else
-				{
-					file.duplicateOf = fileReferences[i];
-					file.warn('Duplicate files named "${file.name}" ignoring ${file.value}');
-				}
+			}
+		}
+		
+		// Show warnings for ignored files, if were not doing unit tests
+		for (name=>files in ignoredFiles)
+		{
+			if (files.length == 1)
+			{
+				final file = files[0];
+				file.warn('Duplicate files named "$name" ignoring "${file.value}"');
+			}
+			else
+			{
+				Context.warning('Multiple files named "$name"', Context.currentPos());
+				for (file in files)
+					file.warn('... ignoring "${file.value}"');
 			}
 		}
 	}
@@ -137,45 +209,52 @@ class FlxAssetPaths
 private class FileReference
 {
 	static var valid = ~/^[_A-Za-z]\w*$/;
-
-	public static function fromPath(value:String, ?library:String, ?rename:String->Null<String>):Null<FileReference>
+	
+	static function defaultNamer(path:String)
 	{
-		var name = value;
+		return path.split("/").pop();
+	}
 
-		if (rename != null)
-		{
-			name = rename(name);
-			// exclude null
-			if (name == null)
-				return null;
-		}
-		else
-			name = value.split("/").pop();
+	public final value:String;
+	public final documentation:String;
+	public var name(default, null):String = null;
 
+	public function new(value:String)
+	{
+		this.value = value;
+		this.documentation = "`\"" + value + "\"` (auto generated).";
+	}
+	
+	/**
+	 * Creates the name for this field using the supplied method and the path.
+	 * @param   namer  Function that takes a path and returns a haxe field name.
+	 */
+	public function createName(?namer:(String)->Null<String>)
+	{
+		if (namer == null)
+			namer = defaultNamer;
+		
+		name = namer(value);
+		if (name == null)
+			return;
+		
+		validateName();
+	}
+	
+	/**
+	 * Replaces hyphens, spaces and periods, and warns if the resulting name is not a valid haxe identifier.
+	 */
+	public function validateName()
+	{
 		// replace some forbidden names to underscores, since variables cannot have these symbols.
 		name = name.split("-").join("_").split(" ").join("_").split(".").join("__");
+		
+		// warn if the name is invalid
 		if (!valid.match(name)) // #1796
 		{
 			warnAsset('Invalid name: $name for file: $value', value);
-			return null;
+			name = null;
 		}
-		
-		if (library != "default" && library != "" && library != null)
-			value = '$library:$value';
-		
-		return new FileReference(name, value);
-	}
-
-	public var name(default, null):String;
-	public var value(default, null):String;
-	public var documentation(default, null):String;
-	public var duplicateOf:FileReference = null;
-
-	function new(name:String, value:String)
-	{
-		this.name = name;
-		this.value = value;
-		this.documentation = "`\"" + value + "\"` (auto generated).";
 	}
 	
 	public function createField():Field
