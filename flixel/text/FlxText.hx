75c0379155f2f751c65e2606a864ed23f1a6b8de
Timur <T1mL3arn@users.noreply.github.com>
2023-10-16 10:20:46 +1000

    Small class doc fix for FlxText (#2939)
    * Small class doc fix
    
    * add @since meta for fieldHeight

--- flixel/text/FlxText.hx
+++ flixel/text/FlxText.hx
@@ -39,7 +39,7 @@ import openfl.utils.AssetType;
  * ## Autosizing
  * 
  * By default `FlxText` is autosized to fit it's text. 
- * To set a fixed size, * use the `fieldWidth`, `fieldHeight` and `autoSize` fields.
+ * To set a fixed size, use the `fieldWidth`, `fieldHeight` and `autoSize` fields.
  */
 class FlxText extends FlxSprite
 {
@@ -137,6 +137,7 @@ class FlxText extends FlxSprite
 	 * Use it when you want to change the visible height of the text. Enables "auto height" if `<= 0`.
 	 * 
 	 * **NOTE:** Fixed height has no effect if `autoSize = true`.
+	 * @since 5.4.0
 	 */
 	public var fieldHeight(get, set):Float;
 
