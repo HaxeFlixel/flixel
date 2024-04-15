1b6d3fd404bfca439ece1ef9a38ec7970564c1c7
rich <87835336+richTrash21@users.noreply.github.com>
2024-04-16 01:04:06 +0400

    Calculate adjustedLerp only when necessary (#3106)
    * Calculate adjustedLerp only when necessary
    
    * Skip updateLerp on followLerp <= 0
    
    * replace boundLerp with gt/lt checks
    
    ---------
    
    Co-authored-by: George Kurelic <Gkurelic@gmail.com>

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -1257,15 +1257,15 @@ class FlxCamera extends FlxBasic
 	
 	function updateLerp(elapsed:Float)
 	{
-		final boundLerp = FlxMath.bound(followLerp, 0, 1);
-		// Adjust lerp based on the current frame rate so lerp is less framerate dependant
-		final adjustedLerp = 1.0 - Math.pow(1.0 - boundLerp, elapsed * 60);
-		if (adjustedLerp >= 1)
+		if (followLerp >= 1.0)
 		{
 			scroll.copyFrom(_scrollTarget); // no easing
 		}
-		else
+		else if (followLerp > 0.0)
 		{
+			// Adjust lerp based on the current frame rate so lerp is less framerate dependant
+			final adjustedLerp = 1.0 - Math.pow(1.0 - followLerp, elapsed * 60);
+			
 			scroll.x += (_scrollTarget.x - scroll.x) * adjustedLerp;
 			scroll.y += (_scrollTarget.y - scroll.y) * adjustedLerp;
 		}
