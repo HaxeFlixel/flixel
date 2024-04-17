f4989951e718119adaff6f93275376a2951540a9
George Kurelic <Gkurelic@gmail.com>
2024-04-16 20:54:21 -0500

    Revert "update transform after setting pan (#2863)"
    This reverts commit 295cececa220bd7b1b1c5ba821cf8b3dead1fa13.

--- flixel/sound/FlxSound.hx
+++ flixel/sound/FlxSound.hx
@@ -81,9 +81,6 @@ class FlxSound extends FlxBasic
 	
 	/**
 	 * Pan amount. -1 = full left, 1 = full right. Proximity based panning overrides this.
-	 * 
-	 * Note: On desktop targets this only works with mono sounds, due to limitations of OpenAL.
-	 * More info: [OpenFL Forums - SoundTransform.pan does not work](https://community.openfl.org/t/windows-legacy-soundtransform-pan-does-not-work/6616/2?u=geokureli)
 	 */
 	public var pan(get, set):Float;
 	
@@ -776,9 +773,7 @@ class FlxSound extends FlxBasic
 	
 	inline function set_pan(pan:Float):Float
 	{
-		_transform.pan = pan;
-		updateTransform();
-		return pan;
+		return _transform.pan = pan;
 	}
 	
 	inline function get_time():Float
