6ac32105048a5a54752098a1feb244e5bd3135bd
George FunBook <gkurelic@gmail.com>
2024-04-21 11:58:45 -0400

    add camera(s) unit tests to sprite groups

--- tests/unit/src/flixel/group/FlxSpriteContainerTest.hx
+++ tests/unit/src/flixel/group/FlxSpriteContainerTest.hx
@@ -13,4 +13,26 @@ class FlxSpriteContainerTest extends FlxSpriteGroupTest
 			group.add(new FlxSpriteContainer());
 		destroyable = group;
 	}
+	
+	override function testMemberCameras()
+	{
+		final subGroup1 = new FlxSpriteContainer();
+		group.add(subGroup1);
+		final subGroup2 = new FlxSpriteContainer();
+		subGroup1.add(subGroup2);
+		final member1 = new FlxSprite();
+		final member2 = new FlxSprite();
+		subGroup2.add(member1);
+		subGroup2.add(member2);
+		
+		final cam = new FlxCamera();
+		group.camera = cam;
+		Assert.areEqual(cam, member1.getCameras()[0]);
+		Assert.areEqual(cam, member2.getCameras()[0]);
+		
+		final cams = [new FlxCamera()];
+		group.cameras = cams;
+		Assert.areEqual(cams, member1.getCameras());
+		Assert.areEqual(cams, member2.getCameras());
+	}
 }
