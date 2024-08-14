1a714f668d68d4d7a2eb8f6203b3317597e26b88
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-08-14 14:09:35 -0400

    remove trailing whitespace

--- flixel/system/debug/completion/CompletionList.hx
+++ flixel/system/debug/completion/CompletionList.hx
@@ -13,11 +13,11 @@ class CompletionList extends Sprite
 	public var completed:String->Void;
 	public var selectionChanged:String->Void;
 	public var closed:Void->Void;
-	
+
 	public var filter(default, set):String;
-	
+
 	public var items(default, null):Array<String>;
-	
+
 	var entries:Array<CompletionListEntry> = [];
 	var originalItems:Array<String>;
 	var selectedIndex:Int = 0;
@@ -25,44 +25,44 @@ class CompletionList extends Sprite
 	var upperVisibleIndex:Int = 0;
 	var scrollBar:CompletionListScrollBar;
 	var actualHeight:Int;
-	
+
 	public function new(capacity:Int)
 	{
 		super();
-		
+
 		visible = false;
 		upperVisibleIndex = capacity - 1;
 		actualHeight = capacity * CompletionListEntry.HEIGHT;
-		
+
 		createPopupEntries(capacity);
 		createScrollBar();
 		updateSelectedItem();
 	}
-	
+
 	public function show(x:Float, items:Array<String>)
 	{
 		visible = true;
 		this.x = x;
 		this.originalItems = items;
 		filter = "";
-		
+
 		updateEntries();
 	}
-	
+
 	public function setY(y:Float)
 	{
 		this.y = y - actualHeight;
 	}
-	
+
 	public function close()
 	{
 		visible = false;
 		filter = null;
-		
+
 		if (closed != null)
 			closed();
 	}
-	
+
 	function createPopupEntries(amount:Int)
 	{
 		for (i in 0...amount)
@@ -73,73 +73,73 @@ class CompletionList extends Sprite
 			entry.y = CompletionListEntry.HEIGHT * i;
 		}
 	}
-	
+
 	function createScrollBar()
 	{
 		scrollBar = new CompletionListScrollBar(CompletionListEntry.WIDTH, 0, 5, actualHeight);
 		addChild(scrollBar);
 	}
-	
+
 	public function onKeyDown(e:KeyboardEvent)
 	{
 		if (!visible)
 			return;
-			
+
 		switch (e.keyCode)
 		{
 			case Keyboard.DOWN:
 				updateIndices(1);
-				
+
 			case Keyboard.UP:
 				updateIndices(-1);
-				
+
 			case Keyboard.ENTER | Keyboard.TAB:
 				if (completed != null)
 					completed(items[selectedIndex]);
 				close();
 				return;
-				
+
 			case Keyboard.ESCAPE:
 				close();
 				return;
 		}
-		
+
 		updateEntries();
 	}
-	
+
 	function updateIndices(modifier:Int)
 	{
 		selectedIndex = bound(selectedIndex + modifier);
 		if (FlxMath.inBounds(selectedIndex, lowerVisibleIndex, upperVisibleIndex))
 			return;
-			
+
 		// scroll visible area
 		lowerVisibleIndex = bound(lowerVisibleIndex + modifier);
 		upperVisibleIndex = bound(upperVisibleIndex + modifier);
 		var range = upperVisibleIndex - lowerVisibleIndex;
-		
+
 		if (range == items.length)
 			return;
-			
+
 		// hit lower or upper end
 		if (lowerVisibleIndex == 0)
 			upperVisibleIndex = entries.length - 1;
 		else if (upperVisibleIndex == items.length - 1)
 			lowerVisibleIndex = items.length - entries.length;
 	}
-	
+
 	function bound(index:Int)
 	{
 		return Std.int(FlxMath.bound(index, 0, items.length - 1));
 	}
-	
+
 	function updateEntries()
 	{
 		updateLabels();
 		updateSelectedItem();
 		scrollBar.updateHandle(lowerVisibleIndex, items.length, entries.length);
 	}
-	
+
 	function updateLabels()
 	{
 		for (i in 0...entries.length)
@@ -150,43 +150,43 @@ class CompletionList extends Sprite
 			entries[i].setItem(selectedItem);
 		}
 	}
-	
+
 	function updateSelectedItem()
 	{
 		for (entry in entries)
 			entry.selected = false;
 		entries[selectedIndex - lowerVisibleIndex].selected = true;
-		
+
 		if (selectionChanged != null)
 			selectionChanged(items[selectedIndex]);
 	}
-	
+
 	function setItems(items:Array<String>)
 	{
 		if (items == null)
 			return;
 		if (items.length == 0)
 			close();
-			
+
 		this.items = items;
-		
+
 		selectedIndex = 0;
 		lowerVisibleIndex = 0;
 		upperVisibleIndex = entries.length - 1;
 		updateEntries();
 	}
-	
+
 	function filterItems(filter:String):Array<String>
 	{
 		if (filter == null)
 			filter = "";
-			
+
 		return sortItems(filter, originalItems.filter(function(item)
 		{
 			return item.toLowerCase().contains(filter.toLowerCase());
 		}));
 	}
-	
+
 	/**
 	 * sort items so that:
 	 *   - strings starting with the filter have a higher priority than those only containing it
@@ -196,7 +196,7 @@ class CompletionList extends Sprite
 	{
 		if (filter == "")
 			return items;
-			
+
 		items.sort(function(a, b)
 		{
 			var valueA = startsWithExt(a, filter);
@@ -205,14 +205,14 @@ class CompletionList extends Sprite
 				return -valueA;
 			if (valueB > valueA)
 				return valueB;
-				
+
 			if (valueA == valueB)
 				return Std.int(a.length - b.length);
 			return 0;
 		});
 		return items;
 	}
-	
+
 	/**
 	 * Custom startsWith() that ignores leading underscores.
 	 */
@@ -224,12 +224,12 @@ class CompletionList extends Sprite
 			return 1;
 		return 0;
 	}
-	
+
 	function set_filter(filter:String):String
 	{
 		if (filter == this.filter)
 			return filter;
-			
+
 		setItems(filterItems(filter));
 		return this.filter = filter;
 	}
