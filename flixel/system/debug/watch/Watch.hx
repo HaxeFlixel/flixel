5fda4bdd8aab86bc958478f91b90bf4627dafc2f
GeoKureli-BlackbookPro <GKurelic@gmail.com>
2025-02-14 16:26:32 -0600

    add scroll bars to watch

--- flixel/system/debug/watch/Watch.hx
+++ flixel/system/debug/watch/Watch.hx
@@ -1,12 +1,14 @@
 package flixel.system.debug.watch;
 
-import openfl.display.DisplayObject;
 import flixel.FlxG;
 import flixel.math.FlxPoint;
 import flixel.system.debug.FlxDebugger.GraphicWatch;
+import openfl.display.DisplayObject;
+import openfl.display.Shape;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.events.MouseEvent;
+import openfl.geom.Point;
 import openfl.geom.Rectangle;
 
 using flixel.util.FlxStringUtil;
@@ -36,6 +38,7 @@ class WatchBase<TEntry:WatchEntry> extends Window
 	inline function set_alwaysOnTop(value:Bool) return _alwaysOnTop = value;
 	
 	final entriesContainer:ScrollSprite;
+	final scrollbar:ScrollBar;
 	final entries:Array<TEntry> = [];
 	var create:(displayName:String, data:WatchEntryData)->TEntry;
 	
@@ -48,6 +51,10 @@ class WatchBase<TEntry:WatchEntry> extends Window
 		entriesContainer.x = 2;
 		entriesContainer.y = 15;
 		addChild(entriesContainer);
+		
+		scrollbar = entriesContainer.createScrollBar();
+		scrollbar.y = entriesContainer.y;
+		addChild(scrollbar);
 		
 		FlxG.signals.preStateSwitch.add(clear);
 	}
@@ -160,13 +167,16 @@ class WatchBase<TEntry:WatchEntry> extends Window
 		super.updateSize();
 		minSize.x = oldMinSize;
 		
+		scrollbar.x = _width - scrollbar.width - 2;
+		scrollbar.resize(getMarginHeight() - 10);
+		
 		entriesContainer.setScrollSize(getMarginWidth(), getMarginHeight());
 		resetEntries();
 	}
 	
 	function getMarginWidth()
 	{
-		return _width - entriesContainer.x - (_resizable ? 5 : 3);
+		return _width - entriesContainer.x - (_resizable ? 5 : 3) - scrollbar.width - 4;
 	}
 	
 	function getMarginHeight()
@@ -213,7 +223,26 @@ class WatchBase<TEntry:WatchEntry> extends Window
 
 class ScrollSprite extends Sprite
 {
+	public var maxScrollY(get, never):Float;
+	inline function get_maxScrollY():Float return this.height - scroll.height;
+	
+	public var viewHeight(get, never):Float;
+	inline function get_viewHeight():Float return scroll.height;
+	
+	/**
+	 * The current amount of scrolling
+	 */
+	public var scrollY(get, set):Float;
+	inline function get_scrollY():Float return scroll.y;
+	inline function set_scrollY(value):Float
+	{
+		scroll.y = value;
+		updateScroll();
+		return scroll.y;
+	}
+	
 	var scroll = new Rectangle();
+	var scrollBar:ScrollBar = null;
 	
 	public function new ()
 	{
@@ -225,6 +254,11 @@ class ScrollSprite extends Sprite
 			stage.addEventListener(MouseEvent.MOUSE_WHEEL, onMouseScroll);
 			addEventListener(Event.REMOVED_FROM_STAGE, (_)->stage.removeEventListener(MouseEvent.MOUSE_WHEEL, onMouseScroll));
 		});
+	}
+	
+	public function createScrollBar()
+	{
+		return scrollBar = new ScrollBar(this);
 	}
 	
 	function onMouseScroll(e:MouseEvent)
@@ -243,6 +277,7 @@ class ScrollSprite extends Sprite
 	{
 		scroll.width = width;
 		scroll.height = height;
+		
 		updateScroll();
 	}
 	
@@ -257,6 +292,9 @@ class ScrollSprite extends Sprite
 			scroll.y = 0;
 		
 		scrollRect = scroll;
+		
+		if (scrollBar != null)
+			scrollBar.onViewChange();
 	}
 	
 	override function addChild(child)
@@ -273,4 +311,143 @@ class ScrollSprite extends Sprite
 		
 		return child.y < scroll.bottom && child.y + child.height > scroll.y;
 	}
+}
+
+@:allow(flixel.system.debug.watch.ScrollSprite)
+class ScrollBar extends Sprite
+{
+	static inline final WIDTH = 10;
+	
+	final target:ScrollSprite;
+	
+	final handle = new Sprite();
+	final bg = new Sprite();
+	
+	var state:ScrollState = IDLE;
+	
+	public function new (target:ScrollSprite)
+	{
+		this.target = target;
+		super();
+		
+		bg.mouseChildren = true;
+		bg.mouseEnabled = true;
+		bg.graphics.beginFill(0xFFFFFF, 0.1);
+		bg.graphics.drawRect(0, 0, WIDTH, 1);
+		bg.graphics.endFill();
+		addChild(bg);
+		
+		handle.mouseChildren = true;
+		handle.mouseEnabled = true;
+		handle.buttonMode = true;
+		handle.graphics.beginFill(0xFFFFFF, 0.3);
+		handle.graphics.drawRect(0, 0, WIDTH, 1);
+		handle.graphics.endFill();
+		addChild(handle);
+		
+		function onAdded(_)
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, onAdded);
+			final stage = this.stage;
+			
+			bg.addEventListener(MouseEvent.MOUSE_DOWN, onBgMouseDown);
+			handle.addEventListener(MouseEvent.MOUSE_DOWN, onHandleMouse);
+			stage.addEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);
+			stage.addEventListener(MouseEvent.MOUSE_UP, onHandleMouse);
+			
+			function onRemoved(_)
+			{
+				removeEventListener(Event.REMOVED_FROM_STAGE, onRemoved);
+				
+				bg.removeEventListener(MouseEvent.MOUSE_DOWN, onBgMouseDown);
+				handle.removeEventListener(MouseEvent.MOUSE_DOWN, onHandleMouse);
+				stage.removeEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);
+				stage.removeEventListener(MouseEvent.MOUSE_UP, onHandleMouse);
+			}
+			addEventListener(Event.REMOVED_FROM_STAGE, onRemoved);
+		}
+		addEventListener(Event.ADDED_TO_STAGE, onAdded);
+	}
+	
+	function onBgMouseDown(e:MouseEvent)
+	{
+		if (state != IDLE)
+			throw "expected state: IDLE";
+		
+		state = DRAG_BG;
+		mouseMoveHelper(e.stageY);
+	}
+	
+	function onHandleMouse(e:MouseEvent)
+	{
+		if (e.type == MouseEvent.MOUSE_DOWN)
+		{
+			if (state != IDLE)
+				throw "expected state: IDLE";
+			
+			final localY = getLocalY(e.stageY);
+			FlxG.watch.addQuick('drag offset', localY - handle.y);
+			state = DRAG_HANDLE(localY - handle.y);
+		}
+		else
+			state = IDLE;
+	}
+	
+	function onMouseMove(e:MouseEvent)
+	{
+		mouseMoveHelper(e.stageY);
+	}
+	
+	function getLocalY(stageY:Float)
+	{
+		return globalToLocal(new Point (0, stageY)).y;
+	}
+	
+	function mouseMoveHelper(stageY:Float)
+	{
+		final localY = getLocalY(stageY);
+		FlxG.watch.addQuick('scrollbar', 'stageY: $stageY, localY: $localY, height: ${bg.height}');
+		
+		switch state
+		{
+			case IDLE:
+			case DRAG_HANDLE(offsetY):
+				handle.y = localY - offsetY;
+				onHandleMove();
+			case DRAG_BG:
+				handle.y = localY - handle.height / 2;
+				onHandleMove();
+		}
+	}
+	
+	function onHandleMove()
+	{
+		if (handle.y < 0)
+			handle.y = 0;
+		
+		if (handle.y > bg.height - handle.height)
+			handle.y = bg.height - handle.height;
+		
+		target.scrollY = handle.y / (bg.height - handle.height) * target.maxScrollY;
+	}
+	
+	public function resize(height:Float)
+	{
+		bg.height = height;
+		handle.height = height / target.height * target.viewHeight;
+		onViewChange();
+	}
+	
+	function onViewChange()
+	{
+		mouseEnabled = mouseChildren = visible = target.maxScrollY > 0 && target.maxScrollY < target.height;
+		handle.y = (target.scrollY / target.maxScrollY) * (bg.height - handle.height);
+	}
+}
+
+enum ScrollState
+{
+	IDLE;
+	DRAG_HANDLE(offsetY:Float);
+	DRAG_BG;
 }
