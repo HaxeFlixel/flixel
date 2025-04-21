b99e3119752a4f0580f3d67e87cb96d06c7b0a1f
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-04-21 12:31:07 -0400

    logarithmic volume control for global soundtray / volume (#3248)
    * logarithmic volume control for global soundtray / volume
    
    * use logarithmic sound volume at the transform
    
    * clamp volume again
    
    * small fixes
    
    * add dynamic FlxG.applySoundCurve
    
    * fix coverage
    
    ---------
    
    Co-authored-by: GeoKureli-BlackbookPro <GKurelic@gmail.com>

--- flixel/sound/FlxSound.hx
+++ flixel/sound/FlxSound.hx
@@ -594,11 +594,24 @@ class FlxSound extends FlxBasic
 	@:allow(flixel.sound.FlxSoundGroup)
 	function updateTransform():Void
 	{
-		_transform.volume = #if FLX_SOUND_SYSTEM (FlxG.sound.muted ? 0 : 1) * FlxG.sound.volume * #end
-			(group != null ? group.volume : 1) * _volume * _volumeAdjust;
-			
+		_transform.volume = calcTransformVolume();
+		
 		if (_channel != null)
 			_channel.soundTransform = _transform;
+	}
+	
+	function calcTransformVolume():Float
+	{
+		final volume = (group != null ? group.volume : 1.0) * _volume * _volumeAdjust;
+		
+		#if FLX_SOUND_SYSTEM
+		if (FlxG.sound.muted)
+			return 0.0;
+		
+		return FlxG.sound.applySoundCurve(FlxG.sound.volume * volume);
+		#else
+		return volume;
+		#end
 	}
 	
 	/**
