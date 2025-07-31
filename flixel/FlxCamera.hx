c9ae61e6ab7631a2704328b1484a523fc7537116
nick <nickngc28@gmail.com>
2025-07-31 17:31:45 +0300

    Fix alpha channel (#3455)

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -1707,7 +1707,7 @@ class FlxCamera extends FlxBasic
 			}
 			else
 			{
-				final alpha = color.alphaFloat * _fxFlashAlpha;
+				final alpha = _fxFlashColor.alphaFloat * _fxFlashAlpha;
 				fill(_fxFlashColor.rgb, true, alpha, canvas.graphics);
 			}
 		}
