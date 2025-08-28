f359716d2412a28a25ba113e0870d82f3563b3d4
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2025-08-28 23:26:11 +0200

    Remove references to unused "blending" variable

--- flixel/graphics/tile/FlxDrawBaseItem.hx
+++ flixel/graphics/tile/FlxDrawBaseItem.hx
@@ -16,11 +16,6 @@ class FlxDrawBaseItem<T>
 	 */
 	public static var drawCalls:Int = 0;
 
-	public static function blendToInt(blend:BlendMode):Int
-	{
-		return 0; // no blend mode support in drawQuads()
-	}
-
 	public var nextTyped:T;
 
 	public var next:FlxDrawBaseItem<T>;
@@ -29,7 +24,6 @@ class FlxDrawBaseItem<T>
 	public var antialiasing:Bool = false;
 	public var colored:Bool = false;
 	public var hasColorOffsets:Bool = false;
-	public var blending:Int = 0;
 	public var blend:BlendMode;
 
 	public var type:FlxDrawItemType;
