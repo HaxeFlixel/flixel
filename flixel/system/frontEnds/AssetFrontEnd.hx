04d71e01cc6f5b7b210d5b09be65160367cb7e4d
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2025-09-19 14:54:20 +0200

    AssetFrontEnd: check length instead of comparing strings

--- flixel/system/frontEnds/AssetFrontEnd.hx
+++ flixel/system/frontEnds/AssetFrontEnd.hx
@@ -461,7 +461,7 @@ class AssetFrontEnd
 	
 	inline function addSoundExt(id:String)
 	{
-		final needsExt = Path.extension(id) == "";
+		final needsExt = Path.extension(id).length == 0;
 		if (needsExt)
 			return id + defaultSoundExtension;
 			
