dfb36b74155c39afc932c513034b1b96dba7acd5
cyn <cyn0x8+git@gmail.com>
2025-09-24 10:49:10 -0700

    remove alpha check

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -1676,9 +1676,6 @@ class FlxCamera extends FlxBasic
 		}
 		else
 		{
-			if (FxAlpha == 0 && (!filtersEnabled || filters == null || filters.length == 0))
-				return;
-
 			final targetGraphics = (graphics == null) ? canvas.graphics : graphics;
 
 			targetGraphics.overrideBlendMode(null);
